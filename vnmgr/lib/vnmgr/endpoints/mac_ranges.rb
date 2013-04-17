2e804ea8b79f368b9e93b82a654643d30be01e8e
Metallion <akieckens@axsh.net>
2013-04-17 17:21:04 +0900

    Added endpoint skeleton for mac ranges

---
+++ vnmgr/lib/vnmgr/endpoints/mac_ranges.rb
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+Vnmgr::Endpoints::VNetAPI.namespace '/mac_ranges' do
+  post do
+    mr_params = define_params(params,{
+      :uuid => [String,nil],
+      :range_begin => [Integer],
+      :range_end => [Integer]
+    })
+
+    # Respond with new mac range
+  end
+
+  get do
+    # Respons with all mac ranges
+  end
+
+  get '/:uuid' do
+    mr_params = define_params(params,{:uuid => [String]})
+  end
+
+  delete '/:uuid' do
+    mr_params = define_params(params,{:uuid => [String]})
+  end
+end
