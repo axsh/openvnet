5bbb921c866ddf8f0ac1d72915f8955b3e411cf8
akry <a-yokokawa@axsh.net>
2013-05-17 22:14:30 +0900

    Fixed vnmgr.rb

--- vnmgr/lib/vnmgr.rb
+++ vnmgr/lib/vnmgr.rb
@@ -26,7 +26,30 @@ module Vnmgr
 
       module Responses
         autoload :Network, 'vnmgr/endpoints/1.0/responses/network'
+        autoload :Vif, 'vnmgr/endpoints/1.0/responses/vif'
+        autoload :DhcpRange, 'vnmgr/endpoints/1.0/responses/dhcp_range'
+        autoload :MacRange, 'vnmgr/endpoints/1.0/responses/mac_range'
+        autoload :MacLease, 'vnmgr/endpoints/1.0/responses/mac_lease'
+        autoload :Router, 'vnmgr/endpoints/1.0/responses/router'
+        autoload :Tunnel, 'vnmgr/endpoints/1.0/responses/tunnel'
+        autoload :DcNetwork, 'vnmgr/endpoints/1.0/responses/dc_network'
+        autoload :Datapath, 'vnmgr/endpoints/1.0/responses/datapath'
+        autoload :OpenFlowController, 'vnmgr/endpoints/1.0/responses/open_flow_controller'
+        autoload :IpAddress, 'vnmgr/endpoints/1.0/responses/ip_address'
+        autoload :IpLease, 'vnmgr/endpoints/1.0/responses/ip_lease'
+
         autoload :NetworkCollection, 'vnmgr/endpoints/1.0/responses/network'
+        autoload :VifCollection, 'vnmgr/endpoints/1.0/responses/vif'
+        autoload :DhcpRangeCollection, 'vnmgr/endpoints/1.0/responses/dhcp_range'
+        autoload :MacRangeCollection, 'vnmgr/endpoints/1.0/responses/mac_range'
+        autoload :MacLeaseCollection, 'vnmgr/endpoints/1.0/responses/mac_lease'
+        autoload :RouterCollection, 'vnmgr/endpoints/1.0/responses/router'
+        autoload :TunnelCollection, 'vnmgr/endpoints/1.0/responses/tunnel'
+        autoload :DcNetworkCollection, 'vnmgr/endpoints/1.0/responses/dc_network'
+        autoload :DatapathCollection, 'vnmgr/endpoints/1.0/responses/datapath'
+        autoload :OpenFlowControllerCollection, 'vnmgr/endpoints/1.0/responses/open_flow_controller'
+        autoload :IpAddressCollection, 'vnmgr/endpoints/1.0/responses/ip_address'
+        autoload :IpLeaseCollection, 'vnmgr/endpoints/1.0/responses/ip_lease'
       end
     end
   end
@@ -42,15 +65,25 @@ module Vnmgr
 
   module Models
     require 'json'
-    autoload :Base,     'vnmgr/models/base'
+    autoload :Base, 'vnmgr/models/base'
     autoload :Network, 'vnmgr/models/network'
-    autoload :Vif,      'vnmgr/models/vif'
+    autoload :Vif, 'vnmgr/models/vif'
     autoload :Taggable, 'vnmgr/models/base'
   end
 
   module ModelWrappers
     autoload :Base, 'vnmgr/model_wrappers/base'
     autoload :NetworkWrapper, 'vnmgr/model_wrappers/network_wrapper'
+    autoload :VifWrapper, 'vnmgr/model_wrappers/vif_wrapper'
+    autoload :DhcpRangeWrapper, 'vnmgr/model_wrappers/dhcp_range_wrapper'
+    autoload :MacRangeWrapper, 'vnmgr/model_wrappers/mac_range_wrapper'
+    autoload :RouterWrapper, 'vnmgr/model_wrappers/router_wrapper'
+    autoload :TunnelWrapper, 'vnmgr/model_wrappers/tunnel_wrapper'
+    autoload :DcNetworkWrapper, 'vnmgr/model_wrappers/dc_network_wrapper'
+    autoload :DatapathWrapper, 'vnmgr/model_wrappers/datapath_wrapper'
+    autoload :OpenFlowControllerWrapper, 'vnmgr/model_wrappers/open_flow_controller_wrapper'
+    autoload :IpAddressWrapper, 'vnmgr/model_wrappers/ip_address_wrapper'
+    autoload :IpLeaseWrapper, 'vnmgr/model_wrappers/ip_lease_wrapper'
   end
 
   require 'vnmgr/storage_backend'
