a037ec43129af837356308ce6057e468f06c31be
Metallion <akieckens@axsh.net>
2013-05-23 15:53:32 +0900

    Flush the package metadata before building a new package. Also added architecture to the packages.

--- deployment/packagebuild/build_package.sh
+++ deployment/packagebuild/build_package.sh
@@ -12,9 +12,21 @@ pkg_to_build=$1
 
 fpm_path=$vnet_path/ruby/bin/fpm
 
+# Resets all package metadata to present leftovers from a previously sourced package
+function flush_package_meta() {
+  pkg_name=""
+  pkg_desc=""
+  pkg_deps=""
+  pkg_arch=""
+  pkg_dirs=""
+  pkg_cfgs=""
+  pkg_owned_dirs=""
+}
+
 function build_package() {
   local pkg_meta_file=$1
 
+  flush_package_meta
   . ${whereami}/packages.d/$pkg_meta_file
 
   echo "building $pkg_format package: $pkg_name"
@@ -34,6 +46,7 @@ function build_package() {
     ${pkg_own_string} \
     --epoch $pkg_epoch \
     --description "${pkg_desc}" \
+    --architecture $pkg_arch \
     $pkg_dirs
 }
 
