8bdf304757fef145df084bee533a4ae31cfdda45
akry <a-yokokawa@axsh.net>
2013-05-23 15:44:44 +0900

    Added and modified configuration classes.

--- vnmgr/lib/vnmgr/configurations/dba.rb
+++ vnmgr/lib/vnmgr/configurations/dba.rb
@@ -3,12 +3,10 @@
 module Vnmgr
   module Configurations
     class Dba < Fuguta::Configuration
-      param :redis_host, :default => '127.0.0.1'
-      param :redis_port, :default => 6374
-      param :db_agent_ip, :default => '127.0.0.1'
-      param :db_agent_port, :default => '9001'
-      param :cluster_name, :default => 'vnmgr'
-      param :node_name, :default => 'db_agent'
+      param :cluster_name
+      param :node_name
+      param :ip
+      param :port
       param :db_uri
     end
   end
