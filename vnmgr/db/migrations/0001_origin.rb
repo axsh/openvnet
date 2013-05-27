8d9e1c4bb93e43b61ed9ff8e29f1aa5ee7c05871
Manabu Ejima <jemiam@gmail.com>
2013-05-27 15:55:02 +0900

    fix and refactor db migration

--- vnmgr/db/migrations/0001_origin.rb
+++ vnmgr/db/migrations/0001_origin.rb
@@ -2,169 +2,140 @@
 
 Sequel.migration do
   up do
-    create_table(:networks) do
-      primary_key :id, :type => 'int(11)'
-      column :uuid, 'varchar(255)', :null=>false
-      column :display_name, 'varchar(255)', :null=>false
-      column :ipv4_network, 'int unsigned', :null=>false
-      column :ipv4_prefix, 'int(5)', :default=>24, :null=>false
-      column :domain_name, 'varchar(255)'
-      column :dc_network_uuid, 'varchar(255)'
-      column :network_mode, 'varchar(255)'
-      column :editable, 'tinyint(1)'
-      column :created_at, 'datetime', :null=>false
-      column :updated_at, 'datetime', :null=>false
+    create_table(:dc_networks) do
+      primary_key :id
+      String :uuid, :unique => true, :null=>false
+      Integer :parent_id, :index => true
+      String :display_name, :null => false
+      DateTime :created_at, :null=>false
+      DateTime :updated_at, :null=>false
+    end
 
-      index [:uuid], :unique=>true
+    create_table(:networks) do
+      primary_key :id
+      String :uuid, :unique => true, :null=>false
+      String :display_name, :null=>false
+      Integer :ipv4_network, :null=>false
+      Integer :ipv4_prefix, :default=>24, :null=>false
+      String :domain_name
+      Integer :dc_network_id, :index => true
+      String :network_mode
+      FalseClass :editable
+      DateTime :created_at, :null=>false
+      DateTime :updated_at, :null=>false
+
       index [:ipv4_network, :ipv4_prefix]
     end
 
     create_table(:vifs) do
-      primary_key :id, :type => 'int(11)'
-      column :uuid, 'varchar(255)', :null=>false
-      column :network_id, 'int(11)'
-      column :mac_addr, 'bigint unsigned', :null=>false
-      column :state, 'varchar(255)', :null=>false
-      column :created_at, 'datetime', :null=>false
-      column :updated_at, 'datetime', :null=>false
-
-      index [:uuid], :unique=>true
+      primary_key :id
+      String :uuid, :unique => true, :null=>false
+      Integer :network_id, :index => true
+      Bignum :mac_addr, :null=>false
+      String :state, :null=>false
+      DateTime :created_at, :null=>false
+      DateTime :updated_at, :null=>false
     end
 
     create_table(:routers) do
-      primary_key :id, :type => 'int(11)'
-      column :uuid, 'varchar(255)', :null=>false
-      column :network_id, 'int(11)', :null=>false
-      column :ipv4_address, 'int unsigned', :null=>false
-      column :created_at, 'datetime', :null=>false
-      column :updated_at, 'datetime', :null=>false
-
-      index [:uuid], :unique=>true
+      primary_key :id
+      String :uuid, :unique => true, :null=>false
+      Integer :network_id, :index => true
+      Integer :ipv4_address, :null=>false
+      DateTime :created_at, :null=>false
+      DateTime :updated_at, :null=>false
     end
 
     create_table(:tunnels) do
-      primary_key :id, :type => 'int(11)'
-      column :uuid, 'varchar(255)', :null=>false
-      column :src_network_uuid, 'varchar(255)', :null=>false
-      column :dst_network_uuid, 'varchar(255)', :null=>false
-      column :tunnel_id, 'int(11)', :null=>false
-      column :ttl, 'datetime'
-      column :created_at, 'datetime', :null=>false
-      column :updated_at, 'datetime', :null=>false
+      primary_key :id
+      String :uuid, :unique => true, :null=>false
+      Integer :src_network_id, :index => true, :null => false
+      Integer :dst_network_id, :index => true, :null => false
+      Integer :tunnel_id, :index => true
+      DateTime :ttl
+      DateTime :created_at, :null=>false
+      DateTime :updated_at, :null=>false
 
-      index [:uuid], :unique=>true
-      index [:src_network_uuid, :dst_network_uuid]
-    end
-
-    create_table(:dc_networks) do
-      primary_key :id, :type => 'int(11)'
-      column :uuid, 'varchar(255)', :null=>false
-      column :parent_id, 'int(11)'
-      column :display_name, 'varchar(255)', :null=>false
-      column :created_at, 'datetime', :null=>false
-      column :updated_at, 'datetime', :null=>false
-
-      index [:uuid], :unique=>true
+      index [:src_network_id, :dst_network_id]
     end
 
     create_table(:dhcp_ranges) do
-      primary_key :id, :type => 'int(11)'
-      column :uuid, 'varchar(255)', :null=>false
-      column :range_begin, 'int(11) unsigned', :null=>false
-      column :range_end, 'int(11) unsigned', :null=>false
-      column :network_uuid, 'varchar(255)', :null=>false
-      column :created_at, 'datetime', :null=>false
-      column :updated_at, 'datetime', :null=>false
-
-      index [:uuid], :unique=>true
+      primary_key :id
+      String :uuid, :unique => true, :null=>false
+      Integer :range_begin, :null=>false
+      Integer :range_end, :null=>false
+      Integer :network_id, :index => true, :null => false
+      DateTime :created_at, :null=>false
+      DateTime :updated_at, :null=>false
     end
 
     create_table(:mac_ranges) do
-      primary_key :id, :type => 'int(11)'
-      column :uuid, 'varchar(255)', :null=>false
-      column :vendor_id, 'mediumint unsigned', :null=>false
-      column :range_begin, 'mediumint unsigned', :null=>false
-      column :range_end, 'mediumint unsigned', :null=>false
-      column :created_at, 'datetime', :null=>false
-      column :updated_at, 'datetime', :null=>false
-
-      index [:uuid], :unique=>true
+      primary_key :id
+      String :uuid, :unique => true, :null=>false
+      Integer :vendor_id, :null=>false
+      Integer :range_begin, :null=>false
+      Integer :range_end, :null=>false
+      DateTime :created_at, :null=>false
+      DateTime :updated_at, :null=>false
     end
 
     create_table(:mac_leases) do
-      primary_key :id, :type => 'int(11)'
-      column :uuid, 'varchar(255)', :null=>false
-      column :mac_addr, 'bigint unsigned', :null=>false
-      column :created_at, 'datetime', :null=>false
-      column :updated_at, 'datetime', :null=>false
-
-      index [:mac_addr], :unique=>true
-    end
-
-    create_table(:ip_leases) do
-      primary_key :id, :type => 'int(11)'
-      column :uuid, 'varchar(255)', :null=>false
-      column :network_uuid, 'varchar(255)', :null=>false
-      column :vif_uuid, 'varchar(255)', :null=>false
-      column :ip_address_uuid, 'varchar(255)', :null=>false
-      column :alloc_type, 'int'
-      column :created_at, 'datetime', :null=>false
-      column :updated_at, 'datetime', :null=>false
-      column :deleted_at, 'datetime', :null=>false
-      column :is_deleted, 'tinyint(1)', :null=>false
-
-      index [:uuid], :unique => true
-      index [:network_uuid]
-      index [:vif_uuid]
-      index [:ip_address_uuid]
+      primary_key :id
+      String :uuid, :unique => true, :null=>false
+      Bignum :mac_addr, :unique => true, :null=>false
+      DateTime :created_at, :null=>false
+      DateTime :updated_at, :null=>false
     end
 
     create_table(:ip_addresses) do
-      primary_key :id, :type => 'int(11)'
-      column :uuid, 'varchar(255)', :null=>false
-      column :ipv4_address, 'int unsigned', :null=>false
-      column :created_at, 'datetime', :null=>false
-      column :updated_at, 'datetime', :null=>false
+      primary_key :id
+      String :uuid, :unique => true, :null=>false
+      Integer :ipv4_address, :null=>false
+      DateTime :created_at, :null=>false
+      DateTime :updated_at, :null=>false
+    end
 
-      index [:uuid], :unique => true
+    create_table(:ip_leases) do
+      primary_key :id
+      String :uuid, :unique => true, :null=>false
+      Integer :network_id, :index => true, :null => false
+      Integer :vif_id, :index => true, :null => false
+      Integer :ip_address_id, :index => true, :null=>false
+      Integer :alloc_type
+      DateTime :created_at, :null=>false
+      DateTime :updated_at, :null=>false
+      DateTime :deleted_at, :null=>false
+      FalseClass :is_deleted, :null=>false
     end
 
     create_table(:network_services) do
-      primary_key :id, :type => 'int(11)'
-      column :uuid, 'varchar(255)', :null=>false
-      column :vif_uuid, 'varchar(255)'
-      column :display_name, 'varchar(255)', :null=>false
-      column :incoming_port, 'int(11)'
-      column :outgoing_port, 'int(11)'
-      column :created_at, 'datetime', :null=>false
-      column :updated_at, 'datetime', :null=>false
-
-      index [:uuid], :unique => true
-      index [:vif_uuid]
-      index [:display_name]
+      primary_key :id
+      String :uuid, :unique => true, :null=>false
+      Integer :vif_id, :index => true
+      String :display_name, :index => true, :null=>false
+      Integer :incoming_port
+      Integer :outgoing_port
+      DateTime :created_at, :null=>false
+      DateTime :updated_at, :null=>false
     end
 
     create_table(:open_flow_controllers) do
-      primary_key :id, :type => 'int(11)'
-      column :uuid, 'varchar(255)', :null=>false
-      column :created_at, 'datetime', :null=>false
-      column :updated_at, 'datetime', :null=>false
-
-      index [:uuid], :unique=>true
+      primary_key :id
+      String :uuid, :unique => true, :null=>false
+      DateTime :created_at, :null=>false
+      DateTime :updated_at, :null=>false
     end
 
     create_table(:datapaths) do
-      primary_key :id, :type => 'int(11)'
-      column :uuid, 'varchar(255)', :null=>false
-      column :open_flow_controller_uuid, 'varchar(255)', :null=>false
-      column :display_name, 'varchar(255)', :null=>false
-      column :ipv4_address, 'int unsigned'
-      column :is_connected, 'tinyint(1)', :null=>false
-      column :datapath_id, 'varchar(255)', :null=>false
-      column :created_at, 'datetime', :null=>false
-      column :updated_at, 'datetime', :null=>false
-
-      index [:uuid], :unique=>true
+      primary_key :id
+      String :uuid, :unique => true, :null=>false
+      Integer :open_flow_controller_id, :index => true, :null=>false
+      String :display_name, :null=>false
+      Integer :ipv4_address
+      FalseClass :is_connected, :null=>false
+      String :datapath_id, :null=>false
+      DateTime :created_at, :null=>false
+      DateTime :updated_at, :null=>false
     end
   end
 
