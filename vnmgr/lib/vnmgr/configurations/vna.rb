e453fcaf805191c09d9c7b8061d22e49f49bd412
Manabu Ejima <jemiam@gmail.com>
2013-05-31 00:55:46 +0900

    add vna.conf

--- vnmgr/lib/vnmgr/configurations/vna.rb
+++ vnmgr/lib/vnmgr/configurations/vna.rb
@@ -5,7 +5,7 @@ module Vnmgr::Configurations
   class Vna < Common
     param :ip
     param :port
-    param :node_name, :default => "vnmgr"
+    param :node_name, :default => "vna"
     param :dba_node_name, :default => "dba"
     param :dba_actor_name, :default => "dba"
     param :data_access_proxy, :default => :dba
