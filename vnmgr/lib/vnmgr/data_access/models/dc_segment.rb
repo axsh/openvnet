c47d30c8b5a044085b96edf0ed2a2c9e8e48ff0d
rakshasa <sundell.software@gmail.com>
2013-06-27 09:49:03 +0900

    Collect all data-access model definitions in a single file.

--- vnmgr/lib/vnmgr/data_access/models/dc_segment.rb
+++
@@ -1,5 +0,0 @@
-# -*- coding: utf-8 -*-
-module Vnmgr::DataAccess::Models
-  class DcSegment < Base
-  end
-end
--- vnmgr/lib/vnmgr/data_access/models/network_service.rb
+++
@@ -1,5 +0,0 @@
-# -*- coding: utf-8 -*-
-module Vnmgr::DataAccess::Models
-  class NetworkService < Base
-  end
-end
