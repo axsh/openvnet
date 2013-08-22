a0f2b2301bd6669ab8ac5491a9ead835b23ad9cd
Metallion <akieckens@axsh.net>
2013-08-22 17:23:24 +0900

    Added the rest of the options for creating datapaths

--- vnctl/lib/vnctl/cli/datapath.rb
+++ vnctl/lib/vnctl/cli/datapath.rb
@@ -12,13 +12,19 @@ module Vnctl::Cli
     }
     api_suffix :datapaths
 
-    desc "create [options]", "Creates a new datapath"
-    option :uuid, :type => :string, :desc => "UUID for the new datapath"
+    desc "create [options]", "Creates a new datapath."
+    option :uuid, :type => :string, :desc => "UUID for the new datapath."
+    option :open_flow_controller_uuid, :type => :string, :desc => "Openflow controller uuid for the new datapath."
     option :display_name, :type => :string, :desc => "Display name for the new datapath."
+    option :is_connected, :type => :boolean, :desc => "Flag that detemines if the new datapath is connected or not."
+    option :dc_segment_id, :type => :string, :desc => "The new datapath's dc segment id."
+    option :node_id, :type => :string, :desc => "The node id for the new datapath."
     option :ipv4_address, :type => :string, :desc => "Ipv4 address for the new datapath."
     option :datapath_id, :type => :string, :desc => "Hexadecimal id for the new datapath."
     def create
-      puts Vnctl::WebApi.post("/api/#{self.class.api_suffix}", :query => options).parsed_response
+      res = Vnctl::WebApi.post("/api/#{self.class.api_suffix}", :query => options)
+
+      puts res.parsed_response
     end
   end
 end
