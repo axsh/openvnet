57279a35603faf4a3c3be246fd692b7368f2568c
Metallion <akieckens@axsh.net>
2013-08-23 19:27:24 +0900

    Added a modify command to vnctl network

--- vnctl/lib/vnctl/cli/network.rb
+++ vnctl/lib/vnctl/cli/network.rb
@@ -5,15 +5,24 @@ module Vnctl::Cli
     namespace :network
     api_suffix "/api/networks"
 
+    no_tasks {
+      def self.add_modify_shared_options
+        option_display_name
+        option :ipv4_network, :type => :string, :desc => "IPv4 network address."
+        option :ipv4_prefix, :type => :numeric, :desc => "IPv4 mask size (1 < prefix < 32)."
+        option :domain_name, :type => :string, :desc => "DNS domain name."
+        option :dc_network_uuid, :type => :string, :desc => "Physical network uuid."
+        option :network_mode, :type => :string, :desc => "Can be either physical or virtual."
+        option :editable, :type => :boolean, :desc => "Flag that decides whether or not we can edit this network."
+      end
+    }
+
     option_uuid
-    option_display_name
-    option :ipv4_network, :type => :string, :desc => "IPv4 network address."
-    option :ipv4_prefix, :type => :numeric, :desc => "IPv4 mask size (1 < prefix < 32)."
-    option :domain_name, :type => :string, :desc => "DNS domain name."
-    option :dc_network_uuid, :type => :string, :desc => "Physical network uuid."
-    option :network_mode, :type => :string, :desc => "Can be either physical or virtual."
-    option :editable, :type => :boolean, :desc => "Flag that decides whether or not we can edit this network."
+    add_modify_shared_options
     define_add
+
+    add_modify_shared_options
+    define_modify
 
     define_show
     define_del
