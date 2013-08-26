442591a0f67245b3584c81c30d16722ca185d346
Metallion <akieckens@axsh.net>
2013-08-26 18:15:45 +0900

    Removed the openflow controller uuid option from vnctl datapath

--- vnctl/lib/vnctl/cli/datapath.rb
+++ vnctl/lib/vnctl/cli/datapath.rb
@@ -8,7 +8,6 @@ module Vnctl::Cli
     no_tasks {
       def self.add_modify_shared_options
         option_display_name
-        option :open_flow_controller_uuid, :type => :string, :desc => "Openflow controller uuid for the datapath."
         option :is_connected, :type => :boolean, :desc => "Flag that detemines if the datapath is connected or not."
         option :dc_segment_id, :type => :string, :desc => "The datapath's dc segment id."
         option :node_id, :type => :string, :desc => "The node id for the datapath."
