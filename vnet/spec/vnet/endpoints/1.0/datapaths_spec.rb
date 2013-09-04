7b1c1ef6521df6891dbe864762f1b2649e2c17d3
Metallion <akieckens@axsh.net>
2013-09-04 17:52:09 +0900

    Moved the relation examples into their own shared examples file

--- vnet/spec/vnet/endpoints/1.0/datapaths_spec.rb
+++ vnet/spec/vnet/endpoints/1.0/datapaths_spec.rb
@@ -49,63 +49,6 @@ describe "/datapaths" do
     include_examples "PUT /:uuid", accepted_params, uuid_params
   end
 
-  describe "POST /:uuid/networks/:network_uuid" do
-    let!(:related_object) { Fabricate(:network) }
-    let!(:base_object) { Fabricate(fabricator) }
-
-    before(:each) do
-      post api_relation_suffix, request_params
-    end
-
-    let(:request_params) { {:broadcast_mac_addr => "02:00:00:cc:00:02"} }
-
-    context "with a nonexistant uuid for the base object" do
-      let(:api_relation_suffix) {
-        "#{api_suffix}/#{model_class.uuid_prefix}-notfound/networks/#{related_object.canonical_uuid}"
-      }
-
-      it "should return a 404 error (UnknownUUIDResource)" do
-        last_response.should fail.with_code(404).with_error("UnknownUUIDResource",
-          "#{model_class.uuid_prefix}-notfound")
-      end
-    end
-
-    context "with a nonexistant uuid for the relation" do
-      let(:api_relation_suffix) {
-        "#{api_suffix}/#{base_object.canonical_uuid}/networks/#{related_object.uuid_prefix}-notfound"
-      }
-
-      it "should return a 404 error (UnknownUUIDResource)" do
-        last_response.should fail.with_code(404).with_error("UnknownUUIDResource",
-          "#{related_object.uuid_prefix}-notfound")
-      end
-    end
-
-    context "with faulty uuid syntax for the base object" do
-      let(:api_relation_suffix) {
-        "#{api_suffix}/this_is_not_an_uuid/networks/#{related_object.canonical_uuid}"
-      }
-
-      it_should_return_error(400, "InvalidUUID", "this_is_not_an_uuid")
-    end
-
-    context "with faulty uuid syntax for the related object" do
-      let(:api_relation_suffix) {
-        "#{api_suffix}/#{base_object.canonical_uuid}/networks/this_is_not_an_uuid"
-      }
-
-      it_should_return_error(400, "InvalidUUID", "this_is_not_an_uuid")
-    end
-
-    context "with a network_uuid that isn't added to this datapath yet" do
-      let(:api_relation_suffix) {
-        "#{api_suffix}/#{base_object.canonical_uuid}/networks/#{related_object.canonical_uuid}"
-      }
-
-      it "should succeed" do
-        last_response.should succeed
-      end
-    end
-  end
+  include_examples "many_to_many_relation", "networks"
 
 end
