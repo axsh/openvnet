65ee2aeb1f3cf916e9a617f45d7a103636c74627
Metallion <akieckens@axsh.net>
2013-09-06 18:33:01 +0900

    Updated ip lease endpoint, made a spec for it and got it green

--- vnet/lib/vnet/model_wrappers/ip_lease.rb
+++ vnet/lib/vnet/model_wrappers/ip_lease.rb
@@ -3,11 +3,14 @@
 module Vnet::ModelWrappers
   class IpLease < Base
     def to_hash
+      network = self.batch.network.commit
+      vif = self.batch.vif.commit
+      ip_address = self.batch.ip_address.commit
       {
         :uuid => self.uuid,
-        :network_uuid => self.network_uuid,
-        :vif_uuid => self.vif_uuid,
-        :ip_handle_uuid => self.ip_address_uuid,
+        :network_uuid => network && network.uuid,
+        :vif_uuid => vif && vif.uuid,
+        :ip_address_uuid => ip_address && ip_address.uuid,
         :alloc_type => self.alloc_type,
         :created_at => self.created_at,
         :updated_at => self.updated_at,
--- vnet/spec/fabricators/ip_lease_fabricator.rb
+++ vnet/spec/fabricators/ip_lease_fabricator.rb
@@ -1,12 +1,6 @@
 # -*- coding: utf-8 -*-
 Fabricator(:ip_lease, class_name: Vnet::Models::IpLease) do
-  #uuid
-  #network_id
-  #vif_id
-  #ip_address_id
-  #alloc_type
-  #created_at
-  #updated_at
-  #deleted_at
-  #is_deleted
+  network { Fabricate(:network) }
+  vif { Fabricate(:vif) }
+  ip_address { Fabricate(:ip_address) }
 end
