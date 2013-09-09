3224e5efa8680f6120af9c71a04f8e5319b316b9
Metallion <akieckens@axsh.net>
2013-09-09 13:00:33 +0900

    Re-enabled the post tests for the vifs endpoint

--- vnet/spec/vnet/endpoints/1.0/vifs_spec.rb
+++ vnet/spec/vnet/endpoints/1.0/vifs_spec.rb
@@ -17,25 +17,25 @@ describe "/vifs" do
   include_examples "GET /:uuid"
   include_examples "DELETE /:uuid"
 
-  # describe "POST /" do
-  #   before(:all) { use_mock_event_handler }
-  #   let!(:network) { Fabricate(:network) { uuid "nw-testnet" }  }
-  #   let!(:owner) { Fabricate(:datapath) { uuid "dp-owner" } }
-  #   let!(:active) { Fabricate(:datapath) { uuid "dp-active" } }
+  describe "POST /" do
+    before(:all) { use_mock_event_handler }
+    let!(:network) { Fabricate(:network) { uuid "nw-testnet" }  }
+    let!(:owner) { Fabricate(:datapath) { uuid "dp-owner" } }
+    let!(:active) { Fabricate(:datapath) { uuid "dp-active" } }
 
-  #   accepted_params = {
-  #     :uuid => "vif-test",
-  #     :network_uuid => "nw-testnet",
-  #     :mac_addr => "52:54:00:12:34:70",
-  #     :owner_datapath_uuid => "dp-owner",
-  #     :active_datapath_uuid => "dp-active",
-  #     :ipv4_address => "192.168.3.40",
-  #     :mode => "virtual"
-  #   }
-  #   required_params = [:mac_addr]
-  #   uuid_params = [:network_uuid, :owner_datapath_uuid]
+    accepted_params = {
+      :uuid => "vif-test",
+      :network_uuid => "nw-testnet",
+      :mac_addr => "52:54:00:12:34:70",
+      :owner_datapath_uuid => "dp-owner",
+      :active_datapath_uuid => "dp-active",
+      :ipv4_address => "192.168.3.40",
+      :mode => "virtual"
+    }
+    required_params = [:mac_addr]
+    uuid_params = [:network_uuid, :owner_datapath_uuid]
 
-  #   include_examples "POST /", accepted_params, required_params, uuid_params
+    include_examples "POST /", accepted_params, required_params, uuid_params
 
   #   it "create vif with ipv4_address" do
 
@@ -60,5 +60,5 @@ describe "/vifs" do
   #     events = MockEventHandler.handled_events
   #     expect(events.size).to eq 1
   #   end
-  # end
+  end
 end
