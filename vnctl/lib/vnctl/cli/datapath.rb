da8250f90fdbfb3d15adfbc3daf240a789c91b55
Metallion <akieckens@axsh.net>
2013-09-09 15:40:01 +0900

    Set required options for all vnctl commands

--- vnctl/lib/vnctl/cli/datapath.rb
+++ vnctl/lib/vnctl/cli/datapath.rb
@@ -9,10 +9,12 @@ module Vnctl::Cli
       option_display_name
       option :is_connected, :type => :boolean, :desc => "Flag that detemines if the datapath is connected or not."
       option :dc_segment_id, :type => :string, :desc => "The datapath's dc segment id."
-      option :node_id, :type => :string, :required => true, :desc => "The node id for the datapath."
+      option :node_id, :type => :string, :desc => "The node id for the datapath."
       option :ipv4_address, :type => :string, :desc => "Ipv4 address for the datapath."
-      option :dpid, :type => :string, :required => true, :desc => "Hexadecimal id for the datapath."
+      option :dpid, :type => :string, :desc => "Hexadecimal id for the datapath."
     }
+
+    add_required_options [:display_name, :dpid, :node_id]
 
     define_standard_crud_commands
 
--- vnctl/lib/vnctl/cli/route.rb
+++ vnctl/lib/vnctl/cli/route.rb
@@ -7,14 +7,14 @@ module Vnctl::Cli
 
     add_modify_shared_options {
       option :vif_uuid, :type => :string, :desc => "Vif uuid for this route."
-      option :route_link_uuid, :type => :string, :required => true,
-        :desc => "Route link uuid for this route."
-      option :ipv4_address, :type => :string, :required => true,
-        :desc => "IPv4 address for this route."
+      option :route_link_uuid, :type => :string,:desc => "Route link uuid for this route."
+      option :ipv4_address, :type => :string, :desc => "IPv4 address for this route."
       option :ipv4_prefix, :type => :numeric, :desc => "IPv4 prefix for this route."
       option :ingress, :type => :boolean, :desc => "Flag to determine if this is an ingress route."
       option :egress, :type => :boolean, :desc => "Flag to determine if this is an egress route."
     }
+
+    add_required_options [:ipv4_address, :route_link_uuid]
 
     define_standard_crud_commands
   end
