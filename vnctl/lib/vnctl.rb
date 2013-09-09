2c1e84ed77bcfaaf512f5fa361a1a9c3e45bbca8
Metallion <akieckens@axsh.net>
2013-09-09 15:57:19 +0900

    Added vnctl ip_address command

--- vnctl/lib/vnctl.rb
+++ vnctl/lib/vnctl.rb
@@ -13,6 +13,7 @@ module Vnctl
     autoload :Base, 'vnctl/cli/base'
     autoload :Datapath, 'vnctl/cli/datapath'
     autoload :DhcpRange, 'vnctl/cli/dhcp_range'
+    autoload :IpAddress, 'vnctl/cli/ip_address'
     autoload :Network, 'vnctl/cli/network'
     autoload :NetworkService, 'vnctl/cli/network_service'
     autoload :Root, 'vnctl/cli/root'
