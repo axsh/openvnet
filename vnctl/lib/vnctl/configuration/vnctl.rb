a759936f21c414a74ec9c42baf1157b081193482
Metallion <akieckens@axsh.net>
2013-09-10 15:26:39 +0900

    Allow the webapi uri and port for vnctl to be set in several config files and an environment variable

--- vnctl/lib/vnctl/configuration/vnctl.rb
+++ vnctl/lib/vnctl/configuration/vnctl.rb
@@ -4,9 +4,5 @@ module Vnctl::Configuration
   class Vnctl < Fuguta::Configuration
     param :webapi_uri, :default => '127.0.0.1'
     param :webapi_port, :default => 9090
-
-    def validate(errors)
-      #TODO: Make sure the ip and port are valid
-    end
   end
 end
