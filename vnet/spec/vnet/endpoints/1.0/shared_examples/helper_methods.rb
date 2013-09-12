0c390fca1013bacfb9812e24682551dd0df8db81
Metallion <akieckens@axsh.net>
2013-09-12 14:21:56 +0900

    Added tests for post networks/:uuid/dhcp_ranges

--- vnet/spec/vnet/endpoints/1.0/shared_examples/helper_methods.rb
+++ vnet/spec/vnet/endpoints/1.0/shared_examples/helper_methods.rb
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-def it_should_return_error(code, name, message)
+def it_should_return_error(code, name, message = nil)
   it "should return a #{code} error (#{name})" do
     last_response.should fail.with_code(code).with_error(name, message)
   end
