b23fbecc849bfdea4dd044f8532eb2ca21d1b962
Manabu Ejima <jemiam@gmail.com>
2013-10-01 22:19:11 +0900

    Remove network_uuid from parameters

--- vnet/spec/vnet/endpoints/1.0/ip_lease_spec.rb
+++ vnet/spec/vnet/endpoints/1.0/ip_lease_spec.rb
@@ -18,30 +18,31 @@ describe "/ip_leases" do
   include_examples "DELETE /:uuid"
 
   describe "POST /" do
-    let!(:network) { Fabricate(:network) { uuid "nw-test" } }
+    #let!(:network) { Fabricate(:network) { uuid "nw-test" } }
     let!(:vif) { Fabricate(:interface) { uuid "vif-test"} }
     let!(:ip_address) { Fabricate(:ip_address) { uuid "ia-test" } }
 
     accepted_params = {
       :uuid => "il-lease",
-      :network_uuid => "nw-test",
+      #:network_uuid => "nw-test",
       :vif_uuid => "vif-test",
       :ip_address_uuid => "ia-test",
       :alloc_type => 1
     }
-    required_params = [:network_uuid, :vif_uuid, :ip_address_uuid]
+    #required_params = [:network_uuid, :vif_uuid, :ip_address_uuid]
+    required_params = [:vif_uuid, :ip_address_uuid]
     uuid_params = [:uuid]
 
     include_examples "POST /", accepted_params, required_params, uuid_params
   end
 
   describe "PUT /:uuid" do
-    let!(:network) { Fabricate(:network) { uuid "nw-test2" } }
+    #let!(:network) { Fabricate(:network) { uuid "nw-test2" } }
     let!(:vif) { Fabricate(:interface) { uuid "vif-test2"} }
     let!(:ip_address) { Fabricate(:ip_address) { uuid "ia-test2" } }
 
     accepted_params = {
-      :network_uuid => "nw-test2",
+      #:network_uuid => "nw-test2",
       :vif_uuid => "vif-test2",
       :ip_address_uuid => "ia-test2",
       :alloc_type => 2
