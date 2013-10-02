b5fb7e8837e342d7f469326a74cb4453497e36a4
akry <a-yokokawa@axsh.net>
2013-10-02 20:15:55 +0900

    Accepts arp request from physical to virtual through vnet edge.

--- vnet/lib/vnet/openflow/ports/generic.rb
+++ vnet/lib/vnet/openflow/ports/generic.rb
@@ -13,20 +13,20 @@ module Vnet::Openflow::Ports
     end
 
     def install
+      @datapath.translation_manager.add_edge_port(port: self, update: true)
+
       flows = []
       flows << Vnet::Openflow::Flow.create(TABLE_CLASSIFIER, 2, {
                             :in_port => self.port_number
                            }, nil,
                            flow_options.merge(:goto_table => TABLE_VLAN_TRANSLATION))
 
-      if mac_addresses != []
-        mac_addresses.each do |mac|
-          flows << Vnet::Openflow::Flow.create(TABLE_VIRTUAL_DST, 80, {
-                                                :eth_dst => Trema::Mac.new(mac)
-                                               }, {
-                                                :output => self.port_number
-                                               }, flow_options)
-        end
+      mac_addresses.each do |mac|
+        flows << Vnet::Openflow::Flow.create(TABLE_VIRTUAL_DST, 80, {
+                                              :eth_dst => Trema::Mac.new(mac)
+                                             }, {
+                                              :output => self.port_number
+                                             }, flow_options)
       end
 
       self.datapath.add_flows(flows)
--- vnet/lib/vnet/openflow/translation_manager.rb
+++ vnet/lib/vnet/openflow/translation_manager.rb
@@ -8,37 +8,93 @@ module Vnet::Openflow
 
     def initialize(dp)
       @datapath = dp
+      @edge_ports = []
+    end
+
+    def add_edge_port(params)
+      @edge_ports << params[:port]
+      update if params[:update]
     end
 
     def update
       flows = []
-      graphs = Vnet::ModelWrappers::VlanTranslation.batch.all.commit
-      graphs.each do |g|
+      ovs_flows = []
+
+      @translation_map = Vnet::ModelWrappers::VlanTranslation.batch.all.commit
+
+      @edge_ports.each do |port|
+
+        flow_options = {:cookie => port.port_number | (COOKIE_PREFIX_PORT << COOKIE_PREFIX_SHIFT)}
+
+        flows << Flow.create(TABLE_CLASSIFIER, 2, {
+                              :in_port => port.port_number
+                             }, nil,
+                             flow_options.merge(:goto_table => TABLE_VLAN_TRANSLATION))
+
+        port.mac_addresses.each do |mac|
+          vlan_id = get_vlan_id_from_mac(mac)
+
+          if vlan_id.nil?
+            flows << Flow.create(TABLE_VIRTUAL_DST, 80, {
+                                                  :eth_dst => Trema::Mac.new(mac)
+                                                 }, {
+                                                  :output => port.port_number
+                                                 }, flow_options)
+          else
+            flows << Flow.create(TABLE_VIRTUAL_DST, 80, {
+                                                  :eth_dst => Trema::Mac.new(mac)
+                                                 }, {
+                                                  :vlan_vid => vlan_id,
+                                                  :output => port.port_number
+                                                 }, flow_options)
+          end
+        end
+      end
+
+      @translation_map.each do |t|
         metadata = nil
 
-        if g.network_id
-          metadata = md_create(:network => g.network_id)
+        if t.network_id
+          metadata = md_create(:network => t.network_id)
         else
-          raise("no network id found: #{g.inspect}")
+          raise("no network id found: #{t.inspect}")
         end
 
-        if g.vlan_id.nil?
-          if g.mac_address.nil?
+        if t.vlan_id.nil?
+          if t.mac_address.nil?
             raise("either mac_address of vlan_id is required.")
           else
             flows << Flow.create(TABLE_VLAN_TRANSLATION, 2, {
-                                  :eth_src => Trema::Mac.new(g.mac_address)
+                                  :eth_src => Trema::Mac.new(t.mac_address)
                                  }, nil, metadata.merge({:goto_table => TABLE_ROUTER_CLASSIFIER}))
           end
         else
-          # TODO refactor ":strip_vlan => true"
-          flows << Flow.create(TABLE_VLAN_TRANSLATION, 2, {
-                                :vlan_vid => g.vlan_id
-                               }, {:strip_vlan => true}, metadata.merge({:goto_table => TABLE_ROUTER_CLASSIFIER}))
+          if t.mac_address.nil?
+            #ovs_flows << "table=#{TABLE_VLAN_TRANSLATION},priority=2,cookie=0x%x,vlan_vid=%x,actions=learn\\(table=#{TABLE_VIRTUAL_DST},cookie=0x%x,priority=80,NXM_OF_ETH_DST\\[\\]=NXM_OF_ETH_SRC\\[\\],load:NXM_OF_VLAN_TCI\\[\\]\\-\\>NXM_OF_VLAN_TCI\\[\\],output:NXM_OF_IN_PORT\\[\\]),strip_vlan,goto_table:%d"
+
+            # TODO refactor ":strip_vlan => true"
+            flows << Flow.create(TABLE_VLAN_TRANSLATION, 2, {
+                                  :vlan_vid => t.vlan_id
+                                 }, {:strip_vlan => true}, metadata.merge({:goto_table => TABLE_ROUTER_CLASSIFIER}))
+          else
+            flows << Flow.create(TABLE_VLAN_TRANSLATION, 2, {
+                                  :eth_src => Trema::Mac.new(t.mac_address)
+                                 }, {:strip_vlan => true}, metadata.merge({:goto_table => TABLE_ROUTER_CLASSIFIER}))
+          end
         end
 
         @datapath.add_flows(flows)
       end
+    end
+
+    private
+
+    def get_vlan_id_from_mac(mac)
+      translation_map = @translation_map.detect { |t| t.mac_address == mac }
+
+      return nil if translation_map.nil?
+
+      translation_map.vlan_id
     end
   end
 end
