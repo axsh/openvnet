7ea6fdefcef7cef5b890d7f2d99ad006506477c0
Manabu Ejima <jemiam@gmail.com>
2013-10-02 19:49:10 +0900

    Fix event name

--- vnet/lib/vnet/event.rb
+++ vnet/lib/vnet/event.rb
@@ -2,6 +2,6 @@
 module Vnet
   module Event
     LeasedIpv4Address = :leased_ipv4_address
-    ReleasedIpv4Address = :leased_ipv4_address
+    ReleasedIpv4Address = :released_ipv4_address
   end
 end
