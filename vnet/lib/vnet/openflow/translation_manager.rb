98d3b0e0183627d6bedcb54d379784f97f731653
rakshasa <sundell.software@gmail.com>
2013-10-18 20:30:36 +0900

    Fixed select filter for translation manager.

--- vnet/lib/vnet/openflow/translation_manager.rb
+++ vnet/lib/vnet/openflow/translation_manager.rb
@@ -36,15 +36,39 @@ module Vnet::Openflow
       entry.network_id
     end
 
+    #
+    # Internal methods:
+    #
+
     private
 
     def log_format(message, values = nil)
       "#{@dpid_s} translation_manager: #{message}" + (values ? " (#{values})" : '')
     end
 
+    #
+    # Specialize Manager:
+    #
+
+    def select_filter_from_params(params)
+      case
+      when params[:id]   then {:id => params[:id]}
+      when params[:uuid] then params[:uuid]
+      when params[:display_name] && params[:owner_datapath_id]
+        { :display_name => params[:display_name],
+          :owner_datapath_id => params[:owner_datapath_id]
+        }
+      else
+        # Any invalid params that should cause an exception needs to
+        # be caught by the item_by_params_direct method.
+        return nil
+      end
+    end
+
     def update_translation_map
       @translation_map = Vnet::ModelWrappers::VlanTranslation.batch.all.commit
     end
+
   end
 
 end
