be3de120e985b32a0828c8ab143a0747e11ac535
Manabu Ejima <jemiam@gmail.com>
2013-10-23 14:35:49 +0900

    Add cookie_id to MacLease

--- vnet/lib/vnet/models/mac_lease.rb
+++ vnet/lib/vnet/models/mac_lease.rb
@@ -11,6 +11,10 @@ module Vnet::Models
     one_to_many :ip_leases
     plugin :association_dependencies, :ip_leases => :destroy
 
+    def cookie_id
+      self.class.with_deleted.where(interface_id: self.interface_id).where("id <= #{self.id}").count
+    end
+
     def to_hash
       super.merge({
         :mac_address => self.mac_address
