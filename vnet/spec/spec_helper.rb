0741f9b4f9cd516334d8950ee4354c9a36a56ab7
Manabu Ejima <jemiam@gmail.com>
2013-10-31 17:28:53 +0900

    Update spec_helper

--- vnet/spec/spec_helper.rb
+++ vnet/spec/spec_helper.rb
@@ -26,11 +26,11 @@ RSpec.configure do |config|
 
   Vnet::Configurations::Common.paths = ["#{File.dirname(File.expand_path(__FILE__))}/config"]
 
-  #logfile = File.open(File.expand_path("../log/spec.log", __FILE__), 'a')
-  #logfile.sync = true
-  #Celluloid.logger = Logger.new(logfile)
-  Celluloid.logger = nil
-  #Celluloid.shutdown_timeout = 1
+  logfile = File.open(File.expand_path("../log/spec.log", __FILE__), 'a')
+  logfile.sync = true
+  Celluloid.logger = Logger.new(logfile)
+  #Celluloid.logger = nil
+  Celluloid.shutdown_timeout = 1
 
   vnmgr_conf = Vnet::Configurations::Vnmgr.load
   webapi_conf = Vnet::Configurations::Webapi.load
@@ -40,18 +40,18 @@ RSpec.configure do |config|
   #Vnet::Initializers::DB.run(vnmgr_conf.db_uri, :debug_sql => true)
 
   config.before(:suite) do
-    Celluloid.shutdown
     DatabaseCleaner.strategy = :truncation
     DatabaseCleaner.clean_with(:truncation)
-    Celluloid.boot
   end
 
   config.before(:each) do
     DatabaseCleaner.start
     Fabrication.clear_definitions
+    Celluloid.boot
   end
 
   config.after(:each) do
+    Celluloid.shutdown
     DatabaseCleaner.clean
   end
 end
