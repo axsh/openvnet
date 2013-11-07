ef462a0a86c0cfbc3a030e9b334d6eaaa12cd112
akry <a-yokokawa@axsh.net>
2013-11-07 20:53:32 +0900

    Used openflow and removed embedded magic numbers.

--- vnet/lib/vnet/openflow/translations/vnet_edge_handler.rb
+++ vnet/lib/vnet/openflow/translations/vnet_edge_handler.rb
@@ -53,37 +53,77 @@ module Vnet::Openflow::Translations
 
       src_network_id = @dp_info.network_manager.network_id_by_mac(src_mac.value)
 
-      return if src_network_id.nil?
+      if src_network_id.nil?
+        error log_format("no corresponded translation entry has been found", "in_port: #{in_port}, src: #{src_mac}, dst: #{dst_mac}")
+        return nil
+      end
 
       vlan_vids = @dp_info.translation_manager.network_to_vlan(src_network_id)
 
-      return if vlan_vids.nil?
+      if vlan_vids.nil?
+        error log_format("blank vlan_vid", "in_port: #{in_port}, src: #{src_mac}, dst: #{dst_mac}")
+        return nil
+      else
+        info log_format("vlan_id found", vlan_vids)
+      end
 
-      flows << "table=#{TABLE_EDGE_SRC},priority=2,dl_src=#{src_mac},actions=write_metadata:0x%x,goto_table=#{TABLE_EDGE_DST}" % METADATA_TYPE_VIRTUAL_TO_EDGE
+      edge_port = @dp_info.port_manager.item(port_type: :generic, reinitialize: false, dynamic_load: false)
+
+      if edge_port.nil?
+        error log_format("Edge ports have not been found.", "in_port: #{in_port}, src: #{src_mac}, dst: #{dst_mac}")
+        return nil
+      else
+        info log_format("edge_port found", edge_port)
+      end
 
       case vlan_vids
       when Array
-        actions=""
+        actions=[]
         vlan_vids.each do |vlan_vid|
-          actions << "mod_vlan_vid:#{vlan_vid},output:2,"
+          actions << {:mod_vlan_vid => vlan_vid, :output => edge_port[:port_number]}
         end
-        actions.chop!
       else
-        actions = "mod_vlan_vid:#{vlan_vids},output:2"
+        actions = {:mod_vlan_vid => vlan_vids, :output => edge_port[:port_number]}
       end
 
       dpn = MW::DatapathNetwork.batch.on_specific_datapath(@dp_info.datapath.datapath_map).all.commit.select { |t| t.network_id == src_network_id }
       dpn_broadcast = dpn.first.broadcast_mac_address
 
+      flows << Flow.create(TABLE_EDGE_SRC, 2, {
+                           :eth_src => src_mac
+                          }, {}, {
+                           :goto_table => TABLE_EDGE_DST,
+                           :metadata => METADATA_TYPE_VIRTUAL_TO_EDGE,
+                           :metadata_mask => METADATA_TYPE_MASK
+                          })
+
       if dst_mac.broadcast?
-        flows << "table=#{TABLE_EDGE_DST},priority=2,arp,dl_dst=ff:ff:ff:ff:ff:ff,metadata=0x%x/0x%x,actions=#{actions}" % [ METADATA_TYPE_VIRTUAL_TO_EDGE, METADATA_TYPE_MASK ]
+        flows << Flow.create(TABLE_EDGE_DST, 2, {
+                             :eth_type => 0x0806,
+                             :eth_dst => MAC_BROADCAST,
+                             :metadata => METADATA_TYPE_VIRTUAL_TO_EDGE,
+                             :metadata_mask => METADATA_TYPE_MASK
+                            }, actions, {})
       elsif dpn_broadcast == dst_mac.value
-        flows << "table=#{TABLE_EDGE_DST},priority=2,arp,dl_dst=#{dst_mac},metadata=0x%x/0x%x,actions=mod_dl_dst:ff:ff:ff:ff:ff:ff,#{actions}" % [ METADATA_TYPE_VIRTUAL_TO_EDGE, METADATA_TYPE_MASK ]
+        flows << Flow.create(TABLE_EDGE_DST, 2, {
+                             :eth_type => 0x0806,
+                             :eth_dst => dst_mac,
+                             :metadata => METADATA_TYPE_VIRTUAL_TO_EDGE,
+                             :metadata_mask => METADATA_TYPE_MASK
+                            }, {:eth_dst => MAC_BROADCAST}.merge(actions), {})
       else
-        flows << "table=#{TABLE_EDGE_DST},priority=2,dl_dst=#{src_mac},metadata=0x%x/0x%x,actions=output:1" %  [ METADATA_TYPE_EDGE_TO_VIRTUAL, METADATA_TYPE_MASK ]
+        flows << Flow.create(TABLE_EDGE_DST, 2, {
+                             :eth_dst => src_mac,
+                             :metadata => METADATA_TYPE_EDGE_TO_VIRTUAL,
+                             :metadata_mask => METADATA_TYPE_MASK
+                            }, {
+                             :output => message.in_port
+                            }, {})
       end
 
-      flows.each { |flow| @dp_info.add_ovs_flow(flow) }
+      @dp_info.add_flows(flows)
+
+      @dp_info.send_packet_out(message, OFPP_TABLE)
     end
 
     def handle_packet_from_edge_port(message)
