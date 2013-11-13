38bc4cfb10ca6fa45290f5dd33225c751a740d23
akry <akira.yokokawa@gmail.com>
2013-11-13 16:34:19 +0900

    Added vxlan and gre specs.

--- lib/vnspec/spec/gre_spec.rb
+++ lib/vnspec/spec/gre_spec.rb
@@ -1,27 +1,27 @@
 # -*- coding: utf-8 -*-
 require_relative "spec_helper"
 
-describe "simple" do
+describe "gre" do
   describe "vnet1" do
-    context "mac2mac" do
+    context "mac2mac over gre tunnel" do
       it "reachable to vnet1" do
-        expect(vm1).to be_reachable_to(vm3)
+        expect(vm1).to be_reachable_to(vm5)
       end
 
       it "not reachable to vnet2" do
-        expect(vm1).not_to be_reachable_to(vm4)
+        expect(vm1).not_to be_reachable_to(vm6)
       end
     end
   end
 
   describe "vnet2" do
-    context "mac2mac" do
+    context "mac2mac over gre tunnel" do
       it "reachable to vnet2" do
-        expect(vm2).to be_reachable_to(vm4)
+        expect(vm2).to be_reachable_to(vm6)
       end
 
       it "not reachable to vnet1" do
-        expect(vm2).not_to be_reachable_to(vm3)
+        expect(vm2).not_to be_reachable_to(vm5)
       end
     end
   end
