da3efaee5a87a7986672b55982f43a8ca9e98f55
akry <a-yokokawa@axsh.net>
2013-11-13 19:22:23 +0900

    gre_spec is integrated into simple_spec.

--- lib/vnspec/spec/simple_spec.rb
+++ lib/vnspec/spec/simple_spec.rb
@@ -12,6 +12,16 @@ describe "simple" do
         expect(vm1).not_to be_reachable_to(vm4)
       end
     end
+
+    context "mac2mac over gre tunnel" do
+      it "reachable to vnet1" do
+        expect(vm1).to be_reachable_to(vm5)
+      end
+
+      it "not reachable to vnet2" do
+        expect(vm1).not_to be_reachable_to(vm6)
+      end
+    end
   end
 
   describe "vnet2" do
@@ -22,6 +32,16 @@ describe "simple" do
 
       it "not reachable to vnet1" do
         expect(vm2).not_to be_reachable_to(vm3)
+      end
+    end
+
+    context "mac2mac over gre tunnel" do
+      it "reachable to vnet2" do
+        expect(vm2).to be_reachable_to(vm6)
+      end
+
+      it "not reachable to vnet1" do
+        expect(vm2).not_to be_reachable_to(vm5)
       end
     end
   end
