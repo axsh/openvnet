8126279f9b495a7979ac3e6cb87b0949416c663d
Metallion <akieckens@axsh.net>
2013-11-13 22:16:40 +0900

    Added an output_format option for vnctl's configuration file. Default is 'yml'

--- vnctl/lib/vnctl/configuration/vnctl.rb
+++ vnctl/lib/vnctl/configuration/vnctl.rb
@@ -4,5 +4,14 @@ module Vnctl::Configuration
   class Vnctl < Fuguta::Configuration
     param :webapi_uri, :default => '127.0.0.1'
     param :webapi_port, :default => 9090
+    param :output_format, :default => 'yml'
+
+    def validate(errors)
+      output_formats = ['yml', 'json']
+      unless output_formats.member?(output_format)
+        errors << "\"%s\" is not a valid output_format. Must be one of %s" %
+          [output_format, output_formats.inspect]
+      end
+    end
   end
 end
