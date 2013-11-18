a61dac4eac4f538748d9802586795956154e952a
Manabu Ejima <jemiam@gmail.com>
2013-11-18 19:34:39 +0900

    Add datapath node_api

---
+++ vnet/lib/vnet/node_api/datapath.rb
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+module Vnet::NodeApi
+  class Datapath < Base
+    class << self
+      def create(options)
+        super.tap do |datapath|
+          dispatch_event(ADDED_DATAPATH, :id => datapath.id)
+        end
+      end
+
+      def destroy(uuid)
+        super.tap do |datapath|
+          dispatch_event(REMOVED_DATAPATH, :id => datapath.id)
+        end
+      end
+    end
+  end
+end
