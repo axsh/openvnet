2d3222b606dd18232998d9bb398abcc65f343618
akry <a-yokokawa@axsh.net>
2013-11-18 20:27:27 +0900

    Added reachable_to? method

--- lib/vnspec/legacy.rb
+++ lib/vnspec/legacy.rb
@@ -4,7 +4,7 @@ module Vnspec
     include Config
     include SSH
     include Logger
-    
+
     class << self
       include Config
       include Logger
@@ -21,8 +21,23 @@ module Vnspec
       alias :[] :find
     end
 
+    attr_reader :name
+    attr_reader :ip
+
     def initialize(name)
-      @name = name
+      @name = name.to_sym
+      @ip = config[:legacy][name][:ssh_ip]
+    end
+
+    def reachable_to?(vm, timeout = 30)
+      options = to_ssh_option_string(
+        "StrictHostKeyChecking" => "no",
+        "UserKnownHostsFile" => "/dev/null",
+        "LogLevel" => "ERROR",
+        "ConnectTimeout" => timeout
+      )
+      ret = ssh(ip, "ssh #{options} #{config[:ssh_user]}@#{vm.ipv4_address} hostname", {})
+      ret == vm.name.to_s
     end
   end
 end
