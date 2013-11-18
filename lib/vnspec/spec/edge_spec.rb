a10e5ca4c291e8ec764f47b559f86352949858f2
akry <a-yokokawa@axsh.net>
2013-11-18 19:24:27 +0900

    Added edge_spec

---
+++ lib/vnspec/spec/edge_spec.rb
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+require_relative "spec_helper"
+
+describe "edge" do
+  describe "mac2mac" do
+    it "reachable to vnet1" do
+      expect(legacy1).to be_reachable_to(vm1)
+    end
+
+    it "not reachable to vnet2" do
+      expect(legacy1).not_to be_reachable_to(vm2)
+    end
+
+    # context "tunnel" do
+    #   it "reachable to vnet1" do
+    #     expect(vm1).to be_reachable_to(vm5)
+    #   end
+
+    #   it "not reachable to vnet2" do
+    #     expect(vm1).not_to be_reachable_to(vm6)
+    #   end
+    # end
+  end
+end
