d6dde105765341a1b2d974078762cdae24afebb9
Manabu Ejima <jemiam@gmail.com>
2013-11-20 17:32:39 +0900

    Datapath event should be handled by the node that it belongs to

--- vnet/lib/vnet/node_api/datapath.rb
+++ vnet/lib/vnet/node_api/datapath.rb
@@ -5,13 +5,13 @@ module Vnet::NodeApi
     class << self
       def create(options)
         super.tap do |datapath|
-          dispatch_event(ADDED_DATAPATH, :id => datapath.id)
+          dispatch_event(ADDED_DATAPATH, id: datapath.id)
         end
       end
 
       def destroy(uuid)
         super.tap do |datapath|
-          dispatch_event(REMOVED_DATAPATH, :id => datapath.id)
+          dispatch_event(REMOVED_DATAPATH, id: datapath.id, node_id: datapath.node_id)
         end
       end
     end
