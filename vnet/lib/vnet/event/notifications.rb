5660b2ba7fed548d97d936a491a2069ae13cec56
Manabu Ejima <jemiam@gmail.com>
2013-11-22 18:37:07 +0900

    Update Notifications#handle_event to handle events without id

--- vnet/lib/vnet/event/notifications.rb
+++ vnet/lib/vnet/event/notifications.rb
@@ -36,12 +36,13 @@ module Vnet::Event::Notifications
 
   def handle_event(event_name, params)
     #debug "handle event: #{event_name} params: #{params.inspect}}"
-    event_queue = (@event_queues[params[:id]] || []).dup
+    queue_id = queue_id || :defualt
+    event_queue = (@event_queues[queue_id] || []).dup
     event_queue << { event_name: event_name, params: params.dup }
-    @event_queues[params[:id]] = event_queue
-    unless @queue_statuses[params[:id]]
-      @queue_statuses[params[:id]] = true
-      async(:fetch_queued_events, params[:id])
+    @event_queues[queue_id] = event_queue
+    unless @queue_statuses[queue_id]
+      @queue_statuses[queue_id] = true
+      async(:fetch_queued_events, queue_id)
     end
   end
 
