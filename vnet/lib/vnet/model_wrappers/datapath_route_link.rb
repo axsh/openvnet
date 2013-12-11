649389956f953dba8202e866de95d03fbe1f023f
rakshasa <sundell.software@gmail.com>
2013-12-12 05:20:48 +0900

    Removed commented out method.

--- vnet/lib/vnet/model_wrappers/datapath_route_link.rb
+++ vnet/lib/vnet/model_wrappers/datapath_route_link.rb
@@ -2,11 +2,5 @@
 
 module Vnet::ModelWrappers
   class DatapathRouteLink < Base
-    # def to_hash
-    #   {
-    #     :datapath_uuid => self.datapath_uuid,
-    #     :route_link_uuid => self.route_link_uuid,
-    #   }
-    # end
   end
 end
