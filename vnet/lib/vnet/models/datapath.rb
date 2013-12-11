70742af01844b356e058e1c7dcff805067c03ac1
rakshasa <sundell.software@gmail.com>
2013-12-12 06:07:12 +0900

    Moved on_*_segment methods to dataset_module.

--- vnet/lib/vnet/models/datapath.rb
+++ vnet/lib/vnet/models/datapath.rb
@@ -21,15 +21,17 @@ module Vnet::Models
 
     subset(:alives, {})
 
-    one_to_many :on_other_segments, :class => Datapath do |ds|
-      Datapath.where(~{:id => self.id} & ~{:dc_segment_id => self.dc_segment_id})
-    end
-
-    one_to_many :on_same_segment, :class => Datapath do |ds|
-      Datapath.where(~{:id => self.id} & {:dc_segment_id => self.dc_segment_id})
-    end
-
     dataset_module do
+      def on_other_segments(datapath_id)
+        dc_segment_id = Datapath.where(id: datapath_id).select(:dc_segment_id)
+        Datapath.where(~{id: datapath_id} & ~{dc_segment_id: dc_segment_id})
+      end
+
+      def on_same_segment(datapath_id)
+        dc_segment_id = Datapath.where(id: datapath_id).select(:dc_segment_id)
+        Datapath.where(~{id: datapath_id} & {dc_segment_id: dc_segment_id})
+      end
+
       def find_all_by_network_id(network_id)
         left_join(:datapath_networks, :datapath_id => :id).where(:datapath_networks__network_id => network_id).all
       end
