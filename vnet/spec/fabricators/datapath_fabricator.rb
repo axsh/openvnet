d0c2eb8bd059b0ba94d95fe8f671925192239715
Metallion <akieckens@axsh.net>
2014-01-09 15:48:34 +0900

    Use the new numeric dpid in the datapath fabricators

--- vnet/spec/fabricators/datapath_fabricator.rb
+++ vnet/spec/fabricators/datapath_fabricator.rb
@@ -8,7 +8,7 @@ Fabricator(:datapath_1, class_name: Vnet::Models::Datapath) do
   uuid 'dp-test1'
   display_name "test-datapath1"
   ipv4_address IPAddr.new("192.168.1.1").to_i
-  dpid "0x#{'a' * 16}"
+  dpid 0xaaaaaaaaaaaaaaaa
   dc_segment_id 1
   node_id 'vna'
   is_connected true
@@ -18,7 +18,7 @@ Fabricator(:datapath_2, class_name: Vnet::Models::Datapath) do
   uuid 'dp-test2'
   display_name "test-datapath2"
   ipv4_address IPAddr.new("192.168.1.2").to_i
-  dpid "0x#{'b' * 16}"
+  dpid 0xbbbbbbbbbbbbbbbb
   dc_segment_id 1
   node_id 'vna2'
   is_connected true
@@ -28,7 +28,7 @@ Fabricator(:datapath_3, class_name: Vnet::Models::Datapath) do
   uuid 'dp-test3'
   display_name "test-datapath3"
   ipv4_address IPAddr.new("192.168.2.2").to_i
-  dpid "0x#{'c' * 16}"
+  dpid 0xcccccccccccccccc
   dc_segment_id 2
   node_id 'vna3'
   is_connected true
