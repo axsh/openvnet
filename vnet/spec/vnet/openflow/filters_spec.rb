65115164fb596eba1f8232ab32648efed0ac61f2
Metallion <akieckens@axsh.net>
2014-01-30 17:40:52 +0900

    Added unit tests for enabling and disabling ingress filtering

--- vnet/spec/vnet/openflow/filters_spec.rb
+++ vnet/spec/vnet/openflow/filters_spec.rb
@@ -62,25 +62,6 @@ describe Vnet::Openflow::FilterManager do
       end
     end
 
-    #
-    # This probably won't be the final behaviour anyway...
-    #
-
-    # context "with an interface that has no security groups" do
-    #   let(:interface) { Fabricate(:interface) }
-
-    #   F = Vnet::Openflow::Filters
-    #   it "applies a flow that accepts all traffic on the interface" do
-    #     expect(flows).to include flow_create(:default,
-    #       table: TABLE_INTERFACE_INGRESS_FILTER,
-    #       priority: Vnet::Openflow::Filters::Rule::PRIORITY,
-    #       cookie: F::AcceptAllTraffic.cookie(interface.id),
-    #       match_metadata: {interface: interface.id},
-    #       goto_table: TABLE_OUT_PORT_INTERFACE_INGRESS
-    #     )
-    #   end
-    # end
-
     context "with an interface that's in two security groups" do
       let(:group1) do
         rules = "tcp:22:0.0.0.0/0\nudp:52:10.1.0.1/24"
@@ -204,5 +185,51 @@ describe Vnet::Openflow::FilterManager do
         )
       end
     end
+  end
+
+  describe "#disabled_filtering" do
+      let(:group) { Fabricate(:security_group, rules: "icmp:-1:0.0.0.0/0") }
+      let(:interface) do
+        Fabricate(:interface, owner_datapath_id: 1, enable_ingress_filtering: true).tap { |i|
+          i.add_security_group(group)
+        }
+      end
+
+      context "with the id of an interface in params" do
+        before(:each) do
+          subject.initialized_interface({item_map: wrapper(interface)})
+          subject.disabled_filtering({id: interface.id})
+        end
+
+        it "removes all security_group rules for the interface" do
+          expect(flows).not_to include rule_flow(
+            cookie: cookie_id(group),
+            match: match_icmp_rule("0.0.0.0/0")
+          )
+        end
+      end
+  end
+
+  describe "#enabled_filtering" do
+      let(:group) { Fabricate(:security_group, rules: "icmp:-1:0.0.0.0/0") }
+      let(:interface) do
+        Fabricate(:interface, owner_datapath_id: 1, enable_ingress_filtering: false).tap { |i|
+          i.add_security_group(group)
+        }
+      end
+
+      context "with the id of an interface in params" do
+        before(:each) do
+          subject.initialized_interface({item_map: wrapper(interface)})
+          subject.enabled_filtering({id: interface.id})
+        end
+
+        it "applies all security_group rules for the interface" do
+          expect(flows).to include rule_flow(
+            cookie: cookie_id(group),
+            match: match_icmp_rule("0.0.0.0/0")
+          )
+        end
+      end
   end
 end
