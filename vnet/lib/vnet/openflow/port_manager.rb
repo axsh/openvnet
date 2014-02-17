6f40ff7d2637babaee5d40cd99d528ee9abb880d
rakshasa <sundell.software@gmail.com>
2014-02-17 19:00:18 +0900

    Cleaned out cruft from port manager that is no longer needed due to the use of port_name.

--- vnet/lib/vnet/openflow/port_manager.rb
+++ vnet/lib/vnet/openflow/port_manager.rb
@@ -80,6 +80,11 @@ module Vnet::Openflow
       return unless port
       return if port.installed?
 
+      if port.port_number == OFPP_LOCAL
+        prepare_port_local(port)
+        return item_to_hash(port)
+      end
+
       interface = @dp_info.interface_manager.item(port_name: port.port_name,
                                                   port_number: port.port_number,
                                                   owner_datapath_id: @dp_info.datapath.datapath_info.id)
@@ -102,10 +107,6 @@ module Vnet::Openflow
         end
       else
         case
-        when port.port_number == OFPP_LOCAL
-          prepare_port_local(port)
-        when port.port_info.name =~ /^if-/
-          prepare_port_vif(port)
         when port.port_info.name =~ /^t-/
           prepare_port_tunnel(port)
         else
@@ -170,8 +171,13 @@ module Vnet::Openflow
       port.install
     end
 
-    def prepare_port_eth(port, interface = nil)
+    def prepare_port_eth(port, interface)
       @dp_info.ovs_ofctl.mod_port(port.port_number, :flood)
+
+      if interface.nil?
+        error log_format("could not find interface for #{port.port_name}")
+        return
+      end
 
       params = {
         :owner_datapath_id => @dp_info.datapath.datapath_info.id,
@@ -179,48 +185,31 @@ module Vnet::Openflow
         :reinitialize => true
       }
 
-      interface = interface || @dp_info.interface_manager.item(params)
+      if interface.mode == :host
+        port.extend(Ports::Host)
+        port.interface_id = interface.id
 
-      if interface
-        if interface.mode == :host
-          port.extend(Ports::Host)
-          port.interface_id = interface.id
+        # We don't need to query the interface before updating it, so do
+        # this directly instead of the item request.
+        interface = @dp_info.interface_manager.update_item(event: :set_port_number,
+                                                           id: interface.id,
+                                                           port_number: port.port_number)
+      elsif interface.mode == :edge
+        port.extend(Ports::Generic)
+        port.interface_id = interface.id
 
-          # We don't need to query the interface before updating it, so do
-          # this directly instead of the item request.
-          interface = @dp_info.interface_manager.update_item(event: :set_port_number,
-                                                             id: interface.id,
-                                                             port_number: port.port_number)
-        elsif interface.mode == :edge
-          port.extend(Ports::Generic)
-          port.interface_id = interface.id
-
-        else
-          error log_format("unknown port type", interface.mode)
-        end
+      else
+        error log_format("unknown port type", interface.mode)
       end
 
       port.install
     end
 
-    def prepare_port_vif(port, interface = nil)
-      # TODO: Fix this so that when interface manager creates a new
-      # interface, it checks if the port is present and get the
-      # port number from port manager.
-      if interface.nil?
-        @dp_info.ovs_ofctl.mod_port(port.port_number, :no_flood)
-
-        interface = @dp_info.interface_manager.item(uuid: port.port_name,
-                                                    port_number: port.port_number)
-      end
+    def prepare_port_vif(port, interface)
+      @dp_info.ovs_ofctl.mod_port(port.port_number, :no_flood)
 
       if interface.nil?
         error log_format("could not find interface for #{port.port_name}")
-        return
-      end
-
-      if interface.mode != :vif
-        info log_format('interface mode not set to \'vif\' for #{interface.uuid}', "mode:#{interface.mode}")
         return
       end
 
