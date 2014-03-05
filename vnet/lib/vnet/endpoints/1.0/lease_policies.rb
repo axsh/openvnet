617114a258b3fd1e55bf76a0d434c4d21d1d5196
Richard Potter <potter@axsh.net>
2014-03-05 16:18:38 +0900

    Dealt with lease_policy webapi TODO items

--- vnet/lib/vnet/endpoints/1.0/lease_policies.rb
+++ vnet/lib/vnet/endpoints/1.0/lease_policies.rb
@@ -1,17 +1,15 @@
 # -*- coding: utf-8 -*-
 
 Vnet::Endpoints::V10::VnetAPI.namespace '/lease_policies' do
-  put_post_shared_params = ["interface_uuid", "mac_address"]  # TODO
+  put_post_shared_params = ["mode"]
 
-  fill_options = [:interface, :mac_address]  # TODO
+  fill_options = [ ]
 
   post do
     accepted_params = put_post_shared_params + ["uuid"]
-    required_params = ["interface_uuid", "mac_address"] # TODO
+    required_params = ["mode"]
 
     post_new(:LeasePolicy, accepted_params, required_params, fill_options) { |params|
-      check_syntax_and_get_id(M::Interface, params, "interface_uuid", "interface_id") # TODO
-      params['mac_address'] = parse_mac(params['mac_address']) # TODO
     }
   end
 
@@ -29,8 +27,7 @@ Vnet::Endpoints::V10::VnetAPI.namespace '/lease_policies' do
 
   put '/:uuid' do
     update_by_uuid(:LeasePolicy, put_post_shared_params, fill_options) { |params|
-      check_syntax_and_get_id(M::Interface, params, "interface_uuid", "interface_id") if params["interface_uuid"]
-      params['mac_address'] = parse_mac(params['mac_address']) if params['mac_address'] # TODO
+
     }
   end
 end
--- vnet/lib/vnet/endpoints/1.0/responses/lease_policy.rb
+++ vnet/lib/vnet/endpoints/1.0/responses/lease_policy.rb
@@ -4,8 +4,6 @@ module Vnet::Endpoints::V10::Responses
   class LeasePolicy < Vnet::Endpoints::ResponseGenerator
     def self.generate(object)
       argument_type_check(object,Vnet::ModelWrappers::LeasePolicy)
-      object.interface_uuid ||= object.interface.uuid
-      # object.mac_address = object.mac_address_s ...TODO: understand this line
       object.to_hash
     end
   end
