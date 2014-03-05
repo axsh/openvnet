1470215f043ebfb738d68d592339b70a3b6ed38c
Richard Potter <potter@axsh.net>
2014-03-05 16:18:38 +0900

    Dealt with lease_policy webapi TODO items

--- vnet/lib/vnet/models/lease_policy.rb
+++ vnet/lib/vnet/models/lease_policy.rb
@@ -1,24 +1,10 @@
 # -*- coding: utf-8 -*-
-# TODO: redo this whole file
+
 module Vnet::Models
   class LeasePolicy < Base
-    taggable 'ml'
+    taggable 'lp'
 
     plugin :paranoia
-    plugin :mac_address
 
-    many_to_one :interface
-    one_to_many :ip_leases
-    plugin :association_dependencies, ip_leases: :destroy
-
-    def cookie_id
-      self.class.with_deleted.where(interface_id: self.interface_id).where("id <= #{self.id}").count
-    end
-
-    def to_hash
-      super.merge({
-        mac_address: self.mac_address
-      })
-    end
   end
 end
