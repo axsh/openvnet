d1ef49c5cbd1e058bc9ae28895ec0344a18c6107
Richard Potter <potter@axsh.net>
2014-03-10 15:29:31 +0900

    Added LeasePolicyBaseNetwork and some webapi support

--- vnet/lib/vnet/endpoints/1.0/responses/lease_policy.rb
+++ vnet/lib/vnet/endpoints/1.0/responses/lease_policy.rb
@@ -6,6 +6,15 @@ module Vnet::Endpoints::V10::Responses
       argument_type_check(object,Vnet::ModelWrappers::LeasePolicy)
       object.to_hash
     end
+
+    def self.lease_policy_network(object)
+      argument_type_check(object,Vnet::ModelWrappers::LeasePolicy)
+      {
+        # TODO: currently just following example in responses/translation.rb,
+        # so verify how this is used and if it is OK
+        :uuid => object.uuid,
+      }
+    end
   end
 
   class LeasePolicyCollection < Vnet::Endpoints::ResponseGenerator
--- vnet/lib/vnet/model_wrappers/lease_policy.rb
+++ vnet/lib/vnet/model_wrappers/lease_policy.rb
@@ -4,4 +4,8 @@ module Vnet::ModelWrappers
   class LeasePolicy < Base
 
   end
+
+  class LeasePolicyBaseNetwork < Base
+
+  end
 end
