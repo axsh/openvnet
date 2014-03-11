8bd92d7e4135995ef0bc1daaed0fae48a1f43466
akry <a-yokokawa@axsh.net>
2014-03-11 21:21:02 +0900

    Updated the version of zeromq package.

--- deployment/packagebuild/packages.d/vnet/wakame-vnet-common/recipe.rb
+++ deployment/packagebuild/packages.d/vnet/wakame-vnet-common/recipe.rb
@@ -7,7 +7,7 @@ class WakameVnetCommon < FPM::Cookery::Recipe
   source   File.expand_path("../../../../../", File.dirname(__FILE__)), :with => :local_path
   arch 'all'
   depends *%w(
-    zeromq-devel
+    zeromq3-devel
     wakame-vnet-ruby
   )
 
