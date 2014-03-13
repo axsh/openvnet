0a93f697505724ee5f35b529cf695518c6e6333d
Metallion <akieckens@axsh.net>
2014-03-14 00:32:51 +0900

    Wrote some unit tests for validating security group rules

--- vnet/spec/vnet/endpoints/1.0/security_groups_spec.rb
+++ vnet/spec/vnet/endpoints/1.0/security_groups_spec.rb
@@ -23,13 +23,54 @@ describe "/security_groups" do
       :display_name => "our test secg",
       :description => "A longer description... or it should be at least.",
       :rules => "
-        tcp,22:22,0.0.0.0
-        udp,53:53,0.0.0.0
+        tcp:22:0.0.0.0
+        udp:53:0.0.0.0
+        icmp:-1:sg-group
       "
     }
     required_params = [:display_name]
     uuid_params = [:uuid]
 
     include_examples "POST /", accepted_params, required_params, uuid_params
+
+    context "with a faulty protocol in the rules" do
+      let(:request_params) { accepted_params.merge(rules: "broken") }
+
+      it_should_return_error(
+         500,
+        "Sequel::ValidationFailed",
+        "invalid protocol in rule 'broken'"
+      )
+    end
+
+    context "with a faulty port in the rules" do
+      let(:request_params) { accepted_params.merge(rules: "tcp:joske:0.0.0.0/0") }
+
+      it_should_return_error(
+        500,
+        "Sequel::ValidationFailed",
+        "invalid port in rule 'tcp:joske:0.0.0.0/0'"
+      )
+    end
+
+    context "with a faulty ip in the rules" do
+      let(:request_params) { accepted_params.merge(rules: "tcp:22:456.462.890.1/33") }
+
+      it_should_return_error(
+        500,
+        "Sequel::ValidationFailed",
+        "invalid ipv4 address or security group uuid in rule 'tcp:22:456.462.890.1/33'"
+      )
+    end
+
+    context "with a faulty security group uuid in the rules" do
+      let(:request_params) { accepted_params.merge(rules: "tcp:22:sg-i'm_illegal") }
+
+      it_should_return_error(
+        500,
+        "Sequel::ValidationFailed",
+        "invalid ipv4 address or security group uuid in rule 'tcp:22:sg-i'm_illegal'"
+      )
+    end
   end
 end
