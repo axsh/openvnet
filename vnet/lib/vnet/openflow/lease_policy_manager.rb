9bbb327be8cd7fe5ca5044bf84e2d0003fc57f8f
Richard Potter <potter@axsh.net>
2014-03-27 03:50:13 -0400

    Added lease_policies/base.rb

--- vnet/lib/vnet/openflow/lease_policy_manager.rb
+++ vnet/lib/vnet/openflow/lease_policy_manager.rb
@@ -30,8 +30,7 @@ module Vnet::Openflow
     end
 
     def item_initialize(item_map, params)
-      ###Routers::RouteLink.new(dp_info: @dp_info, manager: self, map: item_map)
-      # TODO: What should go in here to replace the previous line???
+      LeasePolicies::Base.new(dp_info: @dp_info, manager: self, map: item_map)
     end
 
     def initialized_item_event
