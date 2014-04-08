936cb1ed6c984b4830f1e7566cfffe1a28bfa567
Richard Potter <potter@axsh.net>
2014-04-08 16:15:34 +0900

    Added ip_range_uuid option to lease_policies/*/associate_network

--- vnet/lib/vnet/endpoints/1.0/lease_policies.rb
+++ vnet/lib/vnet/endpoints/1.0/lease_policies.rb
@@ -35,15 +35,17 @@ Vnet::Endpoints::V10::VnetAPI.namespace '/lease_policies' do
   put '/:uuid/associate_network' do
     # TODO: it is now possible to associate twice....probably should not allow that.
     params = parse_params(@params, ['uuid', 'network_uuid', 'method'])
-    check_required_params(params, ['network_uuid'])
-    params['method'] = 'incremental' if params['method'].nil?
+    check_required_params(params, ['network_uuid', 'ip_range_uuid'])
+    params['method'] = 'incremental' if params['method'].nil?  # TODO: remove
     
     lease_policy = check_syntax_and_pop_uuid(M::LeasePolicy, params)
     # TODO: verify this next line is not just a hack (that does work, so far)
     network = check_syntax_and_pop_uuid(M::Network, { "uuid" => params[:network_uuid] } )
+    ip_range = check_syntax_and_pop_uuid(M::IpRange, { "uuid" => params[:ip_range_uuid] } )
 
     M::LeasePolicyBaseNetwork.create({ :network_id => network.id,
                                        :lease_policy_id => lease_policy.id,
+                                       :ip_range_id => ip_range.id,
                                        :mmethod => params['method']
                                      })
     respond_with(R::LeasePolicy.lease_policy_network(lease_policy))
