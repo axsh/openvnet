d1fc37309ed8eed68f0fb7d32c2a1b3decd3f280
Manabu Ejima <jemiam@gmail.com>
2014-04-11 22:18:50 +0900

    Remove unnecessary resource

--- vagrant/site-cookbooks/vnet/recipes/router.rb
+++ vagrant/site-cookbooks/vnet/recipes/router.rb
@@ -5,11 +5,3 @@ include_recipe "vnet::base"
 #end
 node.default[:sysctl][:params][:net][:ipv4][:ip_forward] = 1
 include_recipe "sysctl"
-
-node[:vnet][:router][:ifconfig].each do |i|
-  bash "ifconfig #{i[:device]} down"
-  ifconfig i[:target] do
-    device i[:device]
-    mask i[:mask] || "255.255.255.0"
-  end
-end
