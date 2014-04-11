bddc0e829308c5fe63e61c0e68b6346e7a76c553
Richard Potter <potter@axsh.net>
2014-04-11 14:28:12 +0900

    Adapt ip assignment code to net

--- vnet/lib/vnet/models/ip_ranges_range.rb
+++ vnet/lib/vnet/models/ip_ranges_range.rb
@@ -9,22 +9,22 @@ module Vnet::Models
 
     def_dataset_method(:containing_range) { |begin_range,end_range|
       new_dataset = self
-      new_dataset = new_dataset.filter("range_end >= ?", begin_range) if begin_range
-      new_dataset = new_dataset.filter("range_begin <= ?", end_range) if end_range
+      new_dataset = new_dataset.filter("end_ipv4_address >= ?", begin_range) if begin_range
+      new_dataset = new_dataset.filter("begin_ipv4_address <= ?", end_range) if end_range
       new_dataset
     }
 
-    def available_ip(from, to, order)
-      ipaddr = case order
-               when :asc
-                 boundaries = IpLease.leased_ip_bound_lease(self.network_id, from, to).limit(2).all
-                 ip = get_assignment_ip(boundaries, from, to)
-               when :desc
-                 boundaries = IpLease.leased_ip_bound_lease(self.network_id, from, to).order(:ip_leases__ipv4.desc).limit(2).all
-                 ip = get_assignment_ip(boundaries, to, from)
-               else
-                 raise "Unsupported IP address assignment: #{network[:ip_assignment]}"
-               end
+    def available_ip(network_id, from, to, order)
+      case order
+      when :asc
+        boundaries = IpAddress.leased_ip_bound_lease(network_id, from, to).limit(2).all
+        get_assignment_ip(boundaries, from, to)
+      when :desc
+        boundaries = IpAddress.leased_ip_bound_lease(network_id, from, to).order(:ip_addresses__ipv4_address.desc).limit(2).all
+        get_assignment_ip(boundaries, to, from)
+      else
+        raise "Unsupported IP address assignment: #{allocation_type}"
+      end
     end
 
     def get_assignment_ip(boundaries, from, to)
@@ -40,17 +40,18 @@ module Vnet::Models
         number = :-
       end
       return from if boundaries.size == 0
-      return from if boundaries[0][prev].nil? && boundaries[0][:ipv4] != from
+      return from if boundaries[0][prev].nil? && boundaries[0][:ipv4_address] != from
 
       start_range = nil
 
       if boundaries[0][follow].nil?
-        start_range = boundaries[0][:ipv4] if boundaries[0][:ipv4] != to
+        start_range = boundaries[0][:ipv4_address] if boundaries[0][:ipv4_address] != to
       elsif boundaries.size == 2 && boundaries[1][follow].nil?
-        start_range = boundaries[1][:ipv4] if boundaries[1][:ipv4] != to
+        start_range = boundaries[1][:ipv4_address] if boundaries[1][:ipv4_address] != to
       end
 
       return start_range ? start_range.method(number).call(1) : nil
     end
+
   end
 end
