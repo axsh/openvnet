94301262ef091c4bff9beb00ebce92d1d1f29c9d
Manabu Ejima <jemiam@gmail.com>
2014-04-24 21:21:44 +0900

    Refactor and Add spec for Vnet::NodeApi::LeasePolicy

--- vnet/spec/fabricators/ip_ranges_range_fabricator.rb
+++ vnet/spec/fabricators/ip_ranges_range_fabricator.rb
@@ -2,3 +2,8 @@
 
 Fabricator(:ip_ranges_range, class_name: Vnet::Models::IpRangesRange) do
 end
+
+Fabricator(:ip_ranges_range_with_range, class_name: Vnet::Models::IpRangesRange) do
+  begin_ipv4_address IPAddr.new("10.102.0.100").to_i
+  end_ipv4_address IPAddr.new("10.102.0.102").to_i
+end
