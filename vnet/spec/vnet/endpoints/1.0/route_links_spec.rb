94575343d0b255c894e3fa3062fe244d1579135f
Metallion <akieckens@axsh.net>
2014-05-01 16:44:52 +0900

    I noticed there somehow wasn't a test yet for the route links endpoint so I made one, watched it fail and fixed the endpoint so it succeeds.

---
+++ vnet/spec/vnet/endpoints/1.0/route_links_spec.rb
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+require 'spec_helper'
+require 'vnet'
+Dir["#{File.dirname(__FILE__)}/shared_examples/*.rb"].map {|f| require f }
+Dir["#{File.dirname(__FILE__)}/matchers/*.rb"].map {|f| require f }
+
+def app
+  Vnet::Endpoints::V10::VnetAPI
+end
+
+describe "/route_links" do
+  before(:each) { use_mock_event_handler }
+
+  let(:api_suffix)  { "route_links" }
+  let(:fabricator)  { :route_link }
+  let(:model_class) { Vnet::Models::RouteLink }
+
+  include_examples "GET /"
+  include_examples "GET /:uuid"
+  include_examples "DELETE /:uuid"
+
+  describe "POST /" do
+    accepted_params = {
+      :uuid => "rl-link",
+      :mac_address => "fe:17:9b:9f:e8:33",
+    }
+    required_params = [:mac_address]
+    uuid_params = [:uuid]
+
+    include_examples "POST /", accepted_params, required_params, uuid_params
+  end
+end
