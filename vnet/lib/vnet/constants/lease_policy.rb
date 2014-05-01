c5522771c845096dd719f46a5ab895ba7a4ef2f8
Metallion <akieckens@axsh.net>
2014-05-01 15:44:38 +0900

    Added constants for the possible parameters of lease policies

--- vnet/lib/vnet/constants/lease_policy.rb
+++ vnet/lib/vnet/constants/lease_policy.rb
@@ -5,5 +5,19 @@ module Vnet::Constants
     ALLOCATION_TYPE_INCREMENTAL="incremental"
     ALLOCATION_TYPE_DECREMENTAL="decremental"
     ALLOCATION_TYPE_RANDOM="random"
+
+    ALLOCATION_TYPES = [
+      ALLOCATION_TYPE_INCREMENTAL,
+      ALLOCATION_TYPE_DECREMENTAL,
+      ALLOCATION_TYPE_RANDOM
+    ]
+
+
+    MODE_SIMPLE = "simple"
+    MODES = [ MODE_SIMPLE ]
+
+    TIMING_IMMEDIATE = "immediate"
+    TIMING_DHCP = "dhcp"
+    TIMINGS = [ TIMING_IMMEDIATE, TIMING_DHCP ]
   end
 end
