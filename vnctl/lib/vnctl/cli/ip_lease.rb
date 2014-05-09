3241df81c4a5d3641e1dd39408039bdae0a2e7c9
Metallion <akieckens@axsh.net>
2014-05-09 16:13:12 +0900

    Updated vnctl ip leases

--- vnctl/lib/vnctl/cli/ip_lease.rb
+++ vnctl/lib/vnctl/cli/ip_lease.rb
@@ -6,10 +6,10 @@ module Vnctl::Cli
     api_suffix "/api/ip_leases"
 
     add_modify_shared_options {
-      option_uuid
       option :network_uuid, :type => :string, :desc => "The network to lease this ip in."
       option :mac_lease_uuid, :type => :string, :desc => "The mac lease that this ip lease is tried to."
       option :ipv4_address, :type => :string, :desc => "The ipv4 address to lease."
+      option :enable_routing, :type => :boolean, :desc => "Flag that decides whether or not routing is enabled for this ip lease."
     }
     set_required_options [:network_uuid, :mac_lease_uuid, :ipv4_address]
 
