bcb5e550bb28ed913352b120e14a30991cdcb5ad
Metallion <akieckens@axsh.net>
2014-05-09 18:46:05 +0900

    Simple identation fix in lease policies api

--- vnet/lib/vnet/endpoints/1.0/lease_policies.rb
+++ vnet/lib/vnet/endpoints/1.0/lease_policies.rb
@@ -3,8 +3,8 @@
 Vnet::Endpoints::V10::VnetAPI.namespace '/lease_policies' do
   CLP = Vnet::Constants::LeasePolicy
   def self.put_post_shared_params
-   param :mode, :String, in: CLP::MODES, default: CLP::MODE_SIMPLE
-   param :timing, :String, in: CLP::TIMINGS, default: CLP::TIMING_IMMEDIATE
+    param :mode, :String, in: CLP::MODES, default: CLP::MODE_SIMPLE
+    param :timing, :String, in: CLP::TIMINGS, default: CLP::TIMING_IMMEDIATE
   end
 
   fill_options = [ ]
