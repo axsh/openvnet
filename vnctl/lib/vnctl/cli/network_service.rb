2f83abf6ec4c7ffa0bdb798f9046097c58d72e84
Metallion <akieckens@axsh.net>
2014-05-09 18:51:11 +0900

    Updated network service cli

--- vnctl/lib/vnctl/cli/network_service.rb
+++ vnctl/lib/vnctl/cli/network_service.rb
@@ -10,11 +10,18 @@ module Vnctl::Cli
       option :interface_uuid, :type => :string, :desc => "The interface uuid for this network service."
       option :incoming_port, :type => :numeric, :desc => "The incoming port for this network service."
       option :outgoing_port, :type => :numeric, :desc => "The outgoing port for this network service."
-      option :type, :type => :string, :desc => "The type of this service."
     }
 
     set_required_options [:type]
 
-    define_standard_crud_commands
+    add_modify_shared_options
+    option :type, :type => :string, :desc => "The type of this service."
+    define_add
+
+    add_modify_shared_options
+    define_modify
+
+    define_show
+    define_del
   end
 end
