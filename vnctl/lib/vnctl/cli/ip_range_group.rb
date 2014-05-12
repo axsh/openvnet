b648396bf6ce51fdea5c5305e8b14a2cd54f91a3
Metallion <akieckens@axsh.net>
2014-05-12 13:01:20 +0900

    Added ip ranges relation to ip range groups in vnctl

--- vnctl/lib/vnctl/cli/ip_range_group.rb
+++ vnctl/lib/vnctl/cli/ip_range_group.rb
@@ -11,14 +11,11 @@ module Vnctl::Cli
 
     define_standard_crud_commands
 
-    #TODO: The webapi doesn't use the standard uuid based association here
-    # It instead creates a new range based on begin and end address so we can't
-    # use the standard define_relation method. We'll have to implement this manually ;_;
-    #define_relation :ip_ranges do |relation|
-    #  relation.option :begin_ipv4_address, :type => :string,
-    #    :desc => "The ipv4 address at which our range begins."
-    #  relation.option :end_ipv4_address, :type => :string,
-    #    :desc => "The ipv4 address at which our range ends."
-    #end
+    define_relation :ip_ranges, :require_relation_uuid_label => false do |relation|
+      relation.option :begin_ipv4_address, :type => :string,
+        :desc => "The ipv4 address at which our range begins."
+      relation.option :end_ipv4_address, :type => :string,
+        :desc => "The ipv4 address at which our range ends."
+    end
   end
 end
