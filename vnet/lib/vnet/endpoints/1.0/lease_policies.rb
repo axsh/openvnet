81f9bd00289d2313bbe8c4cd286202ec38d362aa
Manabu Ejima <jemiam@gmail.com>
2014-05-12 20:56:13 +0900

    Add support for lease time

--- vnet/lib/vnet/endpoints/1.0/lease_policies.rb
+++ vnet/lib/vnet/endpoints/1.0/lease_policies.rb
@@ -5,6 +5,7 @@ Vnet::Endpoints::V10::VnetAPI.namespace '/lease_policies' do
   def self.put_post_shared_params
    param :mode, :String, in: CLP::MODES, default: CLP::MODE_SIMPLE
    param :timing, :String, in: CLP::TIMINGS, default: CLP::TIMING_IMMEDIATE
+   param :lease_time, :Integer
   end
 
   fill_options = [ ]
