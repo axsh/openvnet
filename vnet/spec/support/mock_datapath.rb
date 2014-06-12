478eb793ff3b5e9ca0b6f431632d14f239c4edea
rakshasa <sundell.software@gmail.com>
2014-06-12 19:27:01 +0900

    Do mock cleanup of datapath in specs.

--- vnet/spec/support/mock_datapath.rb
+++ vnet/spec/support/mock_datapath.rb
@@ -175,6 +175,13 @@ class MockDatapath < Vnet::Openflow::Datapath
   def deleted_tunnels
     @dp_info.deleted_tunnels
   end
+
+  private
+
+  def do_cleanup
+    info log_format('mock cleaning up')
+  end  
+
 end
 
 def create_mock_datapath
