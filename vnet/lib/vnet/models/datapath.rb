cd6d7533772463399511ddebb3660d4e20e05eb8
rakshasa <sundell.software@gmail.com>
2014-07-24 19:48:22 +0900

    There's no need to optimize sending of deleted datapath event by sending only to those who need it, this is a rare event and overly complicates potential race conditions.

--- vnet/lib/vnet/models/datapath.rb
+++ vnet/lib/vnet/models/datapath.rb
@@ -29,23 +29,6 @@ module Vnet::Models
       "0x%016x" % dpid
     end
 
-    def peers
-      # deleted datapath's datapath_networks should also be deleted
-      if self.deleted_at
-        network_ids = datapath_networks_dataset.unfiltered.where(datapath_id: self.id).map(&:network_id)
-      else
-        network_ids = datapath_networks.map(&:network_id)
-      end
-
-      self.class
-      .graph(DatapathNetwork.dataset, datapath_id: :id)
-      .where(network_id: network_ids)
-      .exclude(datapaths__id: self.id)
-      .group(:datapaths__id)
-      .select_all(:datapaths)
-      .all
-    end
-
     dataset_module do
       def find_all_by_network_id(network_id)
         left_join(:datapath_networks, :datapath_id => :id).where(:datapath_networks__network_id => network_id).all
