abea3004119c62abcee5913e587743b5881776d7
rakshasa <sundell.software@gmail.com>
2014-07-24 19:35:40 +0900

    Split migration schema into two files with services in 0002.

--- vnet/db/migrations/0001_origin.rb
+++ vnet/db/migrations/0001_origin.rb
@@ -87,33 +87,6 @@ Sequel.migration do
       unique [:datapath_id, :route_link_id, :deleted_at]
     end
 
-    create_table(:dns_services) do
-      primary_key :id
-      String :uuid, :unique => true, :null=>false
-
-      Integer :network_service_id, :index => true, :null => false
-      String :public_dns
-
-      DateTime :created_at, :null=>false
-      DateTime :updated_at, :null=>false
-      DateTime :deleted_at, :index => true
-    end
-
-    create_table(:dns_records) do
-      primary_key :id
-      String :uuid, :unique => true, :null=>false
-
-      Integer :dns_service_id, :index => true, :null => false
-
-      String :name, :null => false
-      Bignum :ipv4_address, :null => false
-      Integer :ttl
-
-      DateTime :created_at, :null=>false
-      DateTime :updated_at, :null=>false
-      DateTime :deleted_at, :index => true
-    end
-
     create_table(:interfaces) do
       primary_key :id
       String :uuid, :unique => true, :null=>false
@@ -240,98 +213,6 @@ Sequel.migration do
       DateTime :created_at, :null=>false
       DateTime :updated_at, :null=>false
       DateTime :deleted_at, :index => true
-    end
-
-    create_table(:ip_retentions) do
-      primary_key :id
-
-      Integer :ip_lease_id, :index => true, :null => false
-      Integer :ip_retention_container_id, :index => true, :null => false
-
-      DateTime :leased_at
-      DateTime :released_at
-
-      DateTime :created_at, :null=>false
-      DateTime :updated_at, :null=>false
-      DateTime :deleted_at, :index => true
-    end
-
-    create_table(:ip_retention_containers) do
-      primary_key :id
-      String :uuid, :unique => true, :null=>false
-
-      Integer :lease_time
-      Integer :grace_time
-
-      DateTime :created_at, :null=>false
-      DateTime :updated_at, :null=>false
-      DateTime :deleted_at, :index => true
-    end
-
-    create_table(:lease_policies) do
-      primary_key :id
-      String :uuid, :unique => true, :null=>false
-      String :mode, :null=>false, :default => "simple"
-
-      String :timing, :null=>false, :default => "immediate"
-
-      DateTime :created_at, :null=>false
-      DateTime :updated_at, :null=>false
-      DateTime :deleted_at, :index => true
-    end
-
-    create_table(:lease_policy_base_interfaces) do
-      primary_key :id
-
-      Integer :lease_policy_id, :index => true, :null => false
-      Integer :interface_id, :index => true, :null => false
-
-      String :label
-
-      DateTime :created_at, :null=>false
-      DateTime :updated_at, :null=>false
-      DateTime :deleted_at, :index => true
-    end
-
-    create_table(:lease_policy_base_networks) do
-      primary_key :id
-
-      Integer :lease_policy_id, :index => true, :null => false
-      Integer :network_id, :index => true, :null => false
-      Integer :ip_range_group_id, :index => true, :null => false
-
-      DateTime :created_at, :null=>false
-      DateTime :updated_at, :null=>false
-      DateTime :deleted_at, :index => true
-    end
-
-    create_table(:lease_policy_ip_lease_containers) do
-      primary_key :id
-
-      Integer :lease_policy_id, :index => true, :null => false
-      Integer :ip_lease_container_id, :index => true, :null => false
-
-      String :label
-
-      DateTime :created_at, :null=>false
-      DateTime :updated_at, :null=>false
-      DateTime :deleted_at, :index => true
-
-      unique [:lease_policy_id, :ip_lease_container_id, :deleted_at]
-    end
-
-    create_table(:lease_policy_ip_retention_containers) do
-      primary_key :id
-
-      Integer :lease_policy_id, :index => true, :null => false
-      Integer :ip_retention_container_id, :null => false
-
-      DateTime :created_at, :null=>false
-      DateTime :updated_at, :null=>false
-      DateTime :deleted_at, :index => true
-
-      index :ip_retention_container_id, :name => :container_id_index
-      unique [:lease_policy_id, :ip_retention_container_id, :deleted_at]
     end
 
     create_table(:mac_addresses) do
@@ -528,13 +409,6 @@ Sequel.migration do
                :ip_lease_container_ip_leases,
                :ip_range_groups,
                :ip_ranges,
-               :ip_retentions,
-               :ip_retention_containers,
-               :lease_policies,
-               :lease_policy_base_interfaces,
-               :lease_policy_base_networks,
-               :lease_policy_ip_lease_containers,
-               :lease_policy_ip_retention_containers,
                :mac_addresses,
                :mac_leases,
                :networks,
@@ -545,7 +419,7 @@ Sequel.migration do
                :translation,
                :translation_static_addresses,
                :tunnels,
-               :vlan_translations
+               :vlan_translations,
                )
   end
 end
