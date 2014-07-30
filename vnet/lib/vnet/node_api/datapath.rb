a66d1cdbd4da95e90414551709b934600f1e2f60
rakshasa <sundell.software@gmail.com>
2014-07-30 21:00:27 +0900

    Fixed datapath delete event params.

--- vnet/lib/vnet/node_api/datapath.rb
+++ vnet/lib/vnet/node_api/datapath.rb
@@ -1,8 +1,7 @@
 # -*- coding: utf-8 -*-
 
 module Vnet::NodeApi
-
-  class Datapath < Base
+  class Datapath < EventBase
     class << self
 
       #
@@ -19,14 +18,17 @@ module Vnet::NodeApi
       # DatapathManager cleans up everything on the main deleted
       # event.
       def dispatch_deleted_item_events(model)
-        dispatch_event(DATAPATH_DELETED_ITEM, id: model.id)
+        dispatch_event(DATAPATH_DELETED_ITEM, id: model.id, node_id: model.node_id)
 
-        ActiveInterface.dispatch_deleted_where({ datapath_id: model.id }, model.deleted_at)
-        InterfacePort.dispatch_deleted_where({ datapath_id: model.id }, model.deleted_at)
-        Tunnel.dispatch_deleted_where({ datapath_id: model.id }, model.deleted_at)
+        default_filter = { datapath_id: model.id }
+        tunnel_filter = Sequel.|({ src_datapath_id: model.id },
+                                 { dst_datapath_id: model.id })
+
+        ActiveInterface.dispatch_deleted_where(default_filter, model.deleted_at)
+        InterfacePort.dispatch_deleted_where(default_filter, model.deleted_at)
+        Tunnel.dispatch_deleted_where(tunnel_filter, model.deleted_at)
       end
 
     end
   end
-
 end
