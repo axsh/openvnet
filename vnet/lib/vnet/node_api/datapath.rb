1a928528fdc9d4214fa68dff8c90839996e9a802
rakshasa <sundell.software@gmail.com>
2014-07-30 20:25:03 +0900

    Added association dependency events.

--- vnet/lib/vnet/node_api/datapath.rb
+++ vnet/lib/vnet/node_api/datapath.rb
@@ -1,19 +1,32 @@
 # -*- coding: utf-8 -*-
 
 module Vnet::NodeApi
+
   class Datapath < Base
     class << self
-      def create(options)
-        super.tap do |datapath|
-          dispatch_event(DATAPATH_CREATED_ITEM, datapath.to_hash)
-        end
+
+      #
+      # Internal methods:
+      #
+
+      private
+
+      def dispatch_created_item_events(model)
+        dispatch_event(DATAPATH_CREATED_ITEM, model.to_hash)
       end
 
-      def destroy(uuid)
-        super.tap do |datapath|
-          dispatch_event(DATAPATH_DELETED_ITEM, datapath.to_hash)
-        end
+      # DatapathNetwork and DatapathRouteLink events are not needed as
+      # DatapathManager cleans up everything on the main deleted
+      # event.
+      def dispatch_deleted_item_events(model)
+        dispatch_event(DATAPATH_DELETED_ITEM, id: model.id)
+
+        ActiveInterface.dispatch_deleted_where({ datapath_id: model.id }, model.deleted_at)
+        InterfacePort.dispatch_deleted_where({ datapath_id: model.id }, model.deleted_at)
+        Tunnel.dispatch_deleted_where({ datapath_id: model.id }, model.deleted_at)
       end
+
     end
   end
+
 end
