4e6bbecd45755afbffe8ab7e1fcd5a483c68208f
rakshasa <sundell.software@gmail.com>
2014-07-31 16:59:06 +0900

    Fixed datapath's dpn spec and added dprl.

--- vnet/spec/fabricators/datapath_route_link_fabricator.rb
+++ vnet/spec/fabricators/datapath_route_link_fabricator.rb
@@ -1,2 +1,51 @@
 Fabricator(:datapath_route_link, class_name: Vnet::Models::DatapathRouteLink) do
 end
+
+Fabricator(:datapath_route_link_1_1, class_name: Vnet::Models::DatapathRouteLink) do
+  datapath_id 1
+  route_link_id 1
+  mac_address Trema::Mac.new("52:54:00:01:01:01").value
+  is_connected false
+end
+
+Fabricator(:datapath_route_link_1_2, class_name: Vnet::Models::DatapathRouteLink) do
+  datapath_id 1
+  route_link_id 2
+  mac_address Trema::Mac.new("52:54:00:01:01:02").value
+  is_connected false
+end
+
+Fabricator(:datapath_route_link_2_1, class_name: Vnet::Models::DatapathRouteLink) do
+  datapath_id 2
+  route_link_id 1
+  mac_address Trema::Mac.new("52:54:00:01:02:01").value
+  is_connected false
+end
+
+Fabricator(:datapath_route_link_2_2, class_name: Vnet::Models::DatapathRouteLink) do
+  datapath_id 2
+  route_link_id 2
+  mac_address Trema::Mac.new("52:54:00:01:02:02").value
+  is_connected false
+end
+
+Fabricator(:datapath_route_link_2_3, class_name: Vnet::Models::DatapathRouteLink) do
+  datapath_id 2
+  route_link_id 3
+  mac_address Trema::Mac.new("52:54:00:01:02:03").value
+  is_connected false
+end
+
+Fabricator(:datapath_route_link_3_1, class_name: Vnet::Models::DatapathRouteLink) do
+  datapath_id 3
+  route_link_id 1
+  mac_address Trema::Mac.new("52:54:00:01:03:01").value
+  is_connected false
+end
+
+Fabricator(:datapath_route_link_3_2, class_name: Vnet::Models::DatapathRouteLink) do
+  datapath_id 3
+  route_link_id 2
+  mac_address Trema::Mac.new("52:54:00:01:03:02").value
+  is_connected false
+end
