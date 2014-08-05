38968cb2d73b1ced30959a0433499fb76d3dd811
rakshasa <sundell.software@gmail.com>
2014-08-05 22:19:08 +0900

    Removed routes and network_services associations from network model.

--- vnet/lib/vnet/models/network.rb
+++ vnet/lib/vnet/models/network.rb
@@ -18,32 +18,6 @@ module Vnet::Models
     many_to_many :lease_policies, :join_table => :lease_policy_base_networks, :conditions => "lease_policy_base_networks.deleted_at is null"
     one_to_many :lease_policy_base_networks
 
-    many_to_many :network_services do |ds|
-      NetworkService.join_table(
-        :inner, :interfaces,
-        {interfaces__id: :network_services__interface_id}
-      ).join_table(
-        :left, :ip_leases,
-        {ip_leases__interface_id: :interfaces__id}
-      ).join_table(
-        :inner, :ip_addresses,
-        {ip_addresses__id: :ip_leases__ip_address_id} & {ip_addresses__network_id: self.id}
-      ).select_all(:network_services)
-    end
-
-    one_to_many :routes, :class=>Route do |ds|
-      Route.join_table(
-        :inner, :interfaces,
-        {interfaces__id: :routes__interface_id}
-      ).join_table(
-        :left, :ip_leases,
-        {ip_leases__interface_id: :interfaces__id}
-      ).join_table(
-        :inner, :ip_addresses,
-        {ip_addresses__id: :ip_leases__ip_address_id} & {ip_addresses__network_id: self.id}
-      ).select_all(:routes)
-    end
-
     def before_destroy
       [DatapathNetwork, IpAddress, Route, VlanTranslation].each do |klass|
         if klass[network_id: id]
