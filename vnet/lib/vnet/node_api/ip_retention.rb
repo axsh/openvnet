295d6b75c5f5f2f619c8db7399cbfacd62dbc73a
rakshasa <sundell.software@gmail.com>
2014-08-12 21:38:12 +0900

    Fix ip retention events.

--- vnet/lib/vnet/node_api/ip_retention.rb
+++ vnet/lib/vnet/node_api/ip_retention.rb
@@ -1,10 +1,20 @@
+# -*- coding: utf-8 -*-
+
 module Vnet::NodeApi
   class IpRetention < EventBase
     class << self
       private
 
+      # TODO: Rename event.
+
       def dispatch_created_item_events(model)
-        # dispatch_event(, model.to_hash)
+        dispatch_event(IP_RETENTION_CONTAINER_ADDED_IP_RETENTION, prepare_event_hash(model))
+      end
+
+      def dispatch_deleted_item_events(model)
+        dispatch_event(IP_RETENTION_CONTAINER_REMOVED_IP_RETENTION,
+                       id: model.ip_retention_container_id,
+                       ip_retention_id: model.id)
 
         # 0001_origin
         # ip_lease_container_ip_leases: send?
@@ -12,10 +22,11 @@ module Vnet::NodeApi
         # lease_policy_ip_lease_containers: send?
       end
 
-      def dispatch_deleted_item_events(model)
-        dispatch_event(IP_RETENTION_CONTAINER_REMOVED_IP_RETENTION,
-                       id: model.ip_retention_container_id,
-                       ip_retention_id: model.id)
+      def prepare_event_hash(model)
+        model.to_hash.tap { |event_hash|
+          event_hash[:ip_retention_id] = event_hash[:id]
+          event_hash[:id] = event_hash[:ip_retention_container_id]
+        }
       end
 
     end
--- vnet/lib/vnet/node_api/ip_retention_container.rb
+++ vnet/lib/vnet/node_api/ip_retention_container.rb
@@ -1,6 +1,10 @@
+# -*- coding: utf-8 -*-
+
 module Vnet::NodeApi
   class IpRetentionContainer < EventBase
     class << self
+
+      # TODO: This should call ip_retention's node_api directly.
 
       def add_ip_retention(id, options)
         ip_retention_container = model_class[id]
@@ -10,7 +14,7 @@ module Vnet::NodeApi
           ip_retention = ip_retention_container.add_ip_retention(ip_lease_id: options[:ip_lease_id])
         end
 
-        dispatch_event(IP_RETENTION_CONTAINER_ADDED_IP_RETENTION, ip_retention.to_hash)
+        IpRetention.dispatch_created_for_model(ip_retention)
 
         ip_retention
       end
@@ -24,9 +28,7 @@ module Vnet::NodeApi
           ip_retention.destroy
         end
 
-        dispatch_event(IP_RETENTION_CONTAINER_REMOVED_IP_RETENTION,
-                       id: id,
-                       ip_retention_id: ip_retention_id)
+        IpRetention.dispatch_deleted_for_model(ip_retention)
 
         ip_retention
       end
@@ -38,11 +40,11 @@ module Vnet::NodeApi
       private
 
       def dispatch_created_item_events(model)
-        dispatch_event(IP_RETENTION_CONTAINER_CREATED_ITEM, ip_retention_container.to_hash)
+        dispatch_event(IP_RETENTION_CONTAINER_CREATED_ITEM, model.to_hash)
       end
 
       def dispatch_deleted_item_events(model)
-        dispatch_event(IP_RETENTION_CONTAINER_CREATED_ITEM, ip_retention_container.to_hash)
+        dispatch_event(IP_RETENTION_CONTAINER_DELETED_ITEM, id: model.id)
 
         # 0002_services
         # ip_retentions: :destroy,
