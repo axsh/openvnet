33dbe6ab4a93dbe5d70c2270384594cf9b018bf1
rakshasa <sundell.software@gmail.com>
2014-08-13 16:43:00 +0900

    Refactored route_links models and node_api's.

--- vnet/lib/vnet/models/route_link.rb
+++ vnet/lib/vnet/models/route_link.rb
@@ -10,11 +10,18 @@ module Vnet::Models
     plugin :mac_address
 
     one_to_many :routes
+
     one_to_many :datapath_route_links
-    many_to_many :datapaths, :join_table => :datapath_route_links, :conditions => "datapath_route_links.deleted_at is null"
+    many_to_many :datapaths, join_table: :datapath_route_links, conditions: "datapath_route_links.deleted_at is null"
+
+    one_to_many :translation_static_addresses
+    many_to_many :translations, join_table: :translation_static_addresses, conditions: "translation_static_addresses.deleted_at is null"
 
     plugin :association_dependencies,
     # 0001_origin
+    datapath_route_links: :destroy,
+    routes: :destroy,
+    translation_static_addresses: :destroy,
     _mac_address: :destroy
 
   end
--- vnet/lib/vnet/node_api/route_link.rb
+++ vnet/lib/vnet/node_api/route_link.rb
@@ -6,17 +6,18 @@ module Vnet::NodeApi
       private
 
       def dispatch_created_item_events(model)
-        # dispatch_event(ROUTE_LINK_CREATED_ITEM, model.to_hash)
+        dispatch_event(ROUTE_LINK_CREATED_ITEM, model.to_hash)
       end
 
       def dispatch_deleted_item_events(model)
-        # dispatch_event(ROUTE_LINK_DELETED_ITEM, id: model.id)
+        dispatch_event(ROUTE_LINK_DELETED_ITEM, id: model.id)
 
-        # TODO: Event for all routes (?).
         filter = { route_link_id: model.id }
 
+        # 0001_origin
+        DatapathRouteLink.dispatch_deleted_where(filter, model.deleted_at)
         Route.dispatch_deleted_where(filter, model.deleted_at)
-        # DatapathRouteLink.dispatch_deleted_where(filter, model.deleted_at)
+        # translation_static_addresses: :destroy,
       end
 
     end
