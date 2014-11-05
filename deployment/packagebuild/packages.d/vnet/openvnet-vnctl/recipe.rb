2fb860eadae0438cf79519df17e1e57a5ebefd39
Metallion <akieckens@axsh.net>
2014-11-05 15:24:07 +0900

    Added fpm recipe for vnctl

---
+++ deployment/packagebuild/packages.d/vnet/openvnet-vnctl/recipe.rb
@@ -0,0 +1,21 @@
+class OpenvnetVnctl < FPM::Cookery::Recipe
+  name     'openvnet-vnctl'
+  description 'Commandline client for making requests to OpenVNet\'s web API'
+  homepage 'https://github.com/axsh/openvnet/'
+  version (ENV['BUILD_TIME'] || Time.now.strftime('%Y%m%d%H%M%S')) + (ENV['GIT_COMMIT'] ? "git#{ENV['GIT_COMMIT'].slice(0, 7)}" : "spot")
+  source   File.expand_path("../../../../../", File.dirname(__FILE__)), :with => :local_path
+  arch 'all'
+
+  depends 'openvnet-ruby'
+
+  config_files '/etc/openvnet/vnctl.conf'
+
+  def build
+  end
+
+  def install
+    opt('axsh/openvnet/vnctl').install Dir["vnctl/*"]
+
+    etc('/openvnet').install Dir['deployment/conf_files/etc/openvnet/vnctl.conf']
+  end
+end
