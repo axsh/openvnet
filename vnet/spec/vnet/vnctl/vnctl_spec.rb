590157df00b8ebdf8ebc76b4a9ffb5a8551305ef
Metallion <akieckens@axsh.net>
2015-05-09 10:57:44 +0900

    Wrote some proof of concept code that uses sintra-browse to check if vnctl has all webapi parameters implemented

---
+++ vnet/spec/vnet/vnctl/vnctl_spec.rb
@@ -0,0 +1,29 @@
+
+# -*- coding: utf-8 -*-
+require 'spec_helper'
+require 'vnet'
+require 'yaml'
+
+def app
+  Vnet::Endpoints::V10::VnetAPI
+end
+
+$LOAD_PATH.unshift File.expand_path('../../../../../vnctl/lib', __FILE__)
+
+require 'thor'
+require 'vnctl'
+
+
+describe 'vnctl' do
+  it 'has exactly the same parameters as the webapi' do
+    get('browse', format: :yaml)
+    routes = YAML.load(last_response.body)
+
+    webapi_post_datapaths = routes.find { |r| r[:route] == 'POST  /datapaths' }
+    cli_add_datapath = Vnctl::Cli::Datapath.all_tasks["add"]
+
+    webapi_post_datapaths[:parameters].each do |param|
+      expect(cli_add_datapath.options).to have_key(param[:name])
+    end
+  end
+end
