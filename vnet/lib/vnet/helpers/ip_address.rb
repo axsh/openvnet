b305b4617c0c305d552c8fe36e775aedda7580a5
Kenny Debrauwer <kenny@axsh.net>
2015-06-22 16:26:08 +0900

    ignore 0.0.0.0 ip addresses when checking for subnet

--- vnet/lib/vnet/helpers/ip_address.rb
+++ vnet/lib/vnet/helpers/ip_address.rb
@@ -4,7 +4,9 @@ module Vnet::Helpers
   class IpAddress
   
     def self.valid_in_subnet(network, ipv4_address)
-  
+      if (ipv4_address == 0)
+        return true
+      end
       ipv4_nw = IPAddress::IPv4::parse_u32(network.ipv4_network, network.ipv4_prefix)
       ipv4 = IPAddress::IPv4::parse_u32(ipv4_address, network.ipv4_prefix)
     
