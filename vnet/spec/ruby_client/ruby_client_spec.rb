673f1f8b4998b5bcd67e7d86b13e3a4f23ccbfc1
Metallion <akieckens@axsh.net>
2015-07-06 19:34:38 +0900

    Added the beginning of unit tests for the ruby client

---
+++ vnet/spec/ruby_client/ruby_client_spec.rb
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+require 'spec_helper'
+
+def app
+  Vnet::Endpoints::V10::VnetAPI
+end
+
+include Rack::Test::Methods
+api_specs = YAML.load(get("browse", format: :yaml).body)
+
+#api_specs.each { |api_spec|
+#  p api_spec[:route].split('/')[1]
+#}
+
+#TODO: Freaking optimize this. :)
+expected_classes = api_specs.map { |api_spec|
+  underscored = api_spec[:route].split('/')[1].chomp('s')
+  next if underscored == 'browse'
+
+  underscored.split('_').collect!{ |w| w.capitalize }.join
+}.uniq
+
+describe VNetAPIClient do
+  expected_classes.each do |klass|
+
+    describe klass do
+
+      it "exists" do
+        VNetAPIClient.const_get(klass)
+      end
+
+    end
+
+  end
+end
+
