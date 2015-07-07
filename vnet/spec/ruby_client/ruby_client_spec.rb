19ed67390c44cb28d142ae61b3ef731715c96ceb
Metallion <akieckens@axsh.net>
2015-07-07 18:27:35 +0900

    Added unit tests that check if the required methods are defined in the ruby client gem

--- vnet/spec/ruby_client/ruby_client_spec.rb
+++ vnet/spec/ruby_client/ruby_client_spec.rb
@@ -8,20 +8,45 @@ end
 include Rack::Test::Methods
 api_specs = YAML.load(get("browse", format: :yaml).body)
 
-#TODO: Freaking optimize this. :)
-expected_classes = api_specs.map { |api_spec|
-  underscored = api_spec[:route].split('/')[1].chomp('s')
+expected_classes = {}
+
+api_specs.each { |api_spec|
+  verb, suffix = api_spec[:route].split('  ')
+  underscored = suffix.split('/')[1]
   next if underscored == 'browse'
 
-  underscored.split('_').collect!{ |w| w.capitalize }.join
-}.uniq.compact
+  class_name = underscored.chomp('s').split('_').collect!{ |w| w.capitalize }.join
+
+  expected_classes[class_name] ||= []
+
+  case api_spec[:route]
+  when "POST  /#{underscored}"
+    expected_classes[class_name] << :create
+  when "GET  /#{underscored}"
+    expected_classes[class_name] << :index
+  when "GET  /#{underscored}/:uuid"
+    expected_classes[class_name] << :show
+  when "PUT  /#{underscored}/:uuid"
+    expected_classes[class_name] << :update
+  when "DELETE  /#{underscored}/:uuid"
+    expected_classes[class_name] << :delete
+  end
+}
 
 describe VNetAPIClient do
-  expected_classes.each do |klass|
-    describe klass do
+  expected_classes.each do |class_name, methods|
+    describe class_name do
+
+      let(:klass) { VNetAPIClient.const_get(class_name) }
 
       it "exists" do
-        VNetAPIClient.const_get(klass)
+        klass
+      end
+
+      methods.each do |method|
+        it "has the '#{method}' method" do
+          expect(klass).to respond_to(method)
+        end
       end
 
     end
