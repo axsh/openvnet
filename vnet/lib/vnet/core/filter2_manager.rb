69cd2bc632e9881cc749501b2f0a0d8c8bd8e2ef
root <root@10-vnetdev.(none)>
2015-07-08 18:45:21 +0900

    First adjustments towards filter

--- vnet/lib/vnet/core/filter2_manager.rb
+++ vnet/lib/vnet/core/filter2_manager.rb
@@ -20,8 +20,8 @@ module Vnet::Core
     subscribe_event FILTER_ACTIVATE_INTERFACE, :activate_interface
     subscribe_event FILTER_DEACTIVATE_INTERFACE, :deactivate_interface
 
-    subscribe_event FILTER_ADDED_STATIC_ADDRESS, :added_static_address
-    subscribe_event FILTER_REMOVED_STATIC_ADDRESS, :removed_static_address
+    subscribe_event FILTER_ADDED_STATIC, :added_static_address
+    subscribe_event FILTER_REMOVED_STATIC, :removed_static_address
 
     #
     # Internal methods:
@@ -80,11 +80,11 @@ module Vnet::Core
 
     def item_pre_install(item, item_map)
       case item.mode
-      when :static_address then load_static_addresses(item, item_map)
+      when :static_filter then load_static_filter(item, item_map)
       end
     end
 
-    # TRANSLATION_CREATED_ITEM on queue 'item.id'.
+    # FILTER_CREATED_ITEM on queue 'item.id'.
     def created_item(params)
       return if internal_detect_by_id(params)
       return if params[:interface_id].nil?
@@ -94,56 +94,36 @@ module Vnet::Core
     end
 
     #
-    # Translation events:
+    # Filter events:
     # to change
 
-    # load static addresses on queue 'item.id'.
-    def load_static_addresses(item, item_map)
-      item_map.batch.translation_static_addresses.commit.each { |translation|
-        item.added_static_address(translation.id,
-                                  translation.route_link_id,
-                                  translation.ingress_ipv4_address,
-                                  translation.egress_ipv4_address,
-                                  translation.ingress_port_number,
-                                  translation.egress_port_number)
+    # load static filter on queue 'item.id'.
+    def load_static_filter(item, item_map)
+      item_map.batch.static_filter.commit.each { |filter|
+        item.added_static_filter(filter.id)
       }
     end
 
     # TRANSLATION_ADDED_STATIC_ADDRESS on queue 'item.id'.
-    def added_static_address(params)
+    def added_static_filter(params)
       item = internal_detect_by_id_with_error(params) || return
 
-      static_address_id = get_param_id(params, :static_address_id) || return
-      route_link_id = get_param_id(params, :route_link_id) || return
+      static_filter_id = get_param_id(params, :static_filter_id) || return
 
-      ingress_ipv4_address = get_param_ipv4_address(params, :ingress_ipv4_address) || return
-      egress_ipv4_address = get_param_ipv4_address(params, :egress_ipv4_address) || return
-
-      ingress_port_number = get_param_port_number(params, :ingress_port_number, false)
-      egress_port_number = get_param_port_number(params, :egress_port_number, false)
-
-      if (params.has_key?(:ingress_port_number) || params.has_key?(:egress_port_number)) &&
-         (ingress_port_number.nil? || egress_port_number.nil?)
-         log_format("invalid port numbers", "ingress_port_number:#{params[:ingress_port_number]} egress_port_number:#{params[:egress_port_number]}")
         return
       end
 
 
-      item.added_static_address(static_address_id,
-                                route_link_id,
-                                ingress_ipv4_address,
-                                egress_ipv4_address,
-                                ingress_port_number,
-                                egress_port_number)
+      item.added_static_address(static_filter_id)
     end
 
-    # TRANSLATION_REMOVED_STATIC_ADDRESS on queue 'item.id'.
-    def removed_static_address(params)
+    # FILTER_REMOVED_STATIC on queue 'item.id'.
+    def removed_static_filter(params)
       item = internal_detect_by_id(params) || return
 
-      static_address_id = params[:static_address_id] || return
+      static_filter_id = params[:static_filter_id] || return
 
-      item.removed_static_address(static_address_id)
+      item.removed_static_filter(static_filter_id)
     end
 
   end
