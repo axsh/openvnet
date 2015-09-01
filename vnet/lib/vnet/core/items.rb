654aedec1e055a0c4ebc6e8691d6d89d6ff865eb
rakshasa <sundell.software@gmail.com>
2015-09-01 20:01:32 +0900

    Don't use separate files for item ostructs.

---
+++ vnet/lib/vnet/core/items.rb
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+require 'ostruct'
+
+module Vnet::Core
+
+  class ActiveInterface < OpenStruct
+  end
+
+  class Datapath < OpenStruct
+  end
+
+  class Network < OpenStruct
+  end
+
+  class Route < OpenStruct
+  end
+
+  class Router < OpenStruct
+  end
+
+  class Translation < OpenStruct
+  end
+
+end
