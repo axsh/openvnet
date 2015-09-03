895254906abe85aebb85a4b7402f082ff6f1c828
Akira Yokokawa <a-yokokawa@axsh.net>
2015-09-03 03:34:44 -0700

    Add migration file to set default value as 0 in 'is_deleted' column.

---
+++ vnet/db/migrations/0003_set_is_deleted_default_value.rb
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+
+def tables_with_is_deleted
+  [
+    :datapaths,
+    :datapath_networks,
+    :datapath_route_links,
+    :interfaces,
+    :ip_addresses,
+    :ip_leases,
+    :ip_lease_containers,
+    :ip_lease_container_ip_leases,
+    :ip_range_groups,
+    :ip_ranges,
+    :mac_addresses,
+    :mac_leases,
+    :networks,
+    :network_services,
+    :routes,
+    :route_links,
+    :security_groups,
+    :security_group_interfaces,
+    :translations,
+    :translation_static_addresses,
+    :tunnels,
+    :dns_services,
+    :dns_records,
+    :ip_retentions,
+    :ip_retention_containers,
+    :lease_policies,
+    :lease_policy_base_interfaces,
+    :lease_policy_base_networks,
+    :lease_policy_ip_lease_containers,
+    :lease_policy_ip_retention_containers
+  ]
+end
+
+Sequel.migration do
+  up do
+    tables_with_is_deleted.each do |table|
+      set_column_default table, :is_deleted, 0
+    end
+  end
+
+  down do
+    tables_with_is_deleted.each do |table|
+      drop_column table, :is_deleted
+    end
+  end
+
+end
