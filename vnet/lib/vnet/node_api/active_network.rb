0e09665e0fabe15e5357cdcc519ea330bb368c06
rakshasa <sundell.software@gmail.com>
2015-09-04 18:37:25 +0900

    Delete old active_network when creating a new on.

--- vnet/lib/vnet/node_api/active_network.rb
+++ vnet/lib/vnet/node_api/active_network.rb
@@ -14,11 +14,7 @@ module Vnet::NodeApi
         datapath_id = options[:datapath_id]
 
         transaction {
-          # active_models = model_class.where(network_id: network_id).all
-
-          # old_model = prune_old(active_models, datapath_id)
-
-          # old_model.destroy if old_model
+          model_class.where(network_id: network_id, datapath_id: datapath_id).destroy
 
           internal_create(options)
         }
@@ -31,25 +27,6 @@ module Vnet::NodeApi
       def dispatch_deleted_item_events(model)
         dispatch_event(ACTIVE_NETWORK_DELETED_ITEM, id: model.id)
       end
-
-      # def prune_old(active_models, ignore_datapath_id)
-      #   old_model = nil
-
-      #   active_models.delete_if { |active_model|
-      #     if active_model.datapath_id == ignore_datapath_id
-      #       old_model = active_model
-      #       next
-      #     end
-
-      #     # TODO: Catch exceptions.
-      #     # Only destroy active entries that are old.
-      #     # Currently default to destroying all:
-      #     # next unless active_model.updated_at + timeout < current_time
-      #     # active_model.destroy
-      #   }
-
-      #   old_model
-      # end
 
     end
   end
