ce32b91c39de0045839eed835d0cd1d969fa56b6
rakshasa <sundell.software@gmail.com>
2015-10-13 17:01:50 +0900

    Use a block to create the mac address or lease in MacRange.

--- vnet/lib/vnet/models/mac_range.rb
+++ vnet/lib/vnet/models/mac_range.rb
@@ -21,7 +21,22 @@ module Vnet::Models
       errors.add(:begin_mac_address, 'invalid mac address range') unless begin_mac_address <= end_mac_address
     end
 
+    def address_random
+      block_random { |mac_address|
+        MacAddress.create(mac_address: mac_address)
+      }
+    end
+
     def lease_random(interface_id)
+      block_random { |mac_address|
+        MacLease.create(interface_id: interface_id,
+                        mac_address: mac_address)
+      }
+    end
+
+    private
+
+    def block_random(&block)
       retry_count = 20
       range_size = end_mac_address - begin_mac_address + 1
 
@@ -29,10 +44,9 @@ module Vnet::Models
       # available.
       begin
         mac_address = begin_mac_address + Random.rand(range_size)
-        mac_lease = MacLease.create(interface_id: interface_id,
-                                    mac_address: mac_address)
+        result = block.call(mac_address)
         
-        return mac_lease if mac_lease
+        return result if result
         
         retry_count -= 1
       end while retry_count > 0
