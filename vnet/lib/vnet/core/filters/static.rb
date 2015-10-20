8c6cbde12619e701e1f59680e37fc51695ad7069
toros11 <tobias.rosen@axsh.net>
2015-10-20 19:33:51 +0900

    split rules into separate methods and added ipv4 mask

--- vnet/lib/vnet/core/filters/static.rb
+++ vnet/lib/vnet/core/filters/static.rb
@@ -1,12 +1,11 @@
 # -*- coding: utf-8 -*-
-
 module Vnet::Core::Filters
   
   class Static < Base2
 
     def initialize(params)
       super
-      
+
       @statics = {}
     end
 
@@ -15,46 +14,53 @@ module Vnet::Core::Filters
     end
 
     def pretty_static(sf)
-      "filter_id:#{sf[:static_id]} ipv4_address:#{sf[:ipv4_address]} port_number:#{sf[:port_number]}"
+      "filter_id:#{sf[:static_id]} ipv4_address:#{sf[:ipv4_address]}"
     end
-    
-    def install
 
+    def install
       return if @interface_id.nil?
 
       flows = []
       @statics.each { |id, filter|
-        
-        debug log_format('installing translation for ' + pretty_static(filter))
-        
-        flows_for_ingress_filtering(flows, filter)
-        flows_for_egress_filtering(flows, filter)
+
+        debug log_format('installing filter for ' + pretty_static(filter[:match]))
+
+        rules(filter[:match], filter[:protocol]).each { |ingress_rule, egress_rule|
+          flows_for_ingress_filtering(flows, ingress_rule)
+          flows_for_egress_filtering(flows, egress_rule)
+        }
       }
 
       @dp_info.add_flows(flows)
 
     end
 
-
-    def added_static(static_id, ipv4_address, port_number, protocol)
+    def added_static(static_id, ipv4_address, ipv4_prefix, port, protocol)
 
       filter = {
         :static_id => static_id,
         :ipv4_address => ipv4_address,
-        :port_number => port_number,
+        :ipv4_prefix => ipv4_prefix,
+        :port => port
+      }
+
+      @statics[static_id] = {
+        :match => filter,
         :protocol => protocol
       }
-      
-      @statics[static_id] = filter
 
-      return if @installed == false
+      return if !installed?
 
-      flows = []         
-      flows_for_ingress_filtering(flows, filter) 
-      flows_for_egress_filtering(flows, filter)
+      flows = []
+
+      rules(filter, protocol).each { |ingress_rule, egress_rule|
+        flows_for_ingress_filtering(flows, ingress_rule) 
+        flows_for_egress_filtering(flows, egress_rule)
+      }
+
+      @db_info.add_flows(flows)
 
     end
-
 
     def removed_static(static_id)
     end
@@ -65,107 +71,137 @@ module Vnet::Core::Filters
 
     private
 
-    def match_actions_for_ingress(filter)
-      case filter[:protocol]
-      when 'tcp' then
-        [{ eth_type: ETH_TYPE_IPV4,
-           ipv4_src: filter[:ipv4_address],
-           ip_proto: IPV4_PROTOCOL_TCP,
-           tcp_dst: filter[:port_number]
-        }]
-      when 'udp' then
-         [{ eth_type: ETH_TYPE_IPV4,
-           ipv4_src: filter[:ipv4_address],
-           ip_proto: IPV4_PROTOCOL_UDP,
-           udp_dst: filter[:port_number]
-          }]
-      when 'icmp' then
-        [{ eth_type: ETH_TYPE_IPV4,
-          ipv4_src: filter[:ipv4_address],
-          ip_proto: IPV4_PROTOCOL_ICMP
-         }]
+    def rules(filter, protocol)
+      case protocol
+      when "tcp"  then rule_for_tcp(filter)
+      when "udp"  then rule_for_udp(filter)
+      when "arp"  then rule_for_arp(filter)
+      when "icmp" then rule_for_icmp(filter)
       end
     end
 
-    def match_actions_for_egress(filter)
-      case filter[:protocol]
-      when "tcp" then
-        [{ eth_type: ETH_TYPE_IPV4,
-           ipv4_dst: filter[:ipv4_address],
-           ip_proto: IPV4_PROTOCOL_TCP,
-           tcp_dst: filter[:port_number]
-         }]
-      when "udp" then
-         [{ eth_type: ETH_TYPE_IPV4,
-           ipv4_dst: filter[:ipv4_address],
-           ip_proto: IPV4_PROTOCOL_UDP,
-           udp_dst: filter[:port_number]
-          }]
-      when "icmp" then
-        [{ eth_type: ETH_TYPE_IPV4,
-          ipv4_dst: filter[:ipv4_address],
-          ip_proto: IPV4_PROTOCOL_ICMP
-         }]
-      when "arp" then
-        [{ eth_type: ETH_TYPE_ARP }]
+    def rule_for_tcp(filter)
+      if filter[:port] > 0
+        [
+          [{ eth_type: ETH_TYPE_IPV4,
+             ipv4_src: filter[:ipv4_address],
+             ipv4_src_mask: IPV4_BROADCAST << (32 - filter[:ipv4_prefix]),
+             ip_proto: IPV4_PROTOCOL_TCP,
+             tcp_dst: filter[:port]
+           },
+           { eth_type: ETH_TYPE_IPV4,
+             ipv4_dst: filter[:ipv4_address],
+             ipv4_dst_mask: IPV4_BROADCAST << (32 - filter[:ipv4_prefix]),
+             ip_proto: IPV4_PROTOCOL_TCP,
+             tcp_dst: filter[:port]
+           }]
+        ]
+      else
+        [
+          [{ eth_type: ETH_TYPE_IPV4,
+             ipv4_src: filter[:ipv4_address],
+             ipv4_src_mask: IPV4_BROADCAST << (32 - filter[:ipv4_prefix]),
+             ip_proto: IPV4_PROTOCOL_TCP,
+           },
+           { eth_type: ETH_TYPE_IPV4,
+             ipv4_dst: filter[:ipv4_address],
+             ipv4_dst_mask: IPV4_BROADCAST << (32 - filter[:ipv4_prefix]),
+             ip_proto: IPV4_PROTOCOL_TCP,
+           }]
+        ]
       end
     end
 
-    def check_zero_value(match, filter)
-        if filter[:port_number] == 0
-          match.delete(:tcp_dst)
-          match.delete(:udp_dst)
-        end
-
-        if filter[:ipv4_address] <= 0
-          match.delete(:ipv4_src)
-          match.delete(:ipv4_dst)
-        end
-        return match
+    def rule_for_udp(filter)
+      if filter[:port] > 0
+        [
+          [{ eth_type: ETH_TYPE_IPV4,
+             ipv4_src: filter[:ipv4_address],
+             ipv4_src_mask: IPV4_BROADCAST << (32 - filter[:ipv4_prefix]),
+             ip_proto: IPV4_PROTOCOL_UDP,
+             udp_dst: filter[:port]
+           },
+           { eth_type: ETH_TYPE_IPV4,
+             ipv4_dst: filter[:ipv4_address],
+             ipv4_dst_mask: IPV4_BROADCAST << (32 - filter[:ipv4_prefix]),
+             ip_proto: IPV4_PROTOCOL_UDP,
+             udp_dst: filter[:port]
+           }]
+        ]
+      else
+        [
+          [{ eth_type: ETH_TYPE_IPV4,
+             ipv4_src: filter[:ipv4_address],
+             ipv4_src_mask: IPV4_BROADCAST << (32 - filter[:ipv4_prefix]),
+             ip_proto: IPV4_PROTOCOL_UDP,
+           },
+           { eth_type: ETH_TYPE_IPV4,
+             ipv4_dst: filter[:ipv4_address],
+             ipv4_dst_mask: IPV4_BROADCAST << (32 - filter[:ipv4_prefix]),
+             ip_proto: IPV4_PROTOCOL_UDP,
+           }]
+        ]
+      end        
     end
 
-    def flows_for_ingress_filtering(flows = [], filter)
+    def rule_for_icmp(filter)
+      [
+        [{ eth_type: ETH_TYPE_IPV4,
+           ipv4_src: filter[:ipv4_address],
+           ipv4_src_mask: IPV4_BROADCAST << (32 - filter[:ipv4_prefix]),
+           ip_proto: IPV4_PROTOCOL_ICMP
+         },
+         { eth_type: ETH_TYPE_IPV4,
+           ipv4_dst: filter[:ipv4_address],
+           ipv4_dst_mask: IPV4_BROADCAST << (32 - filter[:ipv4_prefix]),
+           ip_proto: IPV4_PROTOCOL_ICMP
+         }]
+      ]
+    end
 
-      match_actions_for_ingress(filter).each { |match|
+    def rule_for_arp(filter)
+      [
+        [{ eth_type: ETH_TYPE_ARP },
+         { eth_type: ETH_TYPE_ARP }]
+      ]
+    end
+
+    def flows_for_ingress_filtering(flows = [], match)
         if @ingress_passthrough
           flows << flow_create(
             table: TABLE_INTERFACE_INGRESS_FILTER,
             goto_table: TABLE_OUT_PORT_INTERFACE_INGRESS,
             priority: 10,
             match_interface: @interface_id,
-            match: check_zero_value(match, filter),
+            match: match
           )
         else
           flows << flow_create(
             table: TABLE_INTERFACE_INGRESS_FILTER,
             priority: 50,
-            match: check_zero_value(match, filter),
-            match_interface: @interface_id
+            match_interface: @interface_id,
+            match: match
           )
         end
-      }
     end
 
-    def flows_for_egress_filtering(flows = [], filter)
-
-      match_actions_for_egress(filter).each { |match|
+    def flows_for_egress_filtering(flows = [], match)
         if @egress_passthrough
           flows << flow_create(
             table: TABLE_INTERFACE_EGRESS_FILTER,
             goto_table: TABLE_INTERFACE_EGRESS_VALIDATE,
             priority: 10,
             match_interface: @interface_id,
-            match: check_zero_value(match, filter)
+            match: match
           )
         else
           flows << flow_create(
             table: TABLE_INTERFACE_EGRESS_FILTER,
             priority: 50,
             match_interface: @interface_id,
-            match: check_zero_value(match, filter)
+            match: match
           )
         end
-      }
     end
   end
 end
