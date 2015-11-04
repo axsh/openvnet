5266c63a40ee634aa60a991f10db3ce9a886e683
toros11 <tobias.rosen@axsh.net>
2015-11-04 20:58:28 +0900

    fix rule methods

--- vnet/lib/vnet/core/filters/static.rb
+++ vnet/lib/vnet/core/filters/static.rb
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+
 module Vnet::Core::Filters
   
   class Static < Base2
@@ -52,9 +53,8 @@ module Vnet::Core::Filters
       return if !installed?
 
       flows = []
-
       rules(filter, protocol).each { |ingress_rule, egress_rule|
-        flows_for_ingress_filtering(flows, ingress_rule) 
+        flows_for_ingress_filtering(flows, ingress_rule)
         flows_for_egress_filtering(flows, egress_rule)
       }
 
@@ -72,136 +72,146 @@ module Vnet::Core::Filters
     private
 
     def rules(filter, protocol)
+      ipv4_address = filter[:ipv4_adress]
+      port = filter[:port]
+      prefix = filter[:ipv4_prefix]
+
       case protocol
-      when "tcp"  then rule_for_tcp(filter)
-      when "udp"  then rule_for_udp(filter)
-      when "arp"  then rule_for_arp(filter)
-      when "icmp" then rule_for_icmp(filter)
+      when "tcp"  then rule_for_tcp(ipv4_address, port, prefix)
+      when "udp"  then rule_for_udp(ipv4_address, port, prefix)
+      when "arp"  then rule_for_arp(ipv4_address, prefix)
+      when "icmp" then rule_for_icmp(ipv4_address, prefix)
       end
     end
 
-    def rule_for_tcp(filter)
-      if filter[:port] > 0
+    def rule_for_tcp(ipv4_address, port, prefix)
+      if port == 0 || port.nil?
         [
           [{ eth_type: ETH_TYPE_IPV4,
-             ipv4_src: filter[:ipv4_address],
-             ipv4_src_mask: IPV4_BROADCAST << (32 - filter[:ipv4_prefix]),
+             ipv4_src: ipv4_address,
+             ipv4_src_mask: IPV4_BROADCAST << (32 - prefix),
              ip_proto: IPV4_PROTOCOL_TCP,
-             tcp_dst: filter[:port]
            },
            { eth_type: ETH_TYPE_IPV4,
-             ipv4_dst: filter[:ipv4_address],
-             ipv4_dst_mask: IPV4_BROADCAST << (32 - filter[:ipv4_prefix]),
+             ipv4_dst: ipv4_address,
+             ipv4_dst_mask: IPV4_BROADCAST << (32 - prefix),
              ip_proto: IPV4_PROTOCOL_TCP,
-             tcp_dst: filter[:port]
            }]
         ]
       else
         [
           [{ eth_type: ETH_TYPE_IPV4,
-             ipv4_src: filter[:ipv4_address],
-             ipv4_src_mask: IPV4_BROADCAST << (32 - filter[:ipv4_prefix]),
+             ipv4_src: ipv4_address,
+             ipv4_src_mask: IPV4_BROADCAST << (32 - prefix),
              ip_proto: IPV4_PROTOCOL_TCP,
+             tcp_dst: port
            },
            { eth_type: ETH_TYPE_IPV4,
-             ipv4_dst: filter[:ipv4_address],
-             ipv4_dst_mask: IPV4_BROADCAST << (32 - filter[:ipv4_prefix]),
+             ipv4_dst: ipv4_address,
+             ipv4_dst_mask: IPV4_BROADCAST << (32 - prefix),
              ip_proto: IPV4_PROTOCOL_TCP,
+             tcp_dst: port
            }]
         ]
       end
     end
 
-    def rule_for_udp(filter)
-      if filter[:port] > 0
+    def rule_for_udp(ipv4_address, port, prefix)
+      if port == 0 || port.nil?
         [
           [{ eth_type: ETH_TYPE_IPV4,
-             ipv4_src: filter[:ipv4_address],
-             ipv4_src_mask: IPV4_BROADCAST << (32 - filter[:ipv4_prefix]),
+             ipv4_src: ipv4_address,
+             ipv4_src_mask: IPV4_BROADCAST << (32 - prefix),
              ip_proto: IPV4_PROTOCOL_UDP,
-             udp_dst: filter[:port]
            },
            { eth_type: ETH_TYPE_IPV4,
-             ipv4_dst: filter[:ipv4_address],
-             ipv4_dst_mask: IPV4_BROADCAST << (32 - filter[:ipv4_prefix]),
+             ipv4_dst: ipv4_address,
+             ipv4_dst_mask: IPV4_BROADCAST << (32 - prefix),
              ip_proto: IPV4_PROTOCOL_UDP,
-             udp_dst: filter[:port]
            }]
         ]
       else
         [
           [{ eth_type: ETH_TYPE_IPV4,
-             ipv4_src: filter[:ipv4_address],
-             ipv4_src_mask: IPV4_BROADCAST << (32 - filter[:ipv4_prefix]),
+             ipv4_src: ipv4_address,
+             ipv4_src_mask: IPV4_BROADCAST << (32 - prefix),
              ip_proto: IPV4_PROTOCOL_UDP,
+             udp_dst: port
            },
            { eth_type: ETH_TYPE_IPV4,
-             ipv4_dst: filter[:ipv4_address],
-             ipv4_dst_mask: IPV4_BROADCAST << (32 - filter[:ipv4_prefix]),
+             ipv4_dst: ipv4_address,
+             ipv4_dst_mask: IPV4_BROADCAST << (32 - prefix),
              ip_proto: IPV4_PROTOCOL_UDP,
+             udp_dst: port
            }]
         ]
       end        
     end
 
-    def rule_for_icmp(filter)
+    def rule_for_icmp(ipv4_address, prefix)
       [
         [{ eth_type: ETH_TYPE_IPV4,
-           ipv4_src: filter[:ipv4_address],
-           ipv4_src_mask: IPV4_BROADCAST << (32 - filter[:ipv4_prefix]),
+           ipv4_src: ipv4_address,
+           ipv4_src_mask: IPV4_BROADCAST << (32 - prefix),
            ip_proto: IPV4_PROTOCOL_ICMP
          },
          { eth_type: ETH_TYPE_IPV4,
-           ipv4_dst: filter[:ipv4_address],
-           ipv4_dst_mask: IPV4_BROADCAST << (32 - filter[:ipv4_prefix]),
+           ipv4_dst: ipv4_address,
+           ipv4_dst_mask: IPV4_BROADCAST << (32 - prefix),
            ip_proto: IPV4_PROTOCOL_ICMP
          }]
       ]
     end
 
-    def rule_for_arp(filter)
+    def rule_for_arp(ipv4_address, prefix)
       [
-        [{ eth_type: ETH_TYPE_ARP },
-         { eth_type: ETH_TYPE_ARP }]
+        [{ eth_type: ETH_TYPE_ARP,
+           ipv4_src: ipv4_address,
+           ipv4_src_mask: IPV4_BROADCAST << (32 - prefix)
+         },
+         { eth_type: ETH_TYPE_ARP,
+           ipv4_src: ipv4_address,
+           ipv4_dst_mask: IPV4_BROADCAST << (32 - prefix)
+         }]
       ]
     end
 
     def flows_for_ingress_filtering(flows = [], match)
-        if @ingress_passthrough
-          flows << flow_create(
-            table: TABLE_INTERFACE_INGRESS_FILTER,
-            goto_table: TABLE_OUT_PORT_INTERFACE_INGRESS,
-            priority: 10,
-            match_interface: @interface_id,
-            match: match
-          )
-        else
-          flows << flow_create(
-            table: TABLE_INTERFACE_INGRESS_FILTER,
-            priority: 50,
-            match_interface: @interface_id,
-            match: match
-          )
-        end
+      if @ingress_passthrough
+        flows << flow_create(
+          table: TABLE_INTERFACE_INGRESS_FILTER,
+          goto_table: TABLE_OUT_PORT_INTERFACE_INGRESS,
+          priority: 10,
+          match_interface: @interface_id,
+          match: match
+        )
+      else
+        flows << flow_create(
+          table: TABLE_INTERFACE_INGRESS_FILTER,
+          priority: 50,
+          match_interface: @interface_id,
+          match: match
+        )
+      end
     end
 
     def flows_for_egress_filtering(flows = [], match)
-        if @egress_passthrough
-          flows << flow_create(
-            table: TABLE_INTERFACE_EGRESS_FILTER,
-            goto_table: TABLE_INTERFACE_EGRESS_VALIDATE,
-            priority: 10,
-            match_interface: @interface_id,
-            match: match
-          )
-        else
-          flows << flow_create(
-            table: TABLE_INTERFACE_EGRESS_FILTER,
-            priority: 50,
-            match_interface: @interface_id,
-            match: match
-          )
-        end
+      if @egress_passthrough
+        flows << flow_create(
+          table: TABLE_INTERFACE_EGRESS_FILTER,
+          goto_table: TABLE_INTERFACE_EGRESS_VALIDATE,
+          priority: 10,
+          match_interface: @interface_id,
+          match: match
+        )
+      else
+        flows << flow_create(
+          table: TABLE_INTERFACE_EGRESS_FILTER,
+          priority: 50,
+          match_interface: @interface_id,
+          match: match
+        )
+      end
     end
   end
 end
