470a0ffb7002966cd24d0d96a4b1220d096f0e8e
rakshasa <sundell.software@gmail.com>
2015-11-19 01:39:55 +0900

    Moved model associations to where they belong in the migration file order.

--- vnet/lib/vnet/models/network.rb
+++ vnet/lib/vnet/models/network.rb
@@ -9,7 +9,6 @@ module Vnet::Models
     #
     # 0001_origin
     #
-    one_to_many :active_networks
     one_to_many :ip_addresses
     one_to_many :datapath_networks
     one_to_many :routes
@@ -24,15 +23,21 @@ module Vnet::Models
     one_to_many :lease_policy_base_networks
     many_to_many :lease_policies, :join_table => :lease_policy_base_networks, :conditions => "lease_policy_base_networks.deleted_at is null"
 
+    #
+    # 0004_active_items
+    #
+    one_to_many :active_networks
+
     plugin :association_dependencies,
     # 0001_origin
-    active_networks: :destroy,
     ip_addresses: :destroy,
     datapath_networks: :destroy,
     routes: :destroy,
     vlan_translations: :destroy,
     # 0002_services
-    lease_policy_base_networks: :destroy
+    lease_policy_base_networks: :destroy,
+    # 0004_active_items
+    active_networks: :destroy
 
   end
 end
