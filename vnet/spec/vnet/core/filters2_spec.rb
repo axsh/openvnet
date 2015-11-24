b920d04b7be2ac1a097adb05d40038dff28f68e6
toros11 <tobias.rosen@axsh.net>
2015-11-24 22:50:51 +0900

    addded some helper methods

--- vnet/spec/vnet/core/filters2_spec.rb
+++ vnet/spec/vnet/core/filters2_spec.rb
@@ -5,6 +5,66 @@ require 'spec_helper'
 include Vnet::Constants::Openflow
 include Vnet::Openflow::FlowHelpers
 
+
+def flow(params)
+  flow_create(
+    table: params[:table],
+    goto_table: params[:goto_table],
+    priority: params[:priority],
+    match_interface: params[:interface_id],
+    cookie: params[:id] | COOKIE_TYPE_FILTER2,
+    match: params[:match]
+  )
+end
+
+def merged_hash_array(hash_array)
+  return hash_array.inject(&:merge)
+end
+
+def ingress_tables(passthrough)
+   {
+    table: TABLE_INTERFACE_INGRESS_FILTER,
+    goto_table: passthrough ? TABLE_OUT_PORT_INTERFACE_INGRESS : nil
+   }
+end
+
+def egress_tables(passthrough)
+  {
+    table: TABLE_INTERFACE_EGRESS_FILTER,
+    goto_table: passthrough ? TABLE_INTERFACE_EGRESS_VALIDATE : nil,
+  }
+end
+
+def protocol_type(protocol, port_number)
+  case protocol
+    when IPV4_PROTOCOL_TCP then
+      {
+        tcp_dst: port_number,
+        ip_proto: protocol
+      }
+    when IPV4_PROTOCOL_UDP then
+      {
+        udp_dst: port_number,
+        ip_proto: protocol
+      }
+    end
+end
+
+def rule_flow(direction, protocol, ipv4_address, prefix, port_number)
+  case direction
+  when "ingress" then
+    match_ipv4_subnet_src(ipv4_address, prefix).merge(protocol_type(protocol, port_number))
+  when "egress" then
+    match_ipv4_subnet_dst(ipv4_address, prefix).merge(protocol_type(protocol, port_number))
+  else
+    return
+  end
+end
+
+def static_priority(prefix, port, passthrough)
+  (prefix << 1) + ((port.nil? || port == 0) ? 0 : 2) + (passthrough ? 1 : 0)
+end
+
 describe Vnet::Core::Filter2Manager do
 
   use_mock_event_handler
@@ -16,32 +76,17 @@ describe Vnet::Core::Filter2Manager do
   let(:filter2_manager) { datapath.dp_info.filter2_manager }
   let(:interface_manager) { datapath.dp_info.interface_manager }
 
-  let(:interface) { Fabricate(:filter_interface,
-                              uuid: "if-filter",
-                              ingress_filtering_enabled: false,
-                              enable_filtering: true
-                             ) }
-  
   let(:filter) { Fabricate(:filter,
                            uuid: "fil-test",
-                           interface_id: "if-filter",
+                           interface_id: 1,
                            mode: "static",
                           ) }
 
-  let(:filter_static) { Fabricate(:filter_static,
-                                  protocol: "tcp",
-                                  ipv4_src_address: IPAddr.new("10.101.0.11").to_i,
-                                  ipv4_dst_address: IPAddr.new("10.101.0.11").to_i,
-                                  port_src_first: 80,
-                                  port_src_last: 80,
-                                  port_dst_first: 80,
-                                  port_dst_last: 80,
-                                  ipv4_src_prefix: 32,
-                                  ipv4_dst_prefix: 32,
-                                  passthrough: true
-                                 ) }
+
+  let(:filter_static) { Fabricate(:static_tcp_pass) }
+
   before(:each) do
-    filter2_manager.publish(Vnet::Event::FILTER_ACTIVATE_INTERFACE, id: :interface, interface_id: 0)
+    filter2_manager.publish(Vnet::Event::FILTER_ACTIVATE_INTERFACE, id: :interface, interface_id: 1)
   end
 
   describe "#created_item" do
@@ -52,40 +97,61 @@ describe Vnet::Core::Filter2Manager do
 
     context "with with passthrough set to false" do
       it "creates the a filter item" do
-        expect(flows).to include flow_create(table: TABLE_INTERFACE_INGRESS_FILTER,
-                                             priority: 10,
-                                             match_interface: 0,
-                                             cookie: filter.id | COOKIE_TYPE_FILTER2
-                                            )
-        expect(flows).to include flow_create(table: TABLE_INTERFACE_EGRESS_FILTER,
-                                             priority: 10,
-                                             match_interface: 0,
-                                             cookie: filter.id | COOKIE_TYPE_FILTER2
-                                            )
+
+        filter_hash = [
+            filter.to_hash,
+            ingress_tables(filter.ingress_passthrough),
+            { priority: 10 }
+        ]
+
+        expect(flows).to include flow(merged_hash_array(filter_hash))
+
+        filter_hash = [
+          filter.to_hash,
+          ingress_tables(filter.ingress_passthrough),
+          { priority: 10 }
+        ]
+
+        expect(flows).to include flow(merged_hash_array(filter_hash))
+
       end
     end
 
     context "with with passthrough set to true" do
       let(:filter) { Fabricate(:filter,
                                uuid: "fil-test",
-                               interface_id: "if-filter",
+                               interface_id: 1,
                                mode: "static",
                                egress_passthrough: true,
                                ingress_passthrough: true
-                              ) }
+                              )
+      }
+
       it "creates the a filter item" do
-        expect(flows).to include flow_create(table: TABLE_INTERFACE_INGRESS_FILTER,
-                                             goto_table: TABLE_OUT_PORT_INTERFACE_INGRESS,
-                                             priority: 10,
-                                             match_interface: 0,
-                                             cookie: 1 | COOKIE_TYPE_FILTER2
-                                            )
-        expect(flows).to include flow_create(table: TABLE_INTERFACE_EGRESS_FILTER,
-                                             goto_table: TABLE_INTERFACE_EGRESS_VALIDATE,
-                                             priority: 10,
-                                             match_interface: 0,
-                                             cookie: 1 | COOKIE_TYPE_FILTER2
-                                            )
+
+        filter_hash = [ filter.to_hash, ingress_tables(filter.ingress_passthrough), { priority: 10 } ]
+        expect(flows).to include flow(merged_hash_array(filter_hash))
+
+        filter_hash = [ filter.to_hash, ingress_tables(filter.ingress_passthrough), { priority: 10 } ]
+        expect(flows).to include flow(merged_hash_array(filter_hash))
+
+      end
+    end
+  end
+
+  describe "#updated_item" do
+    before(:each) do
+      filter2_manager.publish(Vnet::Event::FILTER_UPDATED, filter.to_hash)
+      sleep(3)
+    end
+
+    context "with with ingress passthrough set false, egress_passthrough set true" do
+      it "updates the a filter item" do
+      end
+    end
+
+    context "with with ingress passthrough set true, egress_passthrough set false" do
+      it "updates the a filter item" do
       end
     end
   end
@@ -94,6 +160,7 @@ describe Vnet::Core::Filter2Manager do
     before(:each) do
       filter2_manager.publish(Vnet::Event::FILTER_CREATED_ITEM, filter.to_hash)
       sleep(3)
+
       model_hash = filter_static.to_hash.merge(id: filter.id, static_id: filter_static.id)
       filter2_manager.publish(Vnet::Event::FILTER_ADDED_STATIC, model_hash)
       sleep(3)
@@ -101,38 +168,41 @@ describe Vnet::Core::Filter2Manager do
 
     context "when protocol is tcp and passthrough is enabled" do
       it "adds the static" do
-        expect(flows).to include flow_create(table: TABLE_INTERFACE_INGRESS_FILTER,
-                                             goto_table: TABLE_OUT_PORT_INTERFACE_INGRESS,
-                                             priority: 20 + 32,
-                                             match_interface: 0,
-                                             cookie: 1 | COOKIE_TYPE_FILTER2,
-                                             match: {
-                                               eth_type: ETH_TYPE_IPV4,
-                                               ipv4_src: IPAddr.new("10.101.0.11").to_i,
-                                               ipv4_src_mask: IPV4_BROADCAST << (32 - 32),
-                                               ip_proto: IPV4_PROTOCOL_TCP,
-                                               tcp_dst: 80
-                                             })
 
-        expect(flows).to include flow_create(table: TABLE_INTERFACE_EGRESS_FILTER,
-                                             goto_table: TABLE_INTERFACE_EGRESS_VALIDATE,
-                                             priority: 20 + 32,
-                                             match_interface: 0,
-                                             cookie: 1 | COOKIE_TYPE_FILTER2,
-                                             match: {
-                                               eth_type: ETH_TYPE_IPV4,
-                                               ipv4_dst: IPAddr.new("10.101.0.11").to_i,
-                                               ipv4_dst_mask: IPV4_BROADCAST << (32 - 32),
-                                               ip_proto: IPV4_PROTOCOL_TCP,
-                                               tcp_dst: 80
-                                             })
+        filter_hash = [
+          filter.to_hash,
+          ingress_tables(filter_static.passthrough),
+          { priority: 20 + static_priority(filter_static.ipv4_src_prefix,
+                                           filter_static.passthrough,
+                                           filter_static.port_src_first) },
+          { match: rule_flow("ingress", IPV4_PROTOCOL_TCP,
+                             filter_static.port_src_first,
+                             filter_static.ipv4_src_address,
+                             filter_static.ipv4_src_prefix) }
+        ]
+        expect(flows).to include flow(merged_hash_array(filter_hash))
+
+        filter_hash = [
+          filter.to_hash,
+          egress_tables(filter_static.passthrough),
+          { priority: 20 + static_priority(filter_static.ipv4_dst_prefix,
+                                           filter_static.passthrough,
+                                           filter_static.port_dst_first) },
+          { match: rule_flow("egress", IPV4_PROTOCOL_TCP,
+                       filter_static.port_dst_first,
+                       filter_static.ipv4_dst_address,
+                       filter_static.ipv4_dst_prefix) }
+        ]
+        expect(flows).to include flow(merged_hash_array(filter_hash))
+
       end
     end
-  end
-  
+ end
+
   describe "#remove static" do
     before(:each) do
     end
+
     it "removes a static rule" do
     end
   end
