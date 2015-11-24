fa76e5154f820db7baa1d04ac359665f79504bb0
toros11 <tobias.rosen@axsh.net>
2015-11-24 22:06:17 +0900

    fabrication templates for filters

--- vnet/spec/fabricators/filter_static_fabricator.rb
+++ vnet/spec/fabricators/filter_static_fabricator.rb
@@ -1,3 +1,29 @@
 Fabricator(:filter_static,
   class_name: Vnet::Models::FilterStatic
 )
+
+Fabricator(:static_enable_tcp, class_name: Vnet::Models::FilterStatic) do
+  protocol "tcp",
+  ipv4_src_address IPAddr.new("10.101.0.11").to_i,
+  ipv4_dst_address IPAddr.new("10.101.0.11").to_i,
+  port_src_first 80,
+  port_src_last 80,
+  port_dst_first  80,
+  port_dst_last 80,
+  ipv4_src_prefix 32, 
+  ipv4_dst_prefix 32,
+  passthrough true
+end
+
+Fabricator(:static_enable_udp, class_name: Vnet::Models::FilterStatic) do
+  protocol "upd",
+  ipv4_src_address IPAddr.new("10.101.0.11").to_i,
+  ipv4_dst_address IPAddr.new("10.101.0.11").to_i,
+  port_src_first 80,
+  port_src_last 80,
+  port_dst_first  80,
+  port_dst_last 80,
+  ipv4_src_prefix 32, 
+  ipv4_dst_prefix 32,
+  passthrough true
+end
