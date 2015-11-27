817ba71d1cf3c6bfa43fd9322201d73f78b39388
toros11 <tobias.rosen@axsh.net>
2015-11-27 17:05:18 +0900

    updated validations

--- vnet/lib/vnet/models/filter_static.rb
+++ vnet/lib/vnet/models/filter_static.rb
@@ -22,15 +22,11 @@ module Vnet::Models
       errors.add(:ipv4_dst_prefix, "prefix out of range") if !(0..32).member?(self.ipv4_dst_prefix)
 
       if protocol_included(["tcp", "udp"])
-        errors.add(:port_src_first, "port not in valid range") if !(0..0xffff).member?(self.port_src_first)
-        errors.add(:port_dst_first, "port not in valid range") if !(0..0xffff).member?(self.port_dst_first)
-        errors.add(:port_src_last, "port not in valid range") if !(self.port_src_first..0xffff).member?(self.port_src_last)
-        errors.add(:port_dst_last, "port not in valid range") if !(self.port_dst_first..0xffff).member?(self.port_dst_last)
+        errors.add(:port_src, "port not in valid range") if !(0..0xffff).member?(self.port_src)
+        errors.add(:port_dst, "port not in valid range") if !(0..0xffff).member?(self.port_dst)
       elsif protocol_included(["arp", "icmp", "all"])
-        errors.add(:port_src_first, "port needs to be nil") if !self.port_src_first.nil?
-        errors.add(:port_dst_first, "port needs to be nil") if !self.port_dst_first.nil?
-        errors.add(:port_src_last, "port needs to be nil") if !self.port_dst_last.nil?
-        errors.add(:port_dst_last, "port needs to be nil") if !self.port_src_last.nil?
+        errors.add(:port_src, "port needs to be nil") if !self.port_src.nil?
+        errors.add(:port_dst, "port needs to be nil") if !self.port_dst.nil?
       elsif protocol_included(["arp", "all"])
         errors.add(:ipv4_src_address, "ip address needs to be 0") if (self.ipv4_src_address > 0) # needs inspection
         errors.add(:ipv4_dst_address, "ip address needs to be 0") if (self.ipv4_dst_address > 0) # needs inspection
