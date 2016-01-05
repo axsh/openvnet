aac9be784b77d1befb150481ac27aee8468d95c7
rakshasa <sundell.software@gmail.com>
2016-01-05 12:14:05 +0900

    Renamed route_type to mode in routes.

--- vnet/db/migrations/0007_mode_only.rb
+++ vnet/db/migrations/0007_mode_only.rb
@@ -3,9 +3,11 @@
 Sequel.migration do
   up do
     rename_column :network_services, :type, :mode
+    rename_column :routes, :route_type, :mode
   end
 
   down do
     rename_column :network_services, :mode, :type
+    rename_column :routes, :mode, :route_type
   end
 end
