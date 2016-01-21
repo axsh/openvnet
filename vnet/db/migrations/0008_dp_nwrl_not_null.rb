b66056bf13b2de4e6699597ac069c8a6612a208a
rakshasa <sundell.software@gmail.com>
2016-01-21 20:49:37 +0900

    Disable not-null for dp_nw/rl's ip_lease_id for now.

--- vnet/db/migrations/0008_dp_nwrl_not_null.rb
+++ vnet/db/migrations/0008_dp_nwrl_not_null.rb
@@ -4,24 +4,24 @@ Sequel.migration do
   up do
     alter_table(:datapath_networks) do
       set_column_not_null :mac_address_id
-      set_column_not_null :ip_lease_id
+      #set_column_not_null :ip_lease_id
     end
 
     alter_table(:datapath_route_links) do
       set_column_not_null :mac_address_id
-      set_column_not_null :ip_lease_id
+      #set_column_not_null :ip_lease_id
     end
   end
 
   down do
     alter_table(:datapath_networks) do
       set_column_allow_null :mac_address_id
-      set_column_allow_null :ip_lease_id
+      #set_column_allow_null :ip_lease_id
     end
 
     alter_table(:datapath_route_links) do
       set_column_allow_null :mac_address_id
-      set_column_allow_null :ip_lease_id
+      #set_column_allow_null :ip_lease_id
     end
   end
 end
