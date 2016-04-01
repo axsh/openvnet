fbf66773782e110bc4d39c064d88d46f2b83a973
Metallion <akieckens@axsh.net>
2016-04-01 18:43:52 +0900

    Fixed some dirty whitespace

--- vnet/lib/vnet/helpers/ip_address.rb
+++ vnet/lib/vnet/helpers/ip_address.rb
@@ -2,14 +2,14 @@
 
 module Vnet::Helpers
   class IpAddress
-  
+
     def self.valid_in_subnet(network, ipv4_address)
       if (ipv4_address == 0)
         return true
       end
       ipv4_nw = IPAddress::IPv4::parse_u32(network.ipv4_network, network.ipv4_prefix)
       ipv4 = IPAddress::IPv4::parse_u32(ipv4_address, network.ipv4_prefix)
-    
+
       ipv4_nw.include?(ipv4)
     end
   end
