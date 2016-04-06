ec01521d04745b70269863a666a1972e832e6009
rakshasa <sundell.software@gmail.com>
2016-04-06 12:25:09 +0900

    Added ip_lease attach/release to vnctl.

--- client/vnctl/lib/vnctl/cli/ip_lease.rb
+++ client/vnctl/lib/vnctl/cli/ip_lease.rb
@@ -6,13 +6,27 @@ module Vnctl::Cli
     api_suffix "ip_leases"
 
     add_modify_shared_options {
-      option :network_uuid, :type => :string, :desc => "The network to lease this ip in."
-      option :mac_lease_uuid, :type => :string, :desc => "The mac lease that this ip lease is tried to."
-      option :ipv4_address, :type => :string, :desc => "The ipv4 address to lease."
       option :enable_routing, :type => :boolean, :desc => "Flag that decides whether or not routing is enabled for this ip lease."
     }
     set_required_options [:network_uuid, :mac_lease_uuid, :ipv4_address]
 
     define_standard_crud_commands
+
+    desc "attach UUID", "Attach a #{namespace} to an interface."
+    # option_uuid
+    option :interface_uuid, :type => :string, :required => false,
+    :desc => "Attach to interface UUID, using the first mac lease if not specified."
+    option :mac_lease_uuid, :type => :string, :required => false,
+    :desc => "Attach to mac lease UUID and its interface."
+    define_method(:attach) do |uuid|
+      puts Vnctl.webapi.put("#{suffix}/#{uuid}/attach", options)
+    end
+
+    desc "release UUID", "Release a #{namespace} from its interface."
+    # option_uuid
+    define_method(:release) do |uuid|
+      puts Vnctl.webapi.put("#{suffix}/#{uuid}/release", options)
+    end
+
   end
 end
