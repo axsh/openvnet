501a2f6e153817dbe1a4be1644c956668c1a30e2
rakshasa <sundell.software@gmail.com>
2016-05-09 19:08:32 +0900

    Added segment to sequel plugins.

--- vnet/lib/sequel/plugins/mac_address.rb
+++ vnet/lib/sequel/plugins/mac_address.rb
@@ -5,9 +5,10 @@ module Sequel
     module MacAddress
       def self.apply(model, opts = OPTS)
         association_name = (opts[:attr_name] ? :mac_address : :_mac_address)
-        model.many_to_one association_name, class: model.name.split(/::/).tap{|n| n[-1] = "MacAddress"}.join("::"), key: :mac_address_id
-
         mac_address_attr_name = opts[:attr_name] || :mac_address
+
+        model.many_to_many :segments, :join_table => :mac_addresses, :left_key => :id, :left_primary_key => :mac_address_id, :right_key => :segment_id
+        model.many_to_one association_name, class: model.name.split(/::/).tap{|n| n[-1] = "MacAddress"}.join("::"), key: :mac_address_id
 
         model.class_eval do
           define_method :mac_address_attr_name do
@@ -19,17 +20,46 @@ module Sequel
           end
 
           define_method mac_address_attr_name do
-            return instance_variable_get("@#{mac_address_attr_name}") if instance_variable_get("@#{mac_address_attr_name}")
-            instance_variable_set("@#{mac_address_attr_name}", __send__(mac_address_association_name) ? __send__(mac_address_association_name).mac_address : nil)
+            if instance_variable_get("@#{mac_address_attr_name}")
+              return instance_variable_get("@#{mac_address_attr_name}")
+            end
+
+            result = __send__(mac_address_association_name)
+
+            instance_variable_set("@#{mac_address_attr_name}", result && result.mac_address)
           end
 
+          # TODO: Remove as it should be read-only? Same for ip_address.
           define_method "#{mac_address_attr_name}=" do |mac_address|
             if mac_address != __send__(mac_address_attr_name)
               instance_variable_set("@#{mac_address_attr_name}", mac_address)
               modified!(mac_address_attr_name)
             end
+
             instance_variable_get("@#{mac_address_attr_name}")
           end
+
+          def segment
+            @segment || segments.first
+          end
+
+          def segment_id
+            return @segment_id if @segment_id
+
+            result = __send__(mac_address_association_name)
+
+            @segment_id = result && result.segment_id
+          end
+
+          def segment_id=(segment_id)
+            if segment_id != self.segment_id
+              @segment_id = segment_id
+              modified!(:segment_id)
+            end
+
+            @segment_id
+          end
+
         end
       end
 
@@ -37,22 +67,28 @@ module Sequel
         def before_save
           if value = __send__(mac_address_attr_name)
             m = __send__(mac_address_association_name)
+
             if m && m.mac_address != value
               m.destroy
               __send__("#{mac_address_association_name}=", nil)
             end
-            unless __send__(mac_address_association_name)
-              __send__("#{mac_address_association_name}=", self.class.association_reflection(mac_address_association_name).associated_class.create(mac_address: value))
+
+            if __send__(mac_address_association_name).nil?
+              assoc_class = self.class.association_reflection(mac_address_association_name).associated_class
+
+              __send__("#{mac_address_association_name}=",
+                assoc_class.create(mac_address: value, segment_id: @segment_id))
             end
           end
+
           super
         end
 
         def to_hash
-          super.merge({
-            mac_address_attr_name.to_sym => __send__(mac_address_attr_name)
-          })
+          super.merge({mac_address_attr_name.to_sym => __send__(mac_address_attr_name),
+                       :segment_id => segment_id})
         end
+
       end
     end
   end
