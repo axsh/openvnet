474aa3cb0967d6e63a020f0a4394afbc56ef46ab
Metallion <akieckens@axsh.net>
2016-05-18 16:47:21 +0900

    Added some more doctor examination results for the datapath_mac_range_group parameter

--- vnet/lib/vnet/doctor/vnmgr.rb
+++ vnet/lib/vnet/doctor/vnmgr.rb
@@ -10,7 +10,19 @@ module Vnet
 
         conf = Vnet::Configurations::Vnmgr.conf
 
-        if conf.datapath_mac_group
+        if mg_uuid = conf.datapath_mac_group
+          if Vnet::Models::MacRangeGroup[mg_uuid]
+            results << [:success,
+                        "The datapath_mac_group UUID was set and found in the database. " +
+                        "OpenVNet will be able to auto-assign MAC addresses"
+                       ]
+          else
+            results << [:warning,
+                        "The datapath_mac_group UUID set in /etc/openvnet/common.conf " +
+                        "was not found in the database. " +
+                        "OpenVNet will not be able to auto-assign MAC addresses."
+                       ]
+          end
         else
           results << [:warning,
                       "The datapath_mac_group UUID set in /etc/openvnet/common.conf. " +
