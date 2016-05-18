077a7eb84d27ed4bfec3326cd3c4468f72231ae7
rakshasa <sundell.software@gmail.com>
2016-05-18 12:26:25 +0900

    Added flows for segments.

--- vnet/lib/vnet/core/segments/virtual.rb
+++ vnet/lib/vnet/core/segments/virtual.rb
@@ -17,33 +17,33 @@ module Vnet::Core::Segments
     end
 
     def install
-      # network_md = md_create(:network => @id)
-      # fo_network_md = flow_options.merge(network_md)
+      # segment_md = md_create(:segment => @id)
+      # fo_segment_md = flow_options.merge(segment_md)
 
-      # flows = []
-      # flows << Flow.create(TABLE_TUNNEL_NETWORK_IDS, 30, {
+      flows = []
+      # flows << Flow.create(TABLE_TUNNEL_SEGMENT_IDS, 30, {
       #                        :tunnel_id => @id | TUNNEL_FLAG_MASK
       #                      }, nil,
-      #                      fo_network_md.merge(:goto_table => TABLE_NETWORK_SRC_CLASSIFIER))
+      #                      fo_segment_md.merge(:goto_table => TABLE_SEGMENT_SRC_CLASSIFIER))
 
-      # flows << flow_create(table: TABLE_NETWORK_SRC_CLASSIFIER,
-      #                      goto_table: TABLE_ROUTE_INGRESS_INTERFACE,
-      #                      priority: 30,
-      #                      match_network: @id)
-      # flows << flow_create(table: TABLE_NETWORK_SRC_CLASSIFIER,
-      #                      goto_table: TABLE_NETWORK_SRC_MAC_LEARNING,
+      flows << flow_create(table: TABLE_SEGMENT_SRC_CLASSIFIER,
+                           goto_table: TABLE_SEGMENT_DST_CLASSIFIER,
+                           priority: 30,
+                           match_segment: @id)
+      # flows << flow_create(table: TABLE_SEGMENT_SRC_CLASSIFIER,
+      #                      goto_table: TABLE_SEGMENT_SRC_MAC_LEARNING,
       #                      priority: 40,
       #                      match: {
       #                        :eth_type => 0x0806
       #                      },
       #                      match_remote: true,
-      #                      match_network: @id)
-      # flows << flow_create(table: TABLE_NETWORK_DST_CLASSIFIER,
-      #                      goto_table: TABLE_NETWORK_DST_MAC_LOOKUP,
-      #                      priority: 30,
-      #                      match_network: @id)
+      #                      match_segment: @id)
+      flows << flow_create(table: TABLE_SEGMENT_DST_CLASSIFIER,
+                           goto_table: TABLE_SEGMENT_DST_MAC_LOOKUP,
+                           priority: 30,
+                           match_segment: @id)
 
-      # @dp_info.add_flows(flows)
+      @dp_info.add_flows(flows)
 
       # ovs_flows = []
       # ovs_flows << create_ovs_flow_learn_arp(3, "tun_id=0,")
@@ -58,7 +58,7 @@ module Vnet::Core::Segments
 
       # flows = []
       # flows << Flow.create(TABLE_FLOOD_LOCAL, 1,
-      #                      md_create(:network => @id),
+      #                      md_create(:segment => @id),
       #                      flood_actions, flow_options.merge(:goto_table => TABLE_FLOOD_TUNNELS))
 
       # @dp_info.add_flows(flows)
@@ -68,17 +68,17 @@ module Vnet::Core::Segments
       #
       # Work around the current limitations of trema / openflow 1.3 using ovs-ofctl directly.
       #
-      # match_md = md_create(network: @id, remote: nil)
-      # learn_md = md_create(network: @id, local: nil)
+      # match_md = md_create(segment: @id, remote: nil)
+      # learn_md = md_create(segment: @id, local: nil)
 
-      # flow_learn_arp = "table=#{TABLE_NETWORK_SRC_MAC_LEARNING},priority=#{priority},cookie=0x%x,arp,metadata=0x%x/0x%x,#{match_options}actions=" %
+      # flow_learn_arp = "table=#{TABLE_SEGMENT_SRC_MAC_LEARNING},priority=#{priority},cookie=0x%x,arp,metadata=0x%x/0x%x,#{match_options}actions=" %
       #   [@cookie, match_md[:metadata], match_md[:metadata_mask]]
       # flow_learn_arp << "learn\\(table=%d,cookie=0x%x,idle_timeout=36000,priority=35,metadata:0x%x,NXM_OF_ETH_DST\\[\\]=NXM_OF_ETH_SRC\\[\\]," %
-      #   [TABLE_NETWORK_DST_MAC_LOOKUP, cookie, learn_md[:metadata]]
+      #   [TABLE_SEGMENT_DST_MAC_LOOKUP, cookie, learn_md[:metadata]]
 
       # flow_learn_arp << learn_options
 
-      # flow_learn_arp << "output:NXM_OF_IN_PORT\\[\\]\\),goto_table:%d" % TABLE_NETWORK_DST_CLASSIFIER
+      # flow_learn_arp << "output:NXM_OF_IN_PORT\\[\\]\\),goto_table:%d" % TABLE_SEGMENT_DST_CLASSIFIER
       # flow_learn_arp
     end
 
