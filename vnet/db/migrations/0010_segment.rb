53cd4c8411e1e2233c290893676a9fbcf49b4b03
rakshasa <sundell.software@gmail.com>
2016-05-19 15:21:36 +0900

    Added dp_segment to migration.

--- vnet/db/migrations/0010_segment.rb
+++ vnet/db/migrations/0010_segment.rb
@@ -20,10 +20,30 @@ Sequel.migration do
       # Add index for segment_id.
     end
 
+    create_table(:datapath_segments) do
+      primary_key :id
+
+      # TODO: Review indices and null.
+      Integer :datapath_id, :index => true, :null=>false
+      Integer :segment_id, :index => true, :null=>false
+
+      Integer :interface_id, :index => true, :null=>true
+      Integer :mac_address_id, :index => true, :null=>false
+      Integer :ip_lease_id, :index => true
+
+      DateTime :created_at, :null=>false
+      DateTime :updated_at, :null=>false
+      DateTime :deleted_at, :index => true
+      Integer :is_deleted, :null=>false, :default=>0
+
+      unique [:datapath_id, :segment_id, :is_deleted]
+    end
+
   end
 
   down do
     drop_table(:segments,
+               :datapath_segments,
                )
 
     alter_table(:mac_addresses) do
