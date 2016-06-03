dc586960a2599327e1a354cad17c652c4aa4e320
Akira Yokokawa <a-yokokawa@axsh.net>
2016-06-03 12:40:11 +0900

    Modify the test case according to the new version of ping method.

--- integration_test/lib/vnspec/spec/wanedge_spec.rb
+++ integration_test/lib/vnspec/spec/wanedge_spec.rb
@@ -2,7 +2,12 @@
 require_relative "spec_helper"
 
 describe "wanedge" do
-  it "reaches to 8.8.8.8" do
-    expect(vm1).to be_able_to_ping("8.8.8.8")
+  describe "from vnet1 to the internet" do
+    it "reaches to 8.8.8.8" do
+      to_dns = double()
+      allow(to_dns).to receive(:ipv4_address).and_return("8.8.8.8")
+
+      expect(vm1).to be_able_to_ping(to_dns, 10)
+    end
   end
 end
