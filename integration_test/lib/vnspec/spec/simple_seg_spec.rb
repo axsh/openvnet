493c616b48adf7b7121386621f4dcf351a4d46a0
rakshasa <sundell.software@gmail.com>
2016-06-16 18:39:45 +0900

    Added initial config files for simple segment integration test.

---
+++ integration_test/lib/vnspec/spec/simple_seg_spec.rb
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+require_relative "spec_helper"
+
+describe "simple_seg" do
+  describe "vnet1" do
+    context "mac2mac" do
+      it "reachable to vnet1" do
+        expect(vm1).to be_reachable_to(vm3)
+      end
+
+      it "not reachable to vnet2" do
+        expect(vm1).not_to be_reachable_to(vm4)
+      end
+    end
+
+    context "mac2mac over gre tunnel" do
+      it "reachable to vnet1" do
+        expect(vm1).to be_reachable_to(vm5)
+      end
+
+      it "not reachable to vnet2" do
+        expect(vm1).not_to be_reachable_to(vm6)
+      end
+    end
+  end
+
+  describe "vnet2" do
+    context "mac2mac" do
+      it "reachable to vnet2" do
+        expect(vm2).to be_reachable_to(vm4)
+      end
+
+      it "not reachable to vnet1" do
+        expect(vm2).not_to be_reachable_to(vm3)
+      end
+    end
+
+    context "mac2mac over gre tunnel" do
+      it "reachable to vnet2" do
+        expect(vm2).to be_reachable_to(vm6)
+      end
+
+      it "not reachable to vnet1" do
+        expect(vm2).not_to be_reachable_to(vm5)
+      end
+    end
+  end
+end
