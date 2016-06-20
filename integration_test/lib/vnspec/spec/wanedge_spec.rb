e514cf1436a6dca426296d5517c3f3f47748d953
Akira Yokokawa <a-yokokawa@axsh.net>
2016-06-20 16:14:33 +0900

    Ping to gateway

--- integration_test/lib/vnspec/spec/wanedge_spec.rb
+++ integration_test/lib/vnspec/spec/wanedge_spec.rb
@@ -2,12 +2,12 @@
 require_relative "spec_helper"
 
 describe "wanedge" do
-  describe "from vnet1 to the internet" do
-    it "reaches to 8.8.8.8" do
-      to_dns = double()
-      allow(to_dns).to receive(:ipv4_address).and_return("8.8.8.8")
+  describe "from vnet1" do
+    it "reaches to the gateway" do
+      to_gw = double()
+      allow(to_gw).to receive(:ipv4_address).and_return("10.210.0.1")
 
-      expect(vm1).to be_able_to_ping(to_dns, 10)
+      expect(vm1).to be_able_to_ping(to_gw, 10)
     end
   end
 end
