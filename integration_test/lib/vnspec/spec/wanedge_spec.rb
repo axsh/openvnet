c9108e7ec0b3c33af51c7fee58efe4805be35ebf
Akira Yokokawa <a-yokokawa@axsh.net>
2016-06-21 10:28:25 +0900

    Add a test case to test internet reachability.

--- integration_test/lib/vnspec/spec/wanedge_spec.rb
+++ integration_test/lib/vnspec/spec/wanedge_spec.rb
@@ -2,10 +2,17 @@
 require_relative "spec_helper"
 
 describe "wanedge" do
-  describe "from vnet1" do
+  describe "vm1 in vnet1" do
     it "reaches to the gateway" do
       to_gw = double()
-      allow(to_gw).to receive(:ipv4_address).and_return("10.210.0.1")
+      allow(to_gw).to receive(:ipv4_address).and_return(config[:physical_network_gw_ip])
+
+      expect(vm1).to be_able_to_ping(to_gw, 10)
+    end
+
+    it "reaches to the internet" do
+      to_gw = double()
+      allow(to_gw).to receive(:ipv4_address).and_return(config[:pseudo_global_ip])
 
       expect(vm1).to be_able_to_ping(to_gw, 10)
     end
