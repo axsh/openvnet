addb88ebbf83e20756548c74c2e152b3f02b6c6f
rakshasa <sundell.software@gmail.com>
2016-07-22 05:07:46 +0900

    Added initial files for promiscuous CI test.

---
+++ integration_test/lib/vnspec/spec/promiscuous_spec.rb
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+require_relative "spec_helper"
+
+describe "promiscuous" do
+  describe "vm1 in vnet1" do
+    it "reaches to the gateway" do
+      to_gw = double()
+      allow(to_gw).to receive(:ipv4_address).and_return(config[:physical_network_gw_ip])
+
+      expect(vm1).to be_able_to_ping(to_gw, 10)
+    end
+
+    it "reaches to the internet" do
+      to_gw = double()
+      allow(to_gw).to receive(:ipv4_address).and_return(config[:pseudo_global_ip])
+
+      expect(vm1).to be_able_to_ping(to_gw, 10)
+    end
+  end
+end
