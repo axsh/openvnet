e51f8dfcd1391fe3f8f990e7d154c4da0ca111fb
toros11 <tobias.rosen@axsh.net>
2016-08-29 21:43:19 +0900

    Created resource map for terraform provider

--- client/terraform-provider-openvnet/openvnet/provider.go
+++ client/terraform-provider-openvnet/openvnet/provider.go
@@ -4,3 +4,16 @@ import (
 	"github.com/hashicorp/terraform/helper/schema"
 	"github.com/hashicorp/terraform/terraform"
 )
+
+func Provider () terraform.ResourceProvider {
+	return &schema.Provider{
+		ResourcesMap: map[string]*schema.Resource{
+			"openvnet_datapath":   resourceOpenVNetDatapaht(),
+			"openvnet_interface":  resourceOpenVNetInterface(),
+			"openvnet_network":    resourceOpenVNetNetwork(),
+			"openvnet_route":      resourceOpenVNetRoute(),
+			"openvnet_route_link": resourceOpenVNetRouteLink(),
+			"openvnet_segment":    resourceOpenVNetSegment(),
+		},
+	}
+}
