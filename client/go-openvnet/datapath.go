fef93d700de416ec358ad1f7c645f6fac85915d1
toros11 <tobias.rosen@axsh.net>
2016-08-30 00:08:03 +0900

    Work on (de)serializing the datapath relation items

--- client/go-openvnet/datapath.go
+++ client/go-openvnet/datapath.go
@@ -15,6 +15,22 @@ type Datapath struct {
 	DeletedAt   string `json:"deleted_at"`
 }
 
+type DatapathRelation struct {
+	ID            int    `json:"id"`
+	DPID          int    `json:"datapath_id"`
+	NetworkID     int    `json:"network_id,omitempty"`
+	SegmentID     int    `json:"segment_id,omitempty"`
+	RouteLinkID   int    `json:"route_link_id,omitempty"`
+	InterfaceID   int    `json:"interface_id"`
+	MacAddresssID int    `json:"mac_address_id"`
+	IpLeaseID     int    `json:"ip_lease_id"`
+	CreatedAt     string `json:"mac_address"`
+	UpdatedAt     string `json:"created_at"`
+	DeletedAt     string `json:"deleted_at"`
+	IsDeleted     int    `json:"is_deleted"`
+	MacAddress    string `json:"mac_address"`
+}
+
 type DatapathService struct {
 	client *Client
 
@@ -26,6 +42,19 @@ type DatapathCreateParams struct {
 	DisplayName string `url:"display_name"`
 	DPID        string `url:"dpid"`
 	NodeId      string `url:"node_id"`
+
+}
+
+type DatapathRelationType struct {
+	Params     *DatapathRelationCreateParams
+	DatapathID string
+	Type       string
+	ID         string
+}
+
+type DatapathRelationCreateParams struct {
+	InterfaceUUID string `url:"interface_uuid"`
+	MacAddress    string `url:"mac_address,omitempty"`
 }
 
 func (s *DatapathService) Create(params *DatapathCreateParams) (*Datapath, *http.Response, error) {
@@ -37,4 +66,14 @@ func (s *DatapathService) Create(params *DatapathCreateParams) (*Datapath, *http
 
 func (s *DatapathService) Delete(id string) (*http.Response, error) {
 	return nil, nil
+}
+
+func (s *DatapathService) CreateDatapathRelation(rel*DatapathRelationType) (*DatapathRelation, *http.Response, error) {
+	dpr := new(DatapathRelation)
+	ovnError := new(OpenVNetError)
+	ns := s.Namespace +"/"+ rel.DatapathID +"/"+ rel.Type +"/"+ rel.ID +".yml"
+
+	resp, err := s.client.sling.New().Post(ns).BodyForm(rel.Params).Receive(dpr, ovnError)
+
+	return dpr, resp, err
 }
