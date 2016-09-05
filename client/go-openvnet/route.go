063a8d57aa645ed4f952591e3822444335b85e4c
toros11 <tobias.rosen@axsh.net>
2016-09-05 19:54:16 +0900

    Added route type and service

---
+++ client/go-openvnet/route.go
@@ -0,0 +1,47 @@
+package openvnet
+
+import (
+	"net/http"
+)
+
+const RouteNamespace = "routes"
+
+type Route struct {
+	UUID          string `json:"uuid"`
+	RouteLinkUUID string `json:"route_link_uuid"`
+	NetworkUUID   string `json:"network_uuid"`
+	InterfaceUUID string `json:"interface_uuid"`
+	Mode          string `json:"mode"`
+	Ipv4Network   string `json:"ipv4_network"`
+	Ipv4Prefix    int    `json:"ipv4_prefix"`
+	Ingress       bool   `json:"ingress"`
+	Egress        bool   `json:"egress"`
+	CreatedAt     string `json:"created_at"`
+	DeletedAt     string `json:"deleted_at"`
+}
+
+type RouteService struct {
+	client *Client
+}
+
+type RouteCreateParams struct {
+	UUID          string `url:"uuid,omitempty"`
+	InterfaceUUID string `url:"interface_uuid,omitempty"`
+	RouteLinkUUID string `url:"route_link_uuid"`
+	NetworkUUID   string `url:"network_uuid"`
+	Ipv4Network   string `url:"ipv4_network,omitempty"`
+	Ipv4Prefix    ing    `url:"ipv4_prefix,omitempty"`
+	Ingress       bool   `url:"ingress,omitempty"`
+	Egress        bool   `url:"egress,omitempty"`
+}
+
+func (s *RouteService) Create (params *RouteCreateParams) (*Route, *http.Response, error) {
+	r := new(Route)
+	resp, err := s.client.post(RouteNamespace, r, params)
+	return r, resp, err
+
+}
+
+func (s *RouteService) Delete (id string) (*http.Response, error) {
+	return s.client.del(id)
+}
