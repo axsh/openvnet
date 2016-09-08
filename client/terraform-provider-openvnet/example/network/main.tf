398e290c739e667c096b4614b51f04496d1ebfdf
b0r6 <tommy@axsh.net>
2016-09-08 13:27:46 +0900

    Fixed variable name

--- client/terraform-provider-openvnet/example/network/main.tf
+++ client/terraform-provider-openvnet/example/network/main.tf
@@ -6,7 +6,7 @@ resource "openvnet_network" "nw_sample" {
 	 uuid = "nw-demo"
 	 display_name = "sample network with segments"
 	 ipv4_network = "10.0.0.0
-	 ipv4_network = "24"
+	 ipv4_prefix = "24"
 	 network_mode = "virtual"
 	 segment_uuid = "seg-demo"
 	 editable = true
