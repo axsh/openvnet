ea4d967a4a6c03610cffaeab56260e2e83972e83
toros11 <tobias.rosen@axsh.net>
2016-09-13 17:53:49 +0900

    Rearranged code and added delete function for datapath relations

--- client/go-openvnet/datapath.go
+++ client/go-openvnet/datapath.go
@@ -14,18 +14,6 @@ type Datapath struct {
 	IsConnected bool   `json:"is_connected"`
 }
 
-type DatapathRelation struct {
-	ItemBase
-	DPID          int    `json:"datapath_id"`
-	NetworkID     int    `json:"network_id,omitempty"`
-	SegmentID     int    `json:"segment_id,omitempty"`
-	RouteLinkID   int    `json:"route_link_id,omitempty"`
-	InterfaceID   int    `json:"interface_id"`
-	MacAddresssID int    `json:"mac_address_id"`
-	IpLeaseID     int    `json:"ip_lease_id"`
-	MacAddress    string `json:"mac_address"`
-}
-
 type DatapathList struct {
 	ListBase
 	Items []Datapath `json:"items"`
@@ -36,22 +24,10 @@ type DatapathService struct {
 }
 
 type DatapathCreateParams struct {
-	UUID        string `url:"uuid"`
+	UUID        string `url:"uuid,omitempty"`
 	DisplayName string `url:"display_name"`
 	DPID        string `url:"dpid"`
 	NodeId      string `url:"node_id"`
-}
-
-type DatapathRelationType struct {
-	Params     *DatapathRelationCreateParams
-	DatapathID string
-	Type       string
-	ID         string
-}
-
-type DatapathRelationCreateParams struct {
-	InterfaceUUID string `url:"interface_uuid"`
-	MacAddress    string `url:"mac_address,omitempty"`
 }
 
 func (s *DatapathService) Create(params *DatapathCreateParams) (*Datapath, *http.Response, error) {
@@ -76,8 +52,43 @@ func (s *DatapathService) GetByUUID(id string) (*Datapath, *http.Response, error
 	return dp, resp, err
 }
 
-func (s *DatapathService) CreateDatapathRelation(rel *DatapathRelationType) (*DatapathRelation, *http.Response, error) {
+///
+///    Datapath Relations
+///
+
+type Relation struct {
+	DatapathID       string
+	Type             string
+	RelationTypeUUID string
+}
+
+type DatapathRelation struct {
+	Relation      *Relation
+	Body          struct {
+		ItemBase
+		DPID          int    `json:"datapath_id"`
+		NetworkID     int    `json:"network_id,omitempty"`
+		SegmentID     int    `json:"segment_id,omitempty"`
+		RouteLinkID   int    `json:"route_link_id,omitempty"`
+		InterfaceID   int    `json:"interface_id"`
+		MacAddresssID int    `json:"mac_address_id"`
+		IpLeaseID     int    `json:"ip_lease_id"`
+		MacAddress    string `json:"mac_address"`
+	}
+}
+
+type DatapathRelationCreateParams struct {
+	InterfaceUUID string `url:"interface_uuid"`
+	MacAddress    string `url:"mac_address,omitempty"`
+}
+
+func (s *DatapathService) CreateDatapathRelation(rel *Relation, params *DatapathRelationCreateParams) (*DatapathRelation, *http.Response, error) {
 	dpr := new(DatapathRelation)
-	resp, err := s.client.post(DatapathNamespace+"/"+rel.DatapathID+"/"+rel.Type+"/"+rel.ID, dpr, rel.Params)
+	dpr.Relation = rel
+	resp, err := s.client.post(DatapathNamespace+"/"+rel.DatapathID+"/"+rel.Type+"/"+rel.RelationTypeUUID, &dpr.Body, params)
 	return dpr, resp, err
+}
+
+func (s *DatapathService) DeleteDatapathRelation(params *DatapathRelation) (*http.Response, error) {
+	return s.client.del(DatapathNamespace+"/"+params.Relation.DatapathID+"/"+params.Relation.Type+"/"+params.Relation.RelationTypeUUID)
 }
