cf27685a2888cc14d23bc0c9a225905237b2b21c
b0r6 <tommy@axsh.net>
2016-09-14 16:17:03 +0900

    Added call to provider

--- client/terraform-provider-openvnet/main.go
+++ client/terraform-provider-openvnet/main.go
@@ -4,3 +4,9 @@ import (
 	"github.com/axsh/openvnet/client/terraform-provider-openvnet/openvnet"
 	"github.com/hashicorp/terraform/plugin"
 )
+
+func main() {
+	plugin.Serve(&plugin.ServeOpts{
+		ProviderFunc: openvnet.Provider,
+	})
+}
