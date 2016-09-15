300af94a7f2ab337c98481cc03e5b58aa9680168
toros11 <tobias.rosen@axsh.net>
2016-09-15 17:50:32 +0900

    Made datapath relations a separate resource

---
+++ client/terraform-provider-openvnet/openvnet/resource_openvnet_datapath_relation.go
@@ -0,0 +1,166 @@
+package openvnet
+
+import (
+	"github.com/axsh/openvnet/client/go-openvnet"
+	"github.com/hashicorp/terraform/helper/schema"
+)
+
+func OpenVNetDatapathRelation() *schema.Resource {
+	return &schema.Resource{
+		Create: openVNetDatapathRelationCreate,
+		Read:   openVNetDatapathRelationRead,
+		Update: openVNetDatapathRelationUpdate,
+		Delete: openVNetDatapathRelationDelete,
+
+		Schema: map[string]*schema.Schema{
+
+			"uuid": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+
+			"network": &schema.Schema{
+				Type:     schema.TypeList,
+				Optional: true,
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"uuid": &schema.Schema{
+							Type:     schema.TypeString,
+							Required: true,
+						},
+
+						"mac_address": &schema.Schema{
+							Type:     schema.TypeString,
+							ForceNew: true,
+							Optional: true,
+						},
+
+						"interface_uuid": &schema.Schema{
+							Type:     schema.TypeString,
+							Optional: true,
+							ForceNew: true,
+						},
+					},
+				},
+			},
+
+			"route_link": &schema.Schema{
+				Type:     schema.TypeList,
+				Optional: true,
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"uuid": &schema.Schema{
+							Type:     schema.TypeString,
+							Required: true,
+						},
+
+						"mac_address": &schema.Schema{
+							Type:     schema.TypeString,
+							ForceNew: true,
+							Optional: true,
+						},
+
+						"interface_uuid": &schema.Schema{
+							Type:     schema.TypeString,
+							Optional: true,
+							ForceNew: true,
+						},
+					},
+				},
+			},
+
+			"segment": &schema.Schema{
+				Type:     schema.TypeList,
+				Optional: true,
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"uuid": &schema.Schema{
+							Type:     schema.TypeString,
+							Required: true,
+						},
+
+						"mac_address": &schema.Schema{
+							Type:     schema.TypeString,
+							Optional: true,
+						},
+
+						"interface_uuid": &schema.Schema{
+							Type:     schema.TypeString,
+							Optional: true,
+						},
+					},
+				},
+			},
+		},
+	}
+}
+
+
+func openVNetDatapathRelationCreate(d *schema.ResourceData, m interface{}) error {
+
+	client := m.(*openvnet.Client)
+	
+	createRelation := func(relationType string) {
+		if r := d.Get(relationType[:len(relationType)-1]) ; r != nil {
+			for _, relationTypeMap := range r.([]interface{}) {
+				relationMap := relationTypeMap.(map[string]interface{})
+				
+				relation := &openvnet.Relation{
+					DatapathID: d.Get("uuid").(string),
+					Type: relationType,
+					RelationTypeUUID: relationMap["uuid"].(string),
+				}
+				
+				relationParams := &openvnet.DatapathRelationCreateParams{
+					InterfaceUUID: relationMap["interface_uuid"].(string),
+					MacAddress: relationMap["mac_address"].(string),
+				}
+
+				client.Datapath.CreateDatapathRelation(relation, relationParams)
+			}
+		}
+	}
+
+	createRelation("networks")
+	createRelation("route_links")
+	createRelation("segments")
+
+	return nil
+}
+
+
+func openVNetDatapathRelationRead(d *schema.ResourceData, m interface{}) error {
+	return nil
+}
+
+func openVNetDatapathRelationUpdate(d *schema.ResourceData, m interface{}) error {
+	return nil
+}
+
+func openVNetDatapathRelationDelete(d *schema.ResourceData, m interface{}) error {
+
+	client := m.(*openvnet.Client)
+
+	deleteRelation := func(relationType string) {
+		if r := d.Get(relationType[:len(relationType)-1]) ; r != nil {
+			for _, relationTypeMap := range r.([]interface{}) {
+				relationMap := relationTypeMap.(map[string]interface{})
+
+				relation := &openvnet.Relation{
+					DatapathID: d.Get("uuid").(string),
+					Type: relationType,
+					RelationTypeUUID: relationMap["uuid"].(string),
+				}
+
+				client.Datapath.DeleteDatapathRelation(relation)
+			}
+		}
+	}
+
+	deleteRelation("networks")
+	deleteRelation("route_links")
+	deleteRelation("segments")
+
+	return nil
+}
