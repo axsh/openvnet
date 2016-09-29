95eb72a0d14f331de4cd3e5cbb3fe2dccf11845a
Masahiro Fujiwara <m-fujiwara@axsh.net>
2016-09-29 17:25:48 +0900

    openvswitch-fedora.spec should be used for RHEL/CentOS 7.

--- deployment/packagebuild/packages.d/third_party/openvnet-openvswitch/rpmbuild.sh
+++ deployment/packagebuild/packages.d/third_party/openvnet-openvswitch/rpmbuild.sh
@@ -14,11 +14,19 @@ curl -O http://openvswitch.org/releases/openvswitch-${ovs_version}.tar.gz
 tar zxvf openvswitch-${ovs_version}.tar.gz
 cp openvswitch-${ovs_version}/rhel/openvswitch-kmod.files .
 
-yum-builddep -y openvswitch-${ovs_version}/rhel/openvswitch.spec
-# Run openvswitch unit tests when you set WITH_TEST=1.
-rpmbuild -bb ${WITH_TEST:---without check} openvswitch-${ovs_version}/rhel/openvswitch.spec
-# Building kmod for RHEL <= 6.5. 
-e=$(set +e; rpmdev-vercmp 6.6 $(lsb_release -s -r) > /dev/null; echo $?;)
-if [[ $e -eq 11 ]]; then
-  rpmbuild -bb openvswitch-${ovs_version}/rhel/openvswitch-kmod-rhel6.spec
+if [[ $(rpm -E '%{rhel}') -eq 6 ]]; then 
+  # RHEL/CentOS6
+  yum-builddep -y openvswitch-${ovs_version}/rhel/openvswitch.spec
+  # Run openvswitch unit tests when you set WITH_TEST=1.
+  rpmbuild -bb ${WITH_TEST:---without check} openvswitch-${ovs_version}/rhel/openvswitch.spec
+  # Building kmod for RHEL <= 6.5. 
+  e=$(set +e; rpmdev-vercmp 6.6 $(lsb_release -s -r) > /dev/null; echo $?;)
+  if [[ $e -eq 11 ]]; then
+    rpmbuild -bb openvswitch-${ovs_version}/rhel/openvswitch-kmod-rhel6.spec
+  fi
+else
+  # RHEL/CentOS >= 7
+  yum-builddep -y openvswitch-${ovs_version}/rhel/openvswitch-fedora.spec
+  # Run openvswitch unit tests when you set WITH_TEST=1.
+  rpmbuild -bb ${WITH_TEST:---without check} openvswitch-${ovs_version}/rhel/openvswitch-fedora.spec
 fi
