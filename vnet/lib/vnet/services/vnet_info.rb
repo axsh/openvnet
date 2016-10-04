5de7e547b06913508a84ca6f969c2f599f8a6810
rakshasa <sundell.software@gmail.com>
2016-10-04 14:49:25 +0900

    Cleaned up how vnmgr is initialized.

--- vnet/lib/vnet/services/vnet_info.rb
+++ vnet/lib/vnet/services/vnet_info.rb
@@ -20,17 +20,53 @@ module Vnet::Services
       attr_reader "#{name}_manager"
     end
 
+    def initialize
+      internal_initialize_managers(MANAGER_NAMES)
+    end
+
+    def inspect
+      "<##{self.class.name}>"
+    end
+
+    #
+    # Managers:
+    #
+
     def managers
       MANAGER_NAMES.map { |name| __send__("#{name}_manager") }
     end
 
-    def initialize_managers
-      MANAGER_NAMES.map do |name|
-        Vnet::Services.const_get("#{name.to_s.camelize}Manager").new(self).tap do |manager|
-          instance_variable_set("@#{name}_manager", manager)
-          yield(manager) if block_given?
+    def terminate_managers(timeout = 10.0)
+      internal_terminate_managers(managers, timeout)
+    end
+
+    #
+    # Internal methods:
+    #
+
+    private
+
+    def internal_initialize_managers(name_list)
+      name_list.each { |name|
+        instance_variable_set("@#{name}_manager", Vnet::Services.const_get("#{name.to_s.camelize}Manager").new(self))
+      }
+    end
+
+    def internal_terminate_managers(manager_list, timeout)
+      manager_list.each { |manager|
+        begin
+          manager.terminate
+        rescue Celluloid::DeadActorError
         end
-      end
+      }
+
+      start_time = Time.new
+
+      manager_list.each { |manager|
+        next_timeout = timeout - (Time.new - start_time)
+
+        Celluloid::Actor.join(manager, (next_timeout < 0.1) ? 0.1 : next_timeout)
+      }
     end
 
   end
--- vnet/lib/vnet/services/vnmgr.rb
+++ vnet/lib/vnet/services/vnmgr.rb
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 module Vnet::Services
   class Vnmgr
     include Celluloid
@@ -8,18 +10,32 @@ module Vnet::Services
 
     def initialize
       @vnet_info = VnetInfo.new
-      link_with_managers
     end
 
-    def link_with_managers
-      @vnet_info.initialize_managers do |manager|
-        begin
-          link(manager)
-        rescue => e
-          error "Fail to link with #{manager.class.name}: #{e}"
-          raise e
-        end
-      end
+    def do_initialize
+      info log_format('initializing managers')
+
+      # Do linking here?...
+
+      @vnet_info.managers { |manager|
+        managers.each { |manager| manager.event_handler_queue_only }
+        managers.each { |manager| manager.async.start_initialize }
+        managers.each { |manager| manager.wait_for_initialized(nil) }
+        managers.each { |manager| manager.event_handler_active }
+      }
+
+      info log_format('initialized managers')
     end
+
+    #
+    # Internal methods:
+    #
+
+    private
+
+    def log_format(message, values = nil)
+      "vnmgr: #{message}" + (values ? " (#{values})" : '')
+    end
+
   end
 end
