2ad4b3716e15a04bdd505e3f824f681be0b652ce
toros11 <tobias.rosen@axsh.net>
2016-10-19 16:19:38 +0900

    Added exmaples for mac range groups to terraform provider

---
+++ client/terraform-provider-openvnet/example/mac_range_group/main.tf
@@ -0,0 +1,12 @@
+provider "openvnet" {
+  api_endpoint = "http://localhost:9090/api/1.0"
+}
+
+resource "openvnet_mac_range_group" "mrg" {
+  uuid = "mgr-dpg"
+ 
+  mac_range {
+    begin_mac_address = "00:00:00:00:00:00"
+    end_mac_address = "00:00:00:00:00:ff"
+  }
+}
