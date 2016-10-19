67a567c6b8019a1a77a99e90537ebeeb8d0ca8d4
toros11 <tobias.rosen@axsh.net>
2016-10-19 16:12:35 +0900

    Added missing character

--- client/terraform-provider-openvnet/example/network/main.tf
+++ client/terraform-provider-openvnet/example/network/main.tf
@@ -5,7 +5,7 @@ provider "openvnet" {
 resource "openvnet_network" "nw_sample" {
 	 uuid = "nw-demo"
 	 display_name = "sample network with segments"
-	 ipv4_network = "10.0.0.0
+	 ipv4_network = "10.0.0.0"
 	 ipv4_prefix = "24"
 	 network_mode = "virtual"
 	 segment_uuid = "seg-demo"
