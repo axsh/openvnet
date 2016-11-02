7af05fd416c1d3180d98ec409d7f15421c705adb
rakshasa <sundell.software@gmail.com>
2016-11-02 19:42:43 +0900

    Try wanedge with GRE after the ping_tries has been increased.

--- integration_test/lib/vnspec/spec/wanedge_spec.rb
+++ integration_test/lib/vnspec/spec/wanedge_spec.rb
@@ -6,5 +6,5 @@ require_relative 'shared_examples/wanedge.rb'
 # TODO: Add multiple networks and use all vm's.
 
 describe 'wanedge', :vms_enable_vm => [:vm1, :vm7] do
-  include_examples 'wanedge examples', 'nw-global', pending_gre: true
+  include_examples 'wanedge examples', 'nw-global', pending_gre: false
 end
