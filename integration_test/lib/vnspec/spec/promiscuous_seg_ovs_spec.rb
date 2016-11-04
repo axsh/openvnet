df84cbc1a8ae462a9aae043edd750e558c26849f
rakshasa <sundell.software@gmail.com>
2016-11-05 03:30:43 +0900

    Bumped the promiscuous ping_tries to 60.

--- integration_test/lib/vnspec/spec/promiscuous_seg_ovs_spec.rb
+++ integration_test/lib/vnspec/spec/promiscuous_seg_ovs_spec.rb
@@ -4,7 +4,7 @@ require_relative 'spec_helper'
 require_relative 'shared_examples/promiscuous.rb'
 
 describe 'promiscuous_seg_ovs', :vms_enable_vm => :vm1_5_7, :vms_disable_dhcp => true do
-  let(:ping_tries) { 30 }
+  let(:ping_tries) { 60 }
 
   include_examples 'promiscuous segment examples'
 end
