50f5293efcb15062c63e48423ceca974770d8c79
rakshasa <sundell.software@gmail.com>
2016-11-05 03:55:09 +0900

    Set promiscuous specs to pending_local.

--- integration_test/lib/vnspec/spec/promiscuous_seg_of_spec.rb
+++ integration_test/lib/vnspec/spec/promiscuous_seg_of_spec.rb
@@ -4,7 +4,7 @@ require_relative 'spec_helper'
 require_relative 'shared_examples/promiscuous.rb'
 
 describe 'promiscuous_seg_of', :vms_enable_vm => :vm1_5_7, :vms_disable_dhcp => true do
-  let(:ping_tries) { 60 }
+  let(:ping_tries) { 10 }
 
-  include_examples 'promiscuous segment examples'
+  include_examples 'promiscuous segment examples', pending_local: true
 end
