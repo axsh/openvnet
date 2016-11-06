d23d1aaafb1d856709853b01bcb94d98c7a9015f
rakshasa <sundell.software@gmail.com>
2016-11-06 10:07:24 +0900

    Added interface_assoc specs for networks and route_links.

--- vnet/spec/fabricators/route_fabricator.rb
+++ vnet/spec/fabricators/route_fabricator.rb
@@ -1,9 +1,15 @@
 # -*- coding: utf-8 -*-
+
 require 'ipaddr'
 
 Fabricator(:route, class_name: Vnet::Models::Route) do
   interface { Fabricate(:interface) }
   route_link { Fabricate(:route_link) }
+  ipv4_network IPAddr.new("192.168.2.0").to_i
+  ipv4_prefix 24
+end
+
+Fabricator(:route_free, class_name: Vnet::Models::Route) do
   ipv4_network IPAddr.new("192.168.2.0").to_i
   ipv4_prefix 24
 end
