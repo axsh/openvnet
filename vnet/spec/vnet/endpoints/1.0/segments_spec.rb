368b69bc268a5aae23debb98f83a406b2271eba9
Metallion <akieckens@axsh.net>
2016-12-14 16:03:04 +0900

    Added tests for POST segments

--- vnet/spec/vnet/endpoints/1.0/segments_spec.rb
+++ vnet/spec/vnet/endpoints/1.0/segments_spec.rb
@@ -18,4 +18,22 @@ describe "/segments" do
   include_examples "GET /"
   include_examples "GET /:uuid"
   include_examples "DELETE /:uuid"
+
+  describe "POST /" do
+    expected_response = {
+      :uuid => "seg-test",
+      :mode => "virtual"
+    }
+    accepted_params = expected_response
+    required_params = [:mode]
+    uuid_params = []
+
+    include_examples "POST /", accepted_params, required_params, uuid_params, expected_response
+
+    context "With a wrong value for mode" do
+      let(:request_params) { { mode: "klein soldaatje, groot soldaatje" } }
+
+      it_should_return_error(400, "ArgumentError")
+    end
+  end
 end
