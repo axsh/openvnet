51bb788e2025d11e853f3d5d7ad467b43b11d884
Metallion <akieckens@axsh.net>
2016-12-14 15:28:06 +0900

    Started writing unit tests for segments API

---
+++ vnet/spec/vnet/endpoints/1.0/segments_spec.rb
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+require 'spec_helper'
+require 'vnet/endpoints/1.0/vnet_api'
+Dir["#{File.dirname(__FILE__)}/shared_examples/*.rb"].map {|f| require f }
+Dir["#{File.dirname(__FILE__)}/matchers/*.rb"].map {|f| require f }
+
+def app
+  Vnet::Endpoints::V10::VnetAPI
+end
+
+describe "/segments" do
+  let(:api_suffix)  { "segments" }
+  let(:fabricator)  { :segment }
+  let(:model_class) { Vnet::Models::Segment }
+
+  include_examples "GET /"
+end
