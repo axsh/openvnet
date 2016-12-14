bb2e05d8e4aec8cfc30fa0fb1cc8d163beabd36d
Metallion <akieckens@axsh.net>
2016-12-14 18:11:58 +0900

    Added tests for POST /filters

--- vnet/spec/vnet/endpoints/1.0/filters_spec.rb
+++ vnet/spec/vnet/endpoints/1.0/filters_spec.rb
@@ -18,4 +18,21 @@ describe "/filters" do
   include_examples "GET /"
   include_examples "GET /:uuid"
   include_examples "DELETE /:uuid"
+
+  describe "POST /" do
+    let!(:interface) { Fabricate(:interface) { uuid "if-filtest" } }
+
+    expected_response = {
+      :uuid => "fil-test",
+      :interface_uuid => "if-filtest",
+      :egress_passthrough => true,
+      :ingress_passthrough => true,
+      :mode => "static"
+    }
+    accepted_params = expected_response
+    required_params = [:interface_uuid, :mode]
+    uuid_params = [:interface_uuid]
+
+    include_examples "POST /", accepted_params, required_params, uuid_params, expected_response
+  end
 end
