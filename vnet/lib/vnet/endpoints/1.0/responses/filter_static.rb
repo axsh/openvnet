34c82841d91111c9e9b952d26744e38f5e74b961
Metallion <akieckens@axsh.net>
2016-12-16 16:28:33 +0900

    Changed the filter static response to match the api

--- vnet/lib/vnet/endpoints/1.0/responses/filter_static.rb
+++ vnet/lib/vnet/endpoints/1.0/responses/filter_static.rb
@@ -4,19 +4,12 @@ module Vnet::Endpoints::V10::Responses
   class FilterStatic < Vnet::Endpoints::CollectionResponseGenerator
     def self.generate(object)
       argument_type_check(object,Vnet::ModelWrappers::FilterStatic)
-      object.to_hash.tap { |res|
-        # Separation between source port and destination port is implemented
-        # in the model but the code to handle it isn't quite there yet. Hide the
-        # model in the API response for now.
-        res[:port_number] = res[:port_src]
-        res.delete(:port_src)
-        res.delete(:port_dst)
 
-        # Same as the above for ipv4 address.
-        res[:ipv4_address] = object.batch.ipv4_src_address_s.commit
-        res.delete(:ipv4_src_address)
-        res.delete(:ipv4_dst_address)
-        res.delete(:ipv4_dst_prefix)
+      res = {
+        ipv4_address: object.batch.ipv4_src_address_s.commit,
+        port_number: object.port_dst,
+        protocol: object.protocol,
+        passthrough: object.passthrough
       }
     end
   end
