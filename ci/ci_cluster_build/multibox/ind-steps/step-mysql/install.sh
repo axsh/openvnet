c78b41d80671517e42d324fe7db5173056c7ace0
toros11 <tobias.rosen@axsh.net>
2017-01-13 22:20:55 +0900

    Added community package required to install mysql server

--- ci/ci_cluster_build/multibox/ind-steps/step-mysql/install.sh
+++ ci/ci_cluster_build/multibox/ind-steps/step-mysql/install.sh
@@ -1,7 +1,14 @@
+(
+    $starting_step "Add mysql community release rpm package"
+    sudo chroot ${TMP_ROOT} /bin/bash -c "rpm qe | grep -wq mysql-community-release-el7"
+    $skip_step_if_already_done; set -ex
+    sudo chroot ${TMP_ROOT} /bin/bash -c "wget http://repo.mysql.com/mysql-community-release-el7-5.noarch.rpm"
+    sudo chroot ${TMP_ROOT} /bin/bash -c "rpm -ivh mysql-community-release-el7-5.noarch.rpm"
+) ; prev_cmd_failed
 
 (
     $starting_step "Install Mysql"
-    [[ -f ${TMP_ROOT}/etc/init.d/mysqld ]]
+    sudo chroot ${TMP_ROOT} /bin/bash -c "rpm qe | grep -wq mysql-community-server-5.6.35-2.el7.x86_64"
     $skip_step_if_already_done; set -xe
     sudo chroot "${TMP_ROOT}" /bin/bash -ex <<EOS
          yum install -y mysql-server
