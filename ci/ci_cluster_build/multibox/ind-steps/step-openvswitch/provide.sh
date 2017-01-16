5f4b9780f53900a47fa39f5bc796e4749ced86fa
toros11 <tobias.rosen@axsh.net>
2017-01-16 19:09:53 +0900

    Reverse provide for openvswitch

--- ci/ci_cluster_build/multibox/ind-steps/step-openvswitch/provide.sh
+++
@@ -1,6 +0,0 @@
-(
-    $starting_step "Install Open vSwitch"
-    run_ssh root@${IP_ADDR} "rpm -qa | grep -wq openvswitch"
-    $skip_step_if_already_done ; set -xe
-    run_ssh root@${IP_ADDR} "yum install -y openvswitch"
-) ; prev_cmd_failed
