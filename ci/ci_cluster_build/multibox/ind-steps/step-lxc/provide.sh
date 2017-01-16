71f009f7985056d792181c4f62ee62e4d2ed3242
toros11 <tobias.rosen@axsh.net>
2017-01-16 23:15:48 +0900

    Disable provide from lxc step

--- ci/ci_cluster_build/multibox/ind-steps/step-lxc/provide.sh
+++ ci/ci_cluster_build/multibox/ind-steps/step-lxc/provide.sh
@@ -1,10 +1,10 @@
-[[ -z ${#containers[@]} ]] || {
-    for c in ${containers[@]} ; do
-        (
-            $starting_step "Start container: $c"
-            run_ssh root@${IP_ADDR} "lxc-info -n $c | grep -q RUNNING"
-            $skip_step_if_already_done; set -ex
-            run_ssh root@${IP_ADDR} "lxc-start -n $c -d"
-        ) ; prev_cmd_failed
-    done
-}
+# [[ -z ${#containers[@]} ]] || {
+#     for c in ${containers[@]} ; do
+#         (
+#             $starting_step "Start container: $c"
+#             run_ssh root@${IP_ADDR} "lxc-info -n $c | grep -q RUNNING"
+#             $skip_step_if_already_done; set -ex
+#             run_ssh root@${IP_ADDR} "lxc-start -n $c -d"
+#         ) ; prev_cmd_failed
+#     done
+# }
