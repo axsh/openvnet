ba2765efaca8cda3205fc11313805e0b8f1456ea
toros11 <tobias.rosen@axsh.net>
2017-01-16 19:11:25 +0900

    Remove build call, openvswitch will be installed independently

--- ci/ci_cluster_build/multibox/ind-steps/step-openvnet/provide.sh
+++ ci/ci_cluster_build/multibox/ind-steps/step-openvnet/provide.sh
@@ -2,9 +2,6 @@
     $starting_group "Install OpenVNet"
     run_ssh root@${IP_ADDR} "rpm -qa | grep -wq openvnet"
     $skip_group_if_unnecessary
-
-    build "openvswitch"
-
     (
         $starting_step "Install OpenVNet"
         run_ssh root@${IP_ADDR} "rpm -qa | grep -wq openvnet"
