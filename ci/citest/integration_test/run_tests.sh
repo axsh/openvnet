db0b826a969f9f37bb3d358f99215ee4a26a6419
toros11 <tobias.rosen@axsh.net>
2017-03-02 19:41:22 +0900

    Overwrite the repo file to use the branch/release suffix we are running the test for

--- ci/citest/integration_test/run_tests.sh
+++ ci/citest/integration_test/run_tests.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+
+cat <<EOF > openvnet/ci/citest/integration_test/multibox/base/guestroot/etc/yum.repos.d/
+[openvnet]
+name=OpenVNet
+failovermethod=priority
+baseurl=https://ci.openvnet.org/repos/${BRANCH}/packages/rhel/7/vnet/${RELEASE_SUFFIX}
+enabled=1
+gpgcheck=0
+EOF
+
 openvnet/ci/citest/integration_test/multibox/build.sh
 
 . /etc/profile.d/rvm.sh
