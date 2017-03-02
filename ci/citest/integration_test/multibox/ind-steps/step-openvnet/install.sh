ac7eff2f9b7736a8697a39ccd1556a244075624e
toros11 <tobias.rosen@axsh.net>
2017-03-02 21:57:04 +0900

    Don't use group here

--- ci/citest/integration_test/multibox/ind-steps/step-openvnet/install.sh
+++ ci/citest/integration_test/multibox/ind-steps/step-openvnet/install.sh
@@ -1,8 +1,7 @@
-   $skip_group_if_unnecessary
-    (
-        $starting_step "Install OpenVNet"
-        run_cmd "rpm -qa | grep -wq openvnet"
-        $skip_step_if_already_done ; set -xe
-        run_cmd "yum install -y openvnet"
-    ) ; prev_cmd_failed
-)
+
+(
+    $starting_step "Install OpenVNet"
+    run_cmd "rpm -qa | grep -wq openvnet"
+    $skip_step_if_already_done ; set -xe
+    run_cmd "yum install -y openvnet"
+) ; prev_cmd_failed
