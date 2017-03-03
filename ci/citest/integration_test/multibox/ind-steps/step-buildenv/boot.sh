2839c463355ec1aadb74994320e2846aa2df26c3
toros11 <tobias.rosen@axsh.net>
2017-03-03 19:14:11 +0900

    Skip base if cache has been built

--- ci/citest/integration_test/multibox/ind-steps/step-buildenv/boot.sh
+++ ci/citest/integration_test/multibox/ind-steps/step-buildenv/boot.sh
@@ -4,6 +4,7 @@ for node in ${scheduled_nodes[@]} ; do
         $starting_group "Building ${node%,*}"
         false
         $skip_group_if_unnecessary
+        [[ "${node}" == "base" && -f "${CACHE_DIR}/${BRANCH}/box-disk1.raw" ]] && continue
         "${ENV_ROOTDIR}/${node}/build.sh"
     ) ; prev_cmd_failed
 done
