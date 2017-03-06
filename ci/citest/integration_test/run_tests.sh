0f827c1dcd84af217035a1de123ecc0146a07884
toros11 <tobias.rosen@axsh.net>
2017-03-06 20:04:05 +0900

    The repo needs to be updated on the cacheed images

--- ci/citest/integration_test/run_tests.sh
+++ ci/citest/integration_test/run_tests.sh
@@ -1,15 +1,5 @@
 #!/bin/bash
 
-
-cat <<EOF > openvnet/ci/citest/integration_test/multibox/base/guestroot/etc/yum.repos.d/
-[openvnet]
-name=OpenVNet
-failovermethod=priority
-baseurl=https://ci.openvnet.org/repos/${BRANCH}/packages/rhel/${BUILD_OS#*el}/vnet/${RELEASE_SUFFIX:-current}
-enabled=1
-gpgcheck=0
-EOF
-
 openvnet/ci/citest/integration_test/multibox/build.sh
 
 . /etc/profile.d/rvm.sh
