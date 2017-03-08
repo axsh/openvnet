770178f2b19763e2e1802868bf78e85643cc5438
toros11 <tobias.rosen@axsh.net>
2017-03-08 21:38:01 +0900

    Use build cache to store gems for integration test

--- ci/ci.el7/integration_test/run_tests.sh
+++ ci/ci.el7/integration_test/run_tests.sh
@@ -2,13 +2,16 @@
 
 openvnet/ci/ci.el7/integration_test/multibox/build.sh
 
+. ci/ci.el7/cache_functions.sh "openvnet-ci/branches/${BRANCH}"
 . /etc/profile.d/rvm.sh
+
+try_load_cache "/data" "/data" "${COMMIT_ID}"
 
 rvm use 2.2.0
 gem install bundler
 cd openvnet/integration_test
 bundle install
-
+create_cache "/data" "/data" "${COMMIT_ID}" "openvnet/ci/ci.el7/integration_test/build-cache.list"
 set -xe
 
 RELEASE_VERSION="el7" bin/itest-spec run
