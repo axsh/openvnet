c29aa046e307e9ef0683fd0cf12d214ce92af3e3
Tobias Rosen <tobias.rosen@axsh.net>
2017-03-29 17:23:40 +0900

    Fix cache dir and distro version

--- ci/ci.el6/integration_test/build_and_run_in_docker.sh
+++ ci/ci.el6/integration_test/build_and_run_in_docker.sh
@@ -15,7 +15,7 @@ set -a
 set +a
 
 DATA_DIR="${DATA_DIR:-/data}"
-CACHE_DIR="/data/openvnet-ci/branches"
+CACHE_DIR="/data/openvnet-ci/el6/branches"
 
 repo_and_tag="openvnet/integration-test:${BRANCH}.${RELEASE_SUFFIX}"
 
