67315c0782ae81e30ad05e4c418463925da84d37
toros11 <tobias.rosen@axsh.net>
2017-04-17 16:35:55 +0900

    Added the Jenkinsfile and scripts for building el7 third-parties

--- ci/ci.el7.third-party/build_packages_third_party.sh
+++ ci/ci.el7.third-party/build_packages_third_party.sh
@@ -6,33 +6,13 @@
 set -e
 set -x
 
-package=$1
-version=$2
-REPO_BASE_DIR="${REPO_BASE_DIR:-/var/www/html/repos}"
+version=$1
+REPO_BASE_DIR="/repos"
 current_dir=$(cd $(dirname ${BASH_SOURCE[0]}) && pwd)
 
-function build_all_packages(){
-  find ${current_dir}/packages.d/third_party -mindepth 1 -maxdepth 1 -type d | while read line; do
-    cd ${line}
-    # Select suitable versions file.
-    # el6.versions, el7.versions, versions
-    vfile=$(
-      if [[ -f el$(rpm -E '%{rhel}').versions ]]; then
-        echo el$(rpm -E '%{rhel}').versions
-      else
-        echo "versions"
-      fi
-    )
-    cat "${vfile}" | while read v; do
-      build_package $(basename ${line}) "${v}"
-    done
-  done
-}
-
 function build_package(){
-  local name=$1
-  local version=$2
-  local recipe_dir=${current_dir}/packages.d/third_party/${name}
+  local version=$1
+  local recipe_dir=${current_dir}/packages.d/openvnet-openvswitch
   if [[ -x ${recipe_dir}/rpmbuild.sh ]]; then
     (cd ${recipe_dir}; ./rpmbuild.sh "${version}")
   else
@@ -57,13 +37,15 @@ fi
 rpm -q rpmdevtools > /dev/null || yum install -y rpmdevtools
 rpm -q createrepo > /dev/null || yum install -y createrepo
 
-if [[ -n ${package} ]]; then
-  build_package ${package} ${version}
+if [[ -n ${version} ]]; then
+    build_package ${version}
 else
-  build_all_packages
+  cat "${current_dir}/packages.d/openvnet-openvswitch/versions" | while read v; do
+    build_package "${v}"
+  done
 fi
 
-repo_dir=${REPO_BASE_DIR}/packages/rhel/$(rpm -E '%{rhel}')/third_party/$(date +%Y%m%d%H%M%S)
+repo_dir=${REPO_BASE_DIR}/packages/rhel/7/third_party/$(date +%Y%m%d%H%M%S)
 [ -d ${repo_dir} ] || mkdir -p ${repo_dir}
 # Copy all rpms from rpmbuild/RPMS/*.
 cp -a $(rpm -E '%{_rpmdir}')/* ${repo_dir}
---
+++ ci/ci.el7.third-party/packages.d/openvnet-openvswitch/rpmbuild.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#
+# dependencies: rpm-build redhat-rpm-config rpmdevtools yum-utils gcc make python-devel openssl-devel kernel-devel-$(uname -r) kernel-debug-devel-$(uname -r)
+#
+
+set -e
+
+ovs_version=${1:-"2.4.1"}
+
+rpmdev-setuptree
+
+rpm -q redhat-lsb > /dev/null || yum install -y redhat-lsb
+
+cd $(rpm -E '%{_sourcedir}')
+curl -O http://openvswitch.org/releases/openvswitch-${ovs_version}.tar.gz
+tar zxvf openvswitch-${ovs_version}.tar.gz
+
+# RHEL/CentOS >= 7
+yum-builddep -y openvswitch-${ovs_version}/rhel/openvswitch-fedora.spec
+# Run openvswitch unit tests when you set WITH_TEST=1.
+rpmbuild -bb ${WITH_TEST:---without check} openvswitch-${ovs_version}/rhel/openvswitch-fedora.spec
+
