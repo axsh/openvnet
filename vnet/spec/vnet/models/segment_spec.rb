c8c4c020e516e97d7a4d884824ba95bad038bb51
Metallion <akieckens@axsh.net>
2017-05-12 14:29:54 +0900

    Added a test for deleting associated networks along with segments

--- vnet/spec/vnet/models/segment_spec.rb
+++ vnet/spec/vnet/models/segment_spec.rb
@@ -18,5 +18,24 @@ describe Vnet::Models::Segment do
       expect(Vnet::Models::MacAddress.where(segment: sgm).count).to eq(0)
       expect(Vnet::Models::MacAddress.with_deleted.where(segment: sgm).count).to eq(3)
     end
+
+    it "deletes associated networks (and in turn, ip_leases)" do
+      sgm = Fabricate(:segment)
+      3.times {
+        nw = Fabricate(:network, segment_id: sgm.id)
+        3.times {Fabricate(:ip_lease, network_id: nw.id) }
+      }
+
+      sgm.destroy
+
+      expect(Vnet::Models::Segment[sgm.canonical_uuid]).to eq(nil)
+      expect(Vnet::Models::Segment.with_deleted.where(uuid: sgm.uuid)).not_to eq(nil)
+
+      expect(Vnet::Models::Network.count).to eq(0)
+      expect(Vnet::Models::Network.with_deleted.count).to eq(3)
+
+      expect(Vnet::Models::IpLease.count).to eq(0)
+      expect(Vnet::Models::IpLease.with_deleted.count).to eq(9)
+    end
   end
 end
