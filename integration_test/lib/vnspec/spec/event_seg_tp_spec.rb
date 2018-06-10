1c4dff05b46b124b29eb1d1a9325db7cd6af7ecf
rakshasa <sundell.software@gmail.com>
2018-06-10 13:18:37 +0900

    Fixed syntax error in event_seg_tp spec.

--- integration_test/lib/vnspec/spec/event_seg_tp_spec.rb
+++ integration_test/lib/vnspec/spec/event_seg_tp_spec.rb
@@ -5,7 +5,7 @@ require_relative 'shared_examples/simple'
 
 describe 'event_seg_tp', :vms_disable_dhcp => true do
   describe 'fail after deleting first tp_nw' do
-    before(:all) do
+    before(:all) {
       vm1.change_ipv4_address('10.101.0.10')
       vm2.change_ipv4_address('10.101.0.10')
       vm3.change_ipv4_address('10.101.0.11')
