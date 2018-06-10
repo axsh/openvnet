fdc5b8235c23f03d23f3c95511909dfcc72eaa1c
rakshasa <sundell.software@gmail.com>
2018-06-10 12:47:22 +0900

    Improved topology event ci tests.

--- integration_test/lib/vnspec/spec/event_seg_tp_spec.rb
+++ integration_test/lib/vnspec/spec/event_seg_tp_spec.rb
@@ -4,22 +4,43 @@ require_relative 'spec_helper'
 require_relative 'shared_examples/simple'
 
 describe 'event_seg_tp', :vms_disable_dhcp => true do
-  before(:all) do
-    vm1.change_ipv4_address('10.101.0.10')
-    vm2.change_ipv4_address('10.101.0.10')
-    vm3.change_ipv4_address('10.101.0.11')
-    vm4.change_ipv4_address('10.101.0.11')
-    vm5.change_ipv4_address('10.101.0.12')
-    vm6.change_ipv4_address('10.101.0.12')
+  describe 'fail after deleting first tp_nw' do
+    before(:all) do
+      vm1.change_ipv4_address('10.101.0.10')
+      vm2.change_ipv4_address('10.101.0.10')
+      vm3.change_ipv4_address('10.101.0.11')
+      vm4.change_ipv4_address('10.101.0.11')
+      vm5.change_ipv4_address('10.101.0.12')
+      vm6.change_ipv4_address('10.101.0.12')
 
-    Vnspec::Models::Topology.add_segment('topo-vnet', 'seg-vseg1')
-    Vnspec::Models::Topology.add_segment('topo-vnet', 'seg-vseg2')
+      Vnspec::Models::Topology.add_segment('topo-vnet', 'seg-vseg1')
+      Vnspec::Models::Topology.add_segment('topo-vnet', 'seg-vseg2')
 
-    # Since no vm's do dhcp requests there is nothing to ensure that
-    # the vna's have properly loaded the segments and other
-    # information.
-    sleep 30
+      sleep 5
+
+      Vnspec::Models::Topology.remove_segment('topo-vnet', 'seg-vseg1')
+      sleep 5
+    }
+
+    include_examples 'simple examples fail first set'
   end
 
-  include_examples 'simple examples'
+  describe 'success after re-adding' do
+    before(:all) {
+      Vnspec::Models::Topology.add_segment('topo-vnet', 'seg-vseg1')
+      sleep 5
+    }
+
+    include_examples 'simple examples'
+  end
+
+  describe 'fail after deleting topology' do
+    before(:all) {
+      Vnspec::Models::Topology.delete('topo-vnet')
+      sleep 5
+    }
+
+    include_examples 'simple examples fail'
+  end
+
 end
