b19b174b30a64361895d2e9fd99e01b395fa8853
rakshasa <sundell.software@gmail.com>
2018-07-03 09:34:26 +0900

    Renamed router_p2v to router_p2v_2v_tp, and added new router_p2v, router_p2v_tp tests.

--- integration_test/lib/vnspec/spec/router_p2v_tp_spec.rb
+++ integration_test/lib/vnspec/spec/router_p2v_tp_spec.rb
@@ -3,6 +3,6 @@
 require_relative 'spec_helper'
 require_relative 'shared_examples/router.rb'
 
-describe 'router_p2v' do
+describe 'router_p2v_tp' do
   include_examples 'router p2v examples'
 end
--- integration_test/lib/vnspec/spec/shared_examples/router.rb
+++ integration_test/lib/vnspec/spec/shared_examples/router.rb
@@ -67,3 +67,70 @@ shared_examples 'router examples' do
     end
   end
 end
+
+shared_examples 'router p2v examples' do
+  describe 'underlay' do
+    it 'public reachable to public' do
+      expect(vm1).to be_reachable_to(vm3)
+      expect(vm3).to be_reachable_to(vm1)
+      expect(vm1).to be_reachable_to(vm5)
+      expect(vm5).to be_reachable_to(vm1)
+      expect(vm3).to be_reachable_to(vm5)
+      expect(vm5).to be_reachable_to(vm3)
+    end
+
+    it 'vnet reachable to vnet' do
+      expect(vm2).to be_reachable_to(vm4)
+      expect(vm4).to be_reachable_to(vm2)
+      expect(vm2).to be_reachable_to(vm6)
+      expect(vm6).to be_reachable_to(vm2)
+      expect(vm4).to be_reachable_to(vm6)
+      expect(vm6).to be_reachable_to(vm4)
+    end
+  end
+
+  describe 'local' do
+    it 'vnet reachable to public' do
+      expect(vm1).to be_reachable_to(vm2)
+      expect(vm2).to be_reachable_to(vm1)
+    end
+  end
+
+  describe 'mac2mac' do
+    it 'vnet reachable to public on different host' do
+      expect(vm2).to be_reachable_to(vm3)
+      expect(vm3).to be_reachable_to(vm2)
+      expect(vm1).to be_reachable_to(vm4)
+      expect(vm4).to be_reachable_to(vm1)
+    end
+
+    it 'vnet reachable to public on same host' do
+      expect(vm3).to be_reachable_to(vm4)
+      expect(vm4).to be_reachable_to(vm3)
+    end
+  end
+
+  describe 'tunnel' do
+    it 'reachable to public on different host' do
+      expect(vm2).to be_reachable_to(vm5)
+      expect(vm5).to be_reachable_to(vm2)
+      expect(vm1).to be_reachable_to(vm6)
+      expect(vm6).to be_reachable_to(vm1)
+    end
+
+    it 'reachable to public on same host' do
+      expect(vm5).to be_reachable_to(vm6)
+      expect(vm6).to be_reachable_to(vm5)
+    end
+  end
+
+  describe 'tunnel+mac2mac' do
+    it 'reachable to public on different host' do
+      expect(vm3).to be_reachable_to(vm6)
+      expect(vm6).to be_reachable_to(vm3)
+      expect(vm4).to be_reachable_to(vm5)
+      expect(vm5).to be_reachable_to(vm4)
+    end
+  end
+
+end
