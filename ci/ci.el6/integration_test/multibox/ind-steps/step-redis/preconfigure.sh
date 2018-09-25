44a380d068ab6ae0bb2ab95c63c47b295e8835b4
rakshasa <sundell.software@gmail.com>
2018-09-25 22:10:38 +0900

    Use yes/no in redis.conf, not on/off.

--- ci/ci.el6/integration_test/multibox/ind-steps/step-redis/preconfigure.sh
+++ ci/ci.el6/integration_test/multibox/ind-steps/step-redis/preconfigure.sh
@@ -9,10 +9,10 @@
 
 (
     $starting_step "Disable redis protected-mode"
-    sudo grep -q "^protected-mode off" ${TMP_ROOT}/etc/redis.conf
+    sudo grep -q "^protected-mode no" ${TMP_ROOT}/etc/redis.conf
     $skip_step_if_already_done; set -xe
-    run_cmd "sed -i 's,protected-mode on,protected-mode off,g' /etc/redis.conf"
-    run_cmd "sed -i 's,#protected-mode off,protected-mode off,g' /etc/redis.conf"
+    run_cmd "sed -i 's,protected-mode yes,protected-mode no,g' /etc/redis.conf"
+    run_cmd "sed -i 's,#protected-mode no,protected-mode no,g' /etc/redis.conf"
 ) ; prev_cmd_failed
 
 enable_service "redis"
