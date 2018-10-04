6f2b76b054bad3041f4741678e0b475f117e7b8e
rakshasa <sundell.software@gmail.com>
2018-10-04 13:34:35 +0900

    Install zeromq4-devel after adding openvnet-third-party repo.

--- ci/ci.el6/rspec_rpmbuild/build_packages_vnet.sh
+++ ci/ci.el6/rspec_rpmbuild/build_packages_vnet.sh
@@ -95,11 +95,11 @@ try_load_cache "${BUILD_CACHE_DIR}" "${CACHE_VOLUME}" "${COMMIT_ID}"
     cd vnet
     bundle install --path vendor/bundle --standalone
 
-    sleep 1000000
-
     bundle exec rake test:db:create
     bundle exec rake test:db:init
     bundle exec rspec spec
+
+#    sleep 1000000
 )
 
 # Copy only the tracked files to rpmbuild SOURCES/.
