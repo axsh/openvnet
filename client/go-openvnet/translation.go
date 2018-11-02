7a5c2a04f3f553bc4a8617caa5025e735051fc39
toros11 <tobias.rosen@axsh.net>
2018-11-02 21:08:10 +0900

    Added static address to translation

--- client/go-openvnet/translation.go
+++ client/go-openvnet/translation.go
@@ -50,3 +50,48 @@ func (s *TranslationService) GetByUUID(uuid string) (*TranslationList, *http.Res
 	item, resp, err := s.BaseService.GetByUUID(uuid)
 	return item.(*TranslationList), resp, err
 }
+
+type TranslationStaticAddress struct {
+	ItemBase
+	ID                 int    `json:"id,omitempty"`
+	TranslationID      int    `json:"translation_id,omitempty"`
+	RouteLinkID        int    `json:"route_link_id,omitempty"`
+	IngressIpv4Address string `json:"ingress_ipv4_address,omitempty"`
+	EgressIpv4Address  string `json:"egress_ipv4_address,omitempty"`
+	InressPortNumber   string `json:"ingress_port_number,omitempty"`
+	EgressPortNumber   string `json:"egress_port_number,omitempty"`
+}
+
+type TranslationStaticAddressList struct {
+	ListBase
+	Items []TranslationStaticAddress `json:"items"`
+}
+
+type TranslationStaticParams struct {
+	TranslationUUID    int    `url:"translation_id,omitempty"`
+	RouteLinkUUID      int    `url:"route_link_id,omitempty"`
+	IngressIpv4Address string `url:"ingress_ipv4_address"`
+	EgressIpv4Address  string `url:"egress_ipv4_address"`
+	InressPortNumber   string `url:"ingress_port_number,omitempty"`
+	EgressPortNumber   string `url:"egress_port_number,omitempty"`
+	InressNetworkUUID  int    `url:"ingress_network,omitempty"`
+	EgressNetworkUUID  int    `url:"egress_network,omitempty"`
+}
+
+func (s *TranslationService) CreateStatic(uuid string, params *TranslationStaticParams) (*TranslationStaticAddress, *http.Response, error) {
+	st := new(TranslationStaticAddress)
+	resp, err := s.client.post(FilterNamespace+"/"+uuid+"/static_address", st, params)
+	return st, resp, err
+}
+
+func (s *TranslationService) DeleteStatic(uuid string, param *TranslationStaticParams) (*http.Response, error) {
+	ovnError := new(OpenVNetError)
+	resp, err := s.client.sling.New().Delete(FilterNamespace+"/"+uuid+"/static_address").BodyForm(param).Receive(nil, ovnError)
+	return checkError(ovnError, resp, err)
+}
+
+func (s *TranslationService) GetStatic(uuid string) (*TranslationStaticAddressList, *http.Response, error) {
+	list := new(TranslationStaticAddressList)
+	resp, err := s.client.get(FilterNamespace+"/"+uuid+"/static_address", list)
+	return list, resp, err
+}
