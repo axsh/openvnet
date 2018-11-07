05c235a2f0e49d889e532de007dfbbe49b89fbcd
toros11 <tobias.rosen@axsh.net>
2018-11-07 20:40:41 +0900

    Test base service only using a local server

--- client/go-openvnet/base_test.go
+++ client/go-openvnet/base_test.go
@@ -1,72 +1,98 @@
 package openvnet
 
 import (
+	"log"
+	"net"
+	"net/http"
+	"net/http/httptest"
 	"os"
 	"testing"
 )
 
-type testableService struct {
-	service *BaseService
-	data    interface{}
+type testService struct {
+	*BaseService
 }
 
-var serviceTable []testableService
-var client *Client
+type testData struct {
+	ItemBase
+}
 
-func TestBaseCreate(t *testing.T) {
-	for _, i := range serviceTable {
-		testCreate(t, i.service, i.data)
+var ts = &testService{
+	BaseService: &BaseService{
+		namespace:    "test",
+		client:       testClient(),
+		resource:     &testData{},
+		resourceList: &testData{},
+	},
+}
+
+func TestCreate(t *testing.T) {
+	_, resp, e := ts.Create(&testData{})
+
+	if e != nil {
+		t.Errorf("Create() error should be nil: %v", e)
+	}
+
+	if resp.StatusCode != 200 {
+		t.Errorf("Status code should be 200")
+	}
+
+	checkBody(t, resp.Body)
+}
+
+func TestGet(t *testing.T) {
+	_, resp, e := ts.Get()
+
+	if e != nil {
+		t.Errorf("Get() error should be nil: %v", e)
+	}
+
+	if resp.StatusCode != 200 {
+		t.Errorf("Status code should be 200")
 	}
 }
 
-func TestBaseGetByUUID(t *testing.T) {
-	for _, i := range serviceTable {
-		testGetByUUID(t, i.service, getFieldValue(i.data, "UUID").String())
+func TestDelete(t *testing.T) {
+	resp, e := ts.Delete("test_id")
+
+	if e != nil {
+		t.Errorf("Delete() error should be nil: %v", e)
 	}
+
+	if resp.StatusCode != 200 {
+		t.Errorf("Status code should be 200")
+	}
+
+	checkBody(t, resp.Body)
 }
 
-func TestBaseGet(t *testing.T) {
-	for _, i := range serviceTable {
-		testGet(t, i.service)
-	}
-}
+func TestGetByUUID(t *testing.T) {
+	_, resp, e := ts.GetByUUID("test_id")
 
-func TestBaseDelete(t *testing.T) {
-	// reverse the array here as deleting the base resources which another depends
-	// depends on automatically sets the depending resource to deleted, which
-	// causes failure
-	s := serviceTable
-	for i, j := 0, len(s)-1; i < j; i, j = i+1, j-1 {
-		s[i], s[j] = s[j], s[i]
+	if e != nil {
+		t.Errorf("GetByUUID() error should be nil: %v", e)
 	}
 
-	for _, i := range s {
-		testDelete(t, i.service, getFieldValue(i.data, "UUID").String())
+	if resp.StatusCode != 200 {
+		t.Errorf("Status code should be 200")
 	}
+
+	checkBody(t, resp.Body)
 }
 
 func TestMain(m *testing.M) {
-	client = testClient()
-	// order is important here as some resources depends on other resources and
-	// will use their as the value for the required parameters
-	serviceTable = []testableService{
-		testableService{NewDatapathService(client).BaseService, testDatapath},
-		testableService{NewNetworkService(client).BaseService, testNetwork},
-		testableService{NewInterfaceService(client).BaseService, testInterface},
-		testableService{NewFilterService(client).BaseService, testFilter},
-		testableService{NewIpLeaseService(client).BaseService, testIpLease},
-		testableService{NewIpLeaseContainerService(client).BaseService, testIpLeaseContainer},
-		testableService{NewIpRangeGroupService(client).BaseService, testIpRangeGroup},
-		testableService{NewIpRetentionContainerService(client).BaseService, testIpRetentionContainer},
-		testableService{NewLeasePolicyService(client).BaseService, testLeasePoilcy},
-		testableService{NewMacRangeGroupService(client).BaseService, testMacRangeGroup},
-		testableService{NewMacLeaseService(client).BaseService, testMacLease},
-		testableService{NewRouteLinkService(client).BaseService, testRouteLink},
-		testableService{NewRouteService(client).BaseService, testRoute},
-		testableService{NewSegmentService(client).BaseService, testSegment},
-		testableService{NewTopologyService(client).BaseService, testTopology},
-		testableService{NewTranslationService(client).BaseService, testTranslation},
+	l, err := net.Listen("tcp", net.JoinHostPort("localhost", "9090"))
+	if err != nil {
+		log.Fatal("failed to create listener")
 	}
+	server := &httptest.Server{
+		Listener: l,
+		Config: &http.Server{
+			Handler: http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {}),
+		},
+	}
+	server.Start()
+	defer server.Close()
 
 	os.Exit(m.Run())
 }
